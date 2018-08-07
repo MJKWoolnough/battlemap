811faa89323aa01f56609b72b7436ba0423edb68
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-07 18:41:54 +0100

    added Assets.Init

--- assets.go
+++ assets.go
@@ -1,10 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+)
 
 var Assets assets
 
 type assets struct {
+}
+
+func (a *assets) Init(database *sql.DB) error {
+	return nil
 }
 
 func (a *assets) ServeHTTP(w http.ResponseWriter, r *http.Request) {
