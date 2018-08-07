508bd376495b44b3329eafb7882a38bbe9242018
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-07 16:47:14 +0100

    added client multiplexing

--- socket.go
+++ socket.go
@@ -1,25 +1,41 @@
 package main
 
 import (
-	"net"
+	"database/sql"
+	"encoding/json"
+	"io"
+	"net/rpc"
+	"net/rpc/jsonrpc"
 	"sync"
 
 	"golang.org/x/net/websocket"
+	"vimagination.zapto.org/memio"
 )
 
-var (
+var RPC rpcs
+
+type rpcs struct {
 	quitMu sync.Mutex
-	quit   = make(chan struct{})
-)
+	quit   chan struct{}
 
-func handleConn(conn *websocket.Conn) {
+	clientMu sync.Mutex
+	clients  map[*websocket.Conn]chan []byte
+}
+
+func (r *rpcs) Init(db *sql.DB) error {
+	r.quit = make(chan struct{})
+	r.clients = make(map[*websocket.Conn]chan []byte)
+	rpc.Register(r)
+}
+
+func (r *rpcs) handleConn(conn *websocket.Conn) {
 	if Session.GetAdmin(conn.Request()) {
-		quitMu.Lock()
-		close(quit)
-		quit = make(chan struct{})
-		myQuit := quit
+		r.quitMu.Lock()
+		close(r.quit)
+		r.quit = make(chan struct{})
+		myQuit := r.quit
 		done := make(chan struct{})
-		quitMu.Unlock()
+		r.quitMu.Unlock()
 		go func() {
 			select {
 			case <-myQuit:
@@ -27,17 +43,35 @@ func handleConn(conn *websocket.Conn) {
 			case <-done:
 			}
 		}()
-		adminSession(conn)
+		io.WriteString(conn, "{\"Admin\": true}")
+		jsonrpc.ServeConn(conn)
 		close(done)
 	} else {
-		userSession(conn)
+		io.WriteString(conn, "{\"Admin\": false}")
+		data := make(chan []byte, 1024)
+		r.clientMu.Lock()
+		r.clients[conn] = data
+		r.clientMu.Unlock()
+		// send loadMap with data
+		for {
+			if _, err := conn.Write(<-data); err != nil {
+				conn.WriteClose(5000)
+				break
+			}
+		}
+		r.clientMu.Lock()
+		delete(r.clients, conn)
+		close(data)
+		r.clientMu.Unlock()
 	}
 }
 
-func adminSession(conn net.Conn) {
-
-}
-
-func userSession(conn net.Conn) {
-
+func (r *rpcs) broadcast(v interface{}) {
+	var buf memio.Buffer
+	json.NewEncoder(&buf).Encode(v)
+	r.clientMu.Lock()
+	for _, c := range r.clients {
+		c <- buf
+	}
+	r.clientMu.Unlock()
 }
