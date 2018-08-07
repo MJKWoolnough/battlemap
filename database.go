f435d47c382e5ae482a116eaf7d88415f9f88fda
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-07 11:37:01 +0100

    added sane defaults for Config values and added Session init'ing

--- database.go
+++ database.go
@@ -21,12 +21,26 @@ func (db *db) Init(filename string) error {
 	if err != nil {
 		return errors.WithContext(fmt.Sprintf("error opening database file %q: ", filename), err)
 	}
-	if _, err = database.Exec("CREATE TABLE IF NOT EXISTS [Config]([Password] TEXT, [SessionKey] TEXT, [SessionData] TEXT);"); err != nil {
+	if _, err = database.Exec("CREATE TABLE IF NOT EXISTS [Config]([Password] TEXT NOT NULL DEFAULT '', [SessionKey] TEXT NOT NULL DEFAULT '', [SessionData] TEXT NOT NULL DEFAULT '');"); err != nil {
 		return errors.WithContext("error creating config table: ", err)
 	}
-	if err = Auth.Init(database); err != nil {
-		database.Close()
-		return errors.WithContext("error initialising authentication: ", err)
+	var num int
+	if err = database.QueryRow("SELECT COUNT(1) FROM [Config];").Scan(&num); err != nil {
+		return errors.WithContext("error counting Config rows: ", err)
+	} else if num == 0 {
+		_, err = database.Exec("INSERT INTO [Config]([SessionData]) VALUES ('');")
+		if err != nil {
+			return errors.WithContext("error creating initial Config data: ", err)
+		}
+	}
+	for name, init := range map[string]func(*sql.DB) error{
+		"authentication": Auth.Init,
+		"session":        Session.Init,
+	} {
+		if err = init(database); err != nil {
+			database.Close()
+			return errors.WithContext(fmt.Sprintf("error initialising %s: ", name), err)
+		}
 	}
 	db.DB = database
 	return nil
