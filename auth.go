05bc65accfad213bc8cfb212a50cacb9b3648de7
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-09 16:37:46 +0100

    used Salt and changed to template for login page

--- auth.go
+++ auth.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"database/sql"
+	"html/template"
 	"io"
 	"net/http"
+	"strings"
 
 	"vimagination.zapto.org/errors"
+	"vimagination.zapto.org/httpdir"
 )
 
 var Auth auth
 
 type auth struct {
-	passwordHash   string
+	password, salt string
+	login          *template.Template
 	updatePassword *sql.Stmt
 	http.ServeMux
 }
 
 func (a *auth) init(db *sql.DB) error {
-	err := db.QueryRow("SELECT [Password] FROM [Config];").Scan(&a.passwordHash)
+	err := db.QueryRow("SELECT [Password], [Salt] FROM [Config];").Scan(&a.passwordHash, &a.salt)
 	if err != nil {
 		return errors.WithContext("error retrieving password hash: ", err)
 	}
@@ -26,14 +31,41 @@ func (a *auth) init(db *sql.DB) error {
 	if err != nil {
 		return errors.WithContext("error creating prepared statement: ", err)
 	}
-	if a.passwordHash == "" {
-		a.passwordHash = a.hash("")
+	if a.salt == "" {
+		var buf [16]byte
+		rand.Read(buf[:])
+		a.salt = string(buf[:])
+		if _, err := db.Exec("UPDATE [Config] SET [Salt] = ?;", a.salt); err != nil {
+			return errors.WithContext("error inserting Salt into database: ", err)
+		}
+	}
+	if a.password == "" {
+		a.password = a.hash("")
+	}
+	f, err := httpdir.Default.Open("login.tmpl")
+	if err != nil {
+		return errors.WithContext("error opening login template: ", err)
+	}
+	var sb strings.Builder
+	if st, err := f.Stat(); err != nil {
+		return errors.WithContext("error getting login template size: ", err)
+	} else {
+		sb.Grow(int(st.Size()))
+	}
+	_, err = io.Copy(sb, f)
+	if err != nil {
+		return errors.WithContext("error reading login template: ", err)
+	}
+	f.Close()
+	a.login, err = template.New("login").Parse(sb.String())
+	if err != nil {
+		return errors.WithContext("error parsing login template: ", err)
 	}
 	return nil
 }
 
 func (a *auth) hash(password string) string {
-	data := sha256.Sum256([]byte(password))
+	data := sha256.Sum256([]byte(password + a.salt))
 	return string(data[:])
 }
 
@@ -43,34 +75,26 @@ func (a *auth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	r.ParseForm()
-	if _, ok := r.PostForm["submit"]; ok && a.hash(r.PostFormValue("password")) == a.passwordHash {
-		if n := r.PostFormValue("new"); n != "" && n == r.PostFormValue("confirm") {
-			a.passwordHash = a.hash(n)
-			DB.Lock()
-			a.updatePassword.Exec(a.passwordHash)
-			DB.Unlock()
-			Session.Refresh()
+	var vars struct {
+		InvalidPassword, PasswordChanged bool
+	}
+	if _, ok := r.PostForm["submit"]; ok {
+		if a.hash(r.PostFormValue("password")) == a.password {
+			if n := r.PostFormValue("new"); r.PostFormValue("change") == "change" && n == r.PostFormValue("confirm") {
+				a.passwordHash = a.hash(n)
+				DB.Lock()
+				a.updatePassword.Exec(a.passwordHash)
+				DB.Unlock()
+				Session.Refresh()
+				vars.PasswordChanged = true
+			} else {
+				Session.SetAdmin(w)
+				http.Redirect(w, r, "/map.html", http.StatusFound)
+				return
+			}
 		} else {
-			Session.SetAdmin(w)
-			http.Redirect(w, r, "/map.html", http.StatusFound)
-			return
+			vars.InvalidPassword = true
 		}
 	}
-	io.WriteString(w, loginPage)
+	a.login.Execute(w, &vars)
 }
-
-const loginPage = `<!doctype html>
-<html lang="en">
-	<head>
-		<title>Login</title>
-	</head>
-	<body>
-		<a href="map.html">Load Map</a>
-		<form action="/" method="post">
-			<label for="password">Password: </label><input id="password" name="password" type="password" /><br />
-			<label for="new">New Password?: </label><input id="new" name="new" type="password" /><br />
-			<label for="confirm">Confirm Password?: </label><input id="confirm" name="confirm" type="password" /><br />
-			<input type="submit" name="submit" value="Login" />
-		</form>
-	</body>
-</html>`
