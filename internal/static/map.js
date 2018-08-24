86da42742815f2132b8a9fe5bf15ee20c9962f61
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-24 13:03:31 +0100

    simplification

--- internal/static/map.js
+++ internal/static/map.js
@@ -2,14 +2,13 @@
 window.addEventListener("load", function() {
 	const rpc = new RPC("/socket");
 	rpc.await(-1, function(admin) {
-		const wg = new waitGroup(() => mapStart(rpc)),
-		      wgDone = wg.done.bind(wg);
+		const wg = new waitGroup(() => mapStart(rpc));
 		if (admin) {
 			wg.add(2);
-			include("assets.css", wgDone, alert.bind(null, "error loading asset css"));
-			include("assets.js", wgDone, alert.bind(null, "error loading asset script"));
+			include("assets.css", wg.done, alert.bind(null, "error loading asset css"));
+			include("assets.js", wg.done, alert.bind(null, "error loading asset script"));
 		}
 		wg.add(1);
-		include(admin ? "admin.js" : "user.js", wgDone, alert.bind(null, "error loading map script"));
+		include(admin ? "admin.js" : "user.js", wg.done, alert.bind(null, "error loading map script"));
 	});
 });
