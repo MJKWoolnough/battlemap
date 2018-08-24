76f8cea769a50c5cc0c15902f64f15946b0757d3
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-24 18:15:31 +0100

    start of Map List GUI code

--- internal/static/admin.js
+++ internal/static/admin.js
@@ -1,4 +1,34 @@
 "use strict";
 function mapStart(rpc) {
-	alert("admin");
+	rpc.request("Maps.ListMaps", null, function(maps) {
+		clearElement(document.body);
+		document.body.appendChild(createHTML(
+			"div",
+			{
+				"id": "mapList",
+			},
+			[
+				createHTML(
+					"button",
+					{
+						"onclick": function() {
+							const name = prompt("Map Name?", "");
+							if (name !== null && name !== "") {
+								rpc.request("Maps.AddMap", name, function(map) {
+									maps.push(map);
+									maps = maps.sort((m, n) => m.Order - n.Order);
+								});
+							}
+						}
+					},
+					"New Map"
+				),
+				createHTML("ul", {}, maps.map(m => createHTML(
+					"ul",
+					{},
+					map.Name
+				)))
+			]
+		));
+	});
 }
