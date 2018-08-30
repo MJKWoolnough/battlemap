bcbdcbcab68aedc0c5852bbc7f5a9a9cef310c43
Michael Woolnough <michael.woolnough@gmail.com>
2018-08-30 16:27:50 +0100

    application/javascript not text/javascript + small changes

--- internal/static/load.js
+++ internal/static/load.js
@@ -10,21 +10,21 @@ const pageLoad = new Promise(successFn => window.addEventListener("load", succes
 		const css = url.substr(-3) === "css",
 		      elm = document.createElement(css ? "link" : "script");
 		elm.setAttribute(css ? "href" : "src", url);
-		elm.setAttribute("type", css ? "text/css" : "text/javascript")
+		elm.setAttribute("type", css ? "text/css" : "application/javascript")
 		if (css) {
 			elm.setAttribute("rel", "stylesheet");
 		}
 		const p = new Promise((successFn, errorFn) => {
-			elm.addEventListener("onload", successFn);
+			elm.addEventListener("load", successFn);
 			elm.addEventListener("error", () => {
 				document.head.removeChild(elm);
-				errorFn("error including: " + url);
+				errorFn(new URIError("error including: " + url));
 			});
 			if (!css) {
-				elm.addEventListener("executed", e => {
+				elm.addEventListener("executed", ({detail}) => {
 					elm.removeEventListener("load", successFn);
 					document.head.removeChild(elm);
-					successFn(e["detail"]);
+					successFn(detail);
 				});
 			}
 			document.head.appendChild(elm);
