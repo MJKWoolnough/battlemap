a2cde20d996f5436cf78f0b312ddf3ef1b2d19f4
Michael Woolnough <michael.woolnough@gmail.com>
2018-09-13 14:20:32 +0100

    added initial admin data loader

--- internal/static/admin.js
+++ internal/static/admin.js
@@ -1,24 +1,21 @@
 "use strict";
 offer(async function(rpc) {
-	const {createHTML, clearElement, Layers} = await include("jslib/html.js");
+	const {createHTML, clearElement, layers} = await include("jslib/html.js"),
+	      Pipe = function() {
+		const out = [];
+		this.send = function(...data) {
+			out.forEach(o => o(...data));
+		};
+		this.receive = out.push.bind(out);
+	      },
+	      mapLoadPipe = new Pipe();
 	clearElement(document.body);
-	const overlay = new Layers(document.body.appendChild(createHTML("div", {"id": "overlay"})));
-	include("maplist.js").then(maplist => maplist(
-		rpc,
-		overlay,
-		document.body.appendChild(createHTML(
-			"div",
-			{
-				"id": "mapList"
-			},
-			createHTML("div", {"class": "loadSpinner"})
-		)),
-		document.body.appendChild(createHTML(
-			"div",
-			{
-				"id": "mapView"
-			},
-			createHTML("div", {"class": "loadSpinner"})
-		))
-	));
+	const overlay = layers(document.body.appendChild(createHTML("div", {"id": "overlay"})), createHTML("div", {"class": "loadSpinner"}));
+	Promise.all([
+		include("assets.js"),
+		include("maplist.js")
+	]).then(([assetFn, mapListFn]) => {
+		assetFn(rpc, overlay, document.body.appendChild(createHTML("div", createHTML("h2", {"id": "assetLoading"}, ["Loading...", createHTML("div", {"class": "loadSpinner"})]))));
+		mapListFn(rpc, overlay, document.body.appendChild(createHTML("div", createHTML("h2", {"id": "mapListLoading"}, ["Loading...", createHTML("div", {"class": "loadSpinner"})]))), mapLoadPipe.send);
+	}, alert);
 });
