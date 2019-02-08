5be547220a7382215df02de42e04210a8e685fa8
Michael Woolnough <michael.woolnough@gmail.com>
2019-02-08 13:45:40 +0000

    restructured main

--- main.go
+++ main.go
@@ -16,18 +16,14 @@ import (
 
 var dir http.FileSystem = httpdir.Default
 
-func e(err error, reason string) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", reason, err)
-		os.Exit(1)
-	}
-}
-
 var dataDir = flag.String("data", "./data", "directory containing all battlemap data")
 
 func main() {
 	flag.Parse()
-	e(Config.Init(*dataDir), "error loading config")
+	if err := Config.Init(*dataDir); err != nil {
+		fmt.Fprintf(os.Stderr, "error loading Config: %s\n", err)
+		os.Exit(1)
+	}
 
 	var port keystore.Uint16
 	Config.Get("port", &port)
@@ -38,35 +34,34 @@ func main() {
 		Handler: mux,
 	}
 
-	e(Socket.Init(), "error initialising Socket module")
-	http.Handle("/socket", websocket.Handler(Socket.ServeConn))
+	for module, init := range map[string]interface{ Init() error }{
+		"Socket":  &Socket,
+		"Auth":    &Auth,
+		"Assets":  &AssetsDir,
+		"Tokens":  &TokensDir,
+		"Chars":   &CharsDir,
+		"Maps":    &MapsDir,
+		"Masks":   &MasksDir,
+		"Files":   &FilesDir,
+		"Plugins": &PluginsDir,
+	} {
+		if err := init.Init(); err != nil {
+			fmt.Fprintf(os.Stderr, "error initialising %s module: %s\n", module, err)
+			os.Exit(1)
+		}
+	}
 
-	e(Auth.Init(), "error initialising Auth module")
+	mux.Handle("/socket", websocket.Handler(Socket.ServeConn))
 	mux.HandleFunc("/login/update", Auth.UpdatePassword)
 	mux.HandleFunc("/login/logout", Auth.Logout)
 	mux.HandleFunc("/login/login", Auth.Login)
-
-	e(AssetsDir.Init(), "error initialising Assets module")
 	mux.Handle("/assets/", http.StripPrefix("/assets/", Dir{&AssetsDir}))
-
-	e(TokensDir.Init(), "error initialising Tokens module")
 	mux.Handle("/tokens/", http.StripPrefix("/tokens", Dir{&TokensDir}))
-
-	e(CharsDir.Init(), "error initialising Characters module")
 	mux.Handle("/characters/", http.StripPrefix("/characters/", Dir{&CharsDir}))
-
-	e(MapsDir.Init(), "error initialising Maps module")
 	mux.Handle("/maps/", http.StripPrefix("/maps/", Dir{&MapsDir}))
-
-	e(MasksDir.Init(), "error initialising Masks module")
 	mux.Handle("/masks/", http.StripPrefix("/masks/", Dir{&MasksDir}))
-
-	e(FilesDir.Init(), "error initialising Files module")
 	mux.Handle("/files/", http.StripPrefix("/files/", Dir{&FilesDir}))
-
-	e(PluginsDir.Init(), "error initialising plugins")
 	mux.Handle("/plugins/", http.StripPrefix("/plugins/", Dir{&PluginsDir}))
-
 	mux.Handle("/", httpgzip.FileServer(dir))
 
 	c := make(chan os.Signal, 1)
@@ -81,5 +76,8 @@ func main() {
 	}()
 
 	log.Println("Running...")
-	e(srv.ListenAndServe(), "fatal error")
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Fprintf(os.Stderr, "fatal error: %s\n", err)
+		os.Exit(1)
+	}
 }
