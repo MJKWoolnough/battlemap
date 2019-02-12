ddd11997d896e499f5b3a2272ba2b1823e00d7a0
Michael Woolnough <michael.woolnough@gmail.com>
2019-02-12 10:44:59 +0000

    added TestMain

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+var srv *httptest.Server
+
+func TestMain(m *testing.M) {
+	dataDir, err := ioutil.TempDir("", "battlemap-test")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating temp dir: %s", err)
+		os.Exit(1)
+	}
+	r := testMain(m, dataDir)
+	os.RemoveAll(dataDir)
+	os.Exit(r)
+}
+
+func testMain(m *testing.M, dataDir string) int {
+	if err := initModules(dataDir); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	srv = httptest.NewServer(initMux())
+	return m.Run()
+}
