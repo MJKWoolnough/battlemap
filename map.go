31927c2200f2c17b4206d64de705486395677ca4
Michael Woolnough <michael.woolnough@gmail.com>
2019-02-22 11:10:39 +0000

    added Mask and missing fields for Token

--- map.go
+++ map.go
@@ -10,18 +10,27 @@ import (
 var svgDoctype = xml.Directive("DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"")
 
 type MapX struct {
-	Width    uint       `xml:"width,attr"`
-	Height   uint       `xml:"height,attr"`
-	Patterns []Patterns `json:"defs>pattern"`
-	Layers   []Layer    `xml:"g"`
+	ID       uint64    `xml:"id,attr"`
+	Name     string    `xml:"name,attr"`
+	Order    uint64    `xml:"order,attr"`
+	Width    uint64    `xml:"width,attr"`
+	Height   uint64    `xml:"height,attr"`
+	Patterns []Pattern `json:"defs>pattern"`
+	Masks    []Mask    `json:"defs>mask"`
+	Layers   []Layer   `xml:"g"`
 }
 
 type PatternX struct {
 	ID     string `xml:"id,attr"`
-	Width  uint   `xml:"width,attr"`
-	Height uint   `xml:"height,attr"`
+	Width  uint64 `xml:"width,attr"`
+	Height uint64 `xml:"height,attr"`
 	Image  *Token `xml:"image,omitempty,any"`
 	Path   *Path  `xml:"path,omitempty"`
+}
+
+type Mask struct {
+	ID    string `xml:"id,attr"`
+	Image Token  `xml:"image"`
 }
 
 type Path struct {
@@ -34,17 +43,21 @@ type Path struct {
 type Layer struct {
 	ID     string  `xml:"id,attr"`
 	Name   string  `xml:"name,attr"`
+	Mask   string  `xml:"mask,attr,omitempty"`
 	Tokens []Token `xml:",any"`
 }
 
 type Token struct {
-	Source     string
-	X          int
-	Y          int
-	Width      uint
-	Height     uint
-	Rotation   uint16
-	Flip, Flop bool
+	Source      string
+	Stroke      string
+	StrokeWidth uint64
+	ID          uint64
+	X           int64
+	Y           int64
+	Width       uint64
+	Height      uint64
+	Rotation    uint8
+	Flip, Flop  bool
 	tokenType
 }
 
