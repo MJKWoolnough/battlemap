734dcacf0205cbfeadb0e118d99c76439bdb68a7
Michael Woolnough <michael.woolnough@gmail.com>
2019-02-23 12:10:47 +0000

    completed maps dir init

--- dir_maps.go
+++ dir_maps.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 
 	"vimagination.zapto.org/errors"
 	"vimagination.zapto.org/keystore"
@@ -13,6 +17,11 @@ type mapsDir struct {
 	DefaultMethods
 
 	store *keystore.FileStore
+
+	mu      sync.RWMutex
+	maps    Maps
+	nextID  uint64
+	handler http.Handler
 }
 
 func (m *mapsDir) Init() error {
@@ -26,21 +35,78 @@ func (m *mapsDir) Init() error {
 	if err != nil {
 		return errors.WithContext("error creating map store: ", err)
 	}
+	m.maps = make(Maps)
+	stat, err := m.store.Stat("")
+	if err != nil {
+		return errors.WithContext("error getting map directory stat: ", err)
+	}
+	t := stat.ModTime()
+	for _, key := range m.store.Keys() {
+		id, err := strconv.ParseUint(key, 10, 0)
+		if err != nil {
+			continue
+		}
+		if _, ok := m.maps[id]; ok {
+			continue
+		}
+		stat, err = m.store.Stat(key)
+		if err != nil {
+			return errors.WithContext("error getting map stat: ", err)
+		}
+		mt := stat.ModTime()
+		if t.Before(mt) {
+			t = mt
+		}
+		mp := new(Map)
+		if err = m.store.Get(key, mp); err != nil {
+			return errors.WithContext(fmt.Sprintf("error reading map data (%q): ", key), err)
+		}
+		if id != mp.ID {
+			return MapIDError{id, mp.ID}
+		}
+		m.maps[id] = mp
+	}
+	genPages(t, m.maps, mapsTemplate, "index", "maps", "map", &m.handler)
 	return nil
 }
+
+var mapsTemplate = template.Must(template.New("").Parse(`<!DOCTYPE html>
+<html>
+	<head>
+		<title>Maps</title>
+	</head>
+	<body>
+		<table>
+			<thead>
+				<tr><th>ID</th><th>Order</th><th>Name</th></tr>
+			</thead>
+			<tbody>
+{{range .}}				<tr><td>{{.ID}}</td><td>{{.Order}}</td><td>{{.Name}}</td></tr>
+{{end}}			</tbody>
+		</table>
+	</body>
+</html>`))
 
 func (m *mapsDir) Options(w http.ResponseWriter, r *http.Request) {
 
 }
 
-type CurrentMap uint
+type CurrentMap uint64
 
 func (c CurrentMap) RPC(method string, data []byte) (interface{}, error) {
 	switch strings.TrimPrefix(method, "maps.") {
 	case "getCurrentMap":
-		return uint(c), nil
+		return uint64(c), nil
 	}
 	return nil, ErrUnknownMethod
 }
 
 var MapsDir mapsDir
+
+type MapIDError struct {
+	KeyID, ParsedID uint64
+}
+
+func (m MapIDError) Error() string {
+	return fmt.Sprintf("Key ID and Parsed ID do not match: %d, %d", m.KeyID, m.ParsedID)
+}
