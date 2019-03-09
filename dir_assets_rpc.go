fe4af4d879cc1947e7324cb67cd2d44f44b166f0
Michael Woolnough <michael.woolnough@gmail.com>
2019-03-09 18:27:47 +0000

    added broadcasters for RPC methods

--- dir_assets_rpc.go
+++ dir_assets_rpc.go
@@ -16,21 +16,21 @@ func (a *assetsDir) RPC(cd ConnData, method string, data []byte) (interface{}, e
 	if cd.IsAdmin {
 		switch strings.TrimPrefix(method, "assets.") {
 		case "deleteAsset":
-			return a.rpcDeleteAsset(data)
+			return a.rpcDeleteAsset(data, cd)
 		case "renameAsset":
-			return a.rpcRenameAsset(data)
+			return a.rpcRenameAsset(data, cd)
 		case "addTagsToAsset":
-			return a.rpcAddTagsToAsset(data)
+			return a.rpcAddTagsToAsset(data, cd)
 		case "removeTagsFromAsset":
-			return a.rpcRemoveTagsFromAsset(data)
+			return a.rpcRemoveTagsFromAsset(data, cd)
 		case "getAssets":
 			return a.rpcGetAssets(data)
 		case "addTag":
-			return a.rpcAddTag(data)
+			return a.rpcAddTag(data, cd)
 		case "deleteTag":
-			return a.rpcDeleteTag(data)
+			return a.rpcDeleteTag(data, cd)
 		case "renameTag":
-			return a.rpcRenameTag(data)
+			return a.rpcRenameTag(data, cd)
 		case "getTags":
 			return a.rpcGetTags(data)
 		}
@@ -38,7 +38,7 @@ func (a *assetsDir) RPC(cd ConnData, method string, data []byte) (interface{}, e
 	return nil, ErrUnknownMethod
 }
 
-func (a *assetsDir) rpcDeleteAsset(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcDeleteAsset(data []byte, cd ConnData) (interface{}, error) {
 	var id uint64
 	if err := json.Unmarshal(data, &id); err != nil {
 		return nil, err
@@ -54,14 +54,15 @@ func (a *assetsDir) rpcDeleteAsset(data []byte) (interface{}, error) {
 				a.genAssetsHandler(fs.ModTime())
 			}
 		}
+		Socket.BroadcastAssetRemove(id, cd.ID)
 	} else {
 		err = ErrUnknownAsset
 	}
-	a.assetMu.Lock()
+	a.assetMu.Unlock()
 	return id, err
 }
 
-func (a *assetsDir) rpcRenameAsset(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcRenameAsset(data []byte, cd ConnData) (interface{}, error) {
 	var idName struct {
 		ID   uint64 `json:"id"`
 		Name string `json:"name"`
@@ -74,6 +75,7 @@ func (a *assetsDir) rpcRenameAsset(data []byte) (interface{}, error) {
 	if ok {
 		if a.renameAsset(asset, idName.Name) {
 			a.writeAsset(idName.ID, true)
+			Socket.BroadcastAssetChange(asset, cd.ID)
 		}
 	}
 	newName := asset.Name
@@ -81,11 +83,10 @@ func (a *assetsDir) rpcRenameAsset(data []byte) (interface{}, error) {
 	if !ok {
 		return nil, ErrUnknownAsset
 	}
-
 	return newName, nil
 }
 
-func (a *assetsDir) rpcAddTagsToAsset(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcAddTagsToAsset(data []byte, cd ConnData) (interface{}, error) {
 	var idTags struct {
 		ID   uint64   `json:"id"`
 		Tags []uint64 `json:"tags"`
@@ -101,6 +102,7 @@ func (a *assetsDir) rpcAddTagsToAsset(data []byte) (interface{}, error) {
 		change = a.addTagsToAsset(asset, idTags.Tags...)
 		if change {
 			a.writeAsset(idTags.ID, true)
+			Socket.BroadcastAssetChange(asset, cd.ID)
 		}
 	}
 	a.tagMu.Unlock()
@@ -111,7 +113,7 @@ func (a *assetsDir) rpcAddTagsToAsset(data []byte) (interface{}, error) {
 	return change, nil
 }
 
-func (a *assetsDir) rpcRemoveTagsFromAsset(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcRemoveTagsFromAsset(data []byte, cd ConnData) (interface{}, error) {
 	var idTags struct {
 		ID   uint64   `json:"id"`
 		Tags []uint64 `json:"tags"`
@@ -127,6 +129,7 @@ func (a *assetsDir) rpcRemoveTagsFromAsset(data []byte) (interface{}, error) {
 		change = a.removeTagsFromAsset(asset, idTags.Tags...)
 		if change {
 			a.writeAsset(idTags.ID, true)
+			Socket.BroadcastAssetChange(asset, cd.ID)
 		}
 	}
 	a.tagMu.Unlock()
@@ -144,7 +147,7 @@ func (a *assetsDir) rpcGetAssets(_ []byte) (interface{}, error) {
 	return ret, nil
 }
 
-func (a *assetsDir) rpcAddTag(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcAddTag(data []byte, cd ConnData) (interface{}, error) {
 	var tagName string
 	if err := json.Unmarshal(data, &tagName); err != nil {
 		return nil, err
@@ -153,10 +156,11 @@ func (a *assetsDir) rpcAddTag(data []byte) (interface{}, error) {
 	tag := a.addTag(tagName)
 	a.writeTags()
 	a.tagMu.Unlock()
+	Socket.BroadcastTagAdda(Tags{tag.ID: tag}, cd.ID)
 	return tag.ID, nil
 }
 
-func (a *assetsDir) rpcDeleteTag(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcDeleteTag(data []byte, cd ConnData) (interface{}, error) {
 	var id uint64
 	err := json.Unmarshal(data, &id)
 	if err != nil {
@@ -166,7 +170,9 @@ func (a *assetsDir) rpcDeleteTag(data []byte) (interface{}, error) {
 	a.tagMu.Lock()
 	tag, ok := a.tags[id]
 	if ok {
-		a.deleteTags(tag)
+		if a.deleteTags(tag) {
+			Socket.BroadcastTagRemove([]uint64{id}, cd.ID)
+		}
 		a.assetMu.Unlock()
 		a.writeTags()
 	} else {
@@ -179,7 +185,7 @@ func (a *assetsDir) rpcDeleteTag(data []byte) (interface{}, error) {
 	return id, nil
 }
 
-func (a *assetsDir) rpcRenameTag(data []byte) (interface{}, error) {
+func (a *assetsDir) rpcRenameTag(data []byte, cd ConnData) (interface{}, error) {
 	var idName struct {
 		ID   uint64 `json:"id"`
 		Name string `json:"name"`
@@ -192,6 +198,7 @@ func (a *assetsDir) rpcRenameTag(data []byte) (interface{}, error) {
 	if ok {
 		if a.renameTag(tag, idName.Name) {
 			a.writeTags()
+			Socket.BroadcastTagAdd(Tags{tag.ID: tag}, cd.ID)
 		}
 	}
 	newName := tag.Name
