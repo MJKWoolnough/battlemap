c6f04084e667629bf1af95c71eb69590bd18ba05
Michael Woolnough <michael.woolnough@gmail.com>
2019-12-02 14:47:27 +0000

    showError can take a string or an Error

--- internal/static/misc.ts
+++ internal/static/misc.ts
@@ -4,15 +4,16 @@ export const enterKey = function(this: Node, e: KeyboardEvent): void {
 	if (e.keyCode === 13) {
 		(this.nextSibling! as HTMLElement).click();
 	}
-}, showError = (elm: Node, e: Error): void => {
+}, showError = (elm: Node, e: Error | string): void => {
+	const msg =  e instanceof Error ? e.message : e;
 	if (elm.nextSibling !== null) {
 		if ((elm.nextSibling as HTMLElement).classList.contains("error")) {
-			elm.nextSibling.textContent = e.message;
+			elm.nextSibling.textContent = msg;
 		} else if (elm.parentNode !== null) {
-			elm.parentNode.insertBefore(createHTML("span", {"class": "error"}, e.message), elm.nextSibling);
+			elm.parentNode.insertBefore(createHTML("span", {"class": "error"}, msg), elm.nextSibling);
 		}
 	} else if (elm.parentNode !== null) {
-		elm.parentNode.appendChild(createHTML("span", {"class": "error"}, e.message));
+		elm.parentNode.appendChild(createHTML("span", {"class": "error"}, msg));
 	}
 }, clearError = (elm: Node): void => {
 	if (elm.parentNode !== null && elm.nextSibling !== null && (elm.nextSibling as HTMLElement).classList.contains("error")) {
