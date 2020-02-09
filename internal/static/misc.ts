454d0a96c706ce122670ff74484ebe8bf926ad4c
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-09 11:53:14 +0000

    added colour conversion utils funcs

--- internal/static/misc.ts
+++ internal/static/misc.ts
@@ -1,4 +1,5 @@
 import {createHTML} from './lib/html.js';
+import {Colour} from './types.js';
 
 export const enterKey = function(this: Node, e: KeyboardEvent): void {
 	if (e.keyCode === 13) {
@@ -24,4 +25,5 @@ export const enterKey = function(this: Node, e: KeyboardEvent): void {
 	if (elm.parentNode !== null && elm.nextSibling !== null && (elm.nextSibling as HTMLElement).classList.contains("error")) {
 		elm.parentNode.removeChild(elm.nextSibling);
 	}
-};
+}, hex2Colour = (hex: string): Colour => ({"r": parseInt(hex.slice(1, 3), 16), "g": parseInt(hex.slice(3, 5), 16), "b": parseInt(hex.slice(5, 7), 16), "a": 1}),
+colour2Hex = (c: Colour) => `#${c.r.toString(16).padStart(2, "0")}${c.g.toString(16).padStart(2, "0")}${c.b.toString(16).padStart(2, "0")}`;
