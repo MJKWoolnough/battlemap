f26030508fa0dd4e1956b023faf9d8d0368e2fba
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-11 18:15:07 +0000

    updated to match new map structure

--- internal/static/map.ts
+++ internal/static/map.ts
@@ -5,7 +5,7 @@ import {showError, enterKey} from './misc.js';
 export default function(rpc: RPC, overlay: LayerType, base: Node,  mapSelect: (fn: (mapID: Int) => void) => void, setLayers: (layers: MapLayer[]) => void) {
 	mapSelect(mapID => HTTPRequest(`/maps/${mapID}`, {"response": "document"}).then(mapData => {
 		const root = (mapData as Document).getElementsByTagName("svg")[0];
-		setLayers(Array.from(root.childNodes).filter(e => e.nodeName === "g").map(e => {
+		setLayers(Array.from((root.lastChild as Node).childNodes).filter(e => e.nodeName === "g").map(e => {
 			const g = e as SVGGElement,
 			      tid = g.getAttribute("id") as string,
 			      id = tid === "Light" ? -1 : tid === "Grid" ? -2 : parseInt(tid.slice(6));
