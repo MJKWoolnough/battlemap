c5ec7dcc9aed2554ba2e9dbbc1a5f1aa1c012b7c
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-12 20:29:26 +0000

    basic structure and showing list for layers

--- internal/static/layerList.ts
+++ internal/static/layerList.ts
@@ -4,9 +4,44 @@ import {br, button, div, h1, input, label, li, span, ul} from './lib/dom.js';
 import {showError, enterKey} from './misc.js';
 import {sortHTML, noSort} from './lib/ordered.js';
 
+class LayerList {
+	list = sortHTML<LayerItem>(ul(), noSort);
+	rpc: RPC;
+	overlay: LayerType;
+	constructor(rpc: RPC, overlay: LayerType) {
+		this.rpc = rpc;
+		this.overlay = overlay;
+	}
+	addLayer(l: MapLayer) {
+		this.list.push(new LayerItem(l));
+	}
+	get html() {
+		return this.list.html;
+	}
+}
+
+class LayerItem {
+	html: HTMLElement;
+	id: string;
+	name: string;
+	constructor(l: MapLayer) {
+		this.id = l.id;
+		this.name = l.name;
+		this.html = li(l.name);
+	}
+}
+
+
 export default function(rpc: RPC, overlay: LayerType, base: Node, mapChange: (fn: (layers: MapLayer[]) => void) => void) {
-	clearElement(base).appendChild(h1("No Map Selected"));
+	base.appendChild(h1("No Map Selected"));
 	mapChange((layers: MapLayer[]) => {
-		console.log(layers);
+		const list = new LayerList(rpc, overlay);
+		layers.forEach(l => list.addLayer(l));
+		createHTML(clearElement(base), {"id": "layerList"}, [
+			button("Add Layer", {"onclick": () => {
+
+			}}),
+			list.html
+		]);
 	});
 }
