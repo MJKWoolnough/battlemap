a652d7e5d8504e6dbd1d8add19f4ab2fe4f82116
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-12 19:40:35 +0000

    removed use of SortHTMLType

--- internal/static/assets.ts
+++ internal/static/assets.ts
@@ -3,7 +3,7 @@ import {createHTML, clearElement} from './lib/html.js';
 import {audio, br, button, div, form, h1, img, input, label, li, option, progress, span, select, ul} from './lib/dom.js';
 import {HTTPRequest} from './lib/conn.js';
 import {showError, enterKey} from './misc.js';
-import {sortHTML, stringSort, SortHTMLType} from './lib/ordered.js';
+import {sortHTML, stringSort} from './lib/ordered.js';
 
 const stringSorter = (a: Asset | AssetFolder, b: Asset | AssetFolder) => stringSort(a.name, b.name),
       idSorter = (a: Asset, b: Asset) => b.id - a.id,
@@ -105,13 +105,11 @@ class AssetFolder {
 	parent: AssetFolder;
 	name: string;
 	html: Node;
-	folders: SortHTMLType<AssetFolder>;
-	assets: SortHTMLType<Asset>;
+	folders = sortHTML<AssetFolder>(ul(), stringSorter);
+	assets = sortHTML<Asset>(ul(), stringSorter);
 	constructor(parent: AssetFolder, name: string, folders: Record<string, Folder>, assets: Record<string, Int>) {
 		this.parent = parent;
 		this.name = name;
-		this.folders = sortHTML(ul(), stringSorter);
-		this.assets = sortHTML(ul(), stringSorter);
 		const self = this;
 		this.html = li([
 			input({"type": "checkbox", "class": "expander", "id": `folder_${folderID}`}),
--- internal/static/layerList.ts
+++ internal/static/layerList.ts
@@ -2,11 +2,11 @@ import {Int, RPC, LayerType, MapLayer} from './types.js';
 import {createHTML, clearElement} from './lib/html.js';
 import {br, button, div, h1, input, label, li, span, ul} from './lib/dom.js';
 import {showError, enterKey} from './misc.js';
-import {sortHTML} from './lib/ordered.js';
+import {sortHTML, noSort} from './lib/ordered.js';
 
 export default function(rpc: RPC, overlay: LayerType, base: Node, mapChange: (fn: (layers: MapLayer[]) => void) => void) {
 	clearElement(base).appendChild(h1("No Map Selected"));
 	mapChange((layers: MapLayer[]) => {
-
+		console.log(layers);
 	});
 }
