c1e5c6291869c775c56eb2e6d8c64b485344ecec
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-13 18:18:27 +0000

    added cache bypassing GET param for map loading

--- internal/static/map.ts
+++ internal/static/map.ts
@@ -3,7 +3,7 @@ import {HTTPRequest} from './lib/conn.js';
 import {showError, enterKey} from './misc.js';
 
 export default function(rpc: RPC, overlay: LayerType, base: Node,  mapSelect: (fn: (mapID: Int) => void) => void, setLayers: (layers: MapLayer[]) => void) {
-	mapSelect(mapID => HTTPRequest(`/maps/${mapID}`, {"response": "document"}).then(mapData => {
+	mapSelect(mapID => HTTPRequest(`/maps/${mapID}?d=${Date.now()}`, {"response": "document"}).then(mapData => {
 		const root = (mapData as Document).getElementsByTagName("svg")[0];
 		setLayers(Array.from((root.lastChild as Node).childNodes).filter(e => e.nodeName === "g").map(e => {
 			const g = e as SVGGElement;
