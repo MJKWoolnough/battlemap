83754169ddf3ed47fc0793c65ceefece66938a3b
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-29 16:30:26 +0000

    reworked how IDs are to work for maps

--- internal/static/map.ts
+++ internal/static/map.ts
@@ -4,10 +4,25 @@ import {showError, enterKey} from './misc.js';
 
 export default function(rpc: RPC, overlay: LayerType, base: Node,  mapSelect: (fn: (mapID: Int) => void) => void, setLayers: (layers: LayerFolder) => void) {
 	mapSelect(mapID => HTTPRequest(`/maps/${mapID}?d=${Date.now()}`, {"response": "document"}).then(mapData => {
-		const root = (mapData as Document).getElementsByTagName("svg")[0];
-		setLayers({"id": "", "name": "", "hidden": false,  "folders": {}, "items": {}, "children": Array.from((root.lastChild!).childNodes).filter(e => e.nodeName === "g").map(e => {
-			const g = e as SVGGElement;
-			return {"id": g.getAttribute("id")!, "name": g.getAttribute("data-name")!, "hidden": g.getAttribute("visibility") === "hidden", "mask": parseInt(g.getAttribute("mask")!), folders: {}, items: {}, children: []};
+		const root = (mapData as Document).getElementsByTagName("svg")[0],
+		      layers = new Map<Int, string>();
+		let layerNum = 0;
+		setLayers({"id": 0, "name": "", "hidden": false,  "folders": {}, "items": {}, "children": Array.from((root.lastChild!).childNodes).filter(e => e.nodeName === "g").map(e => {
+			const g = e as SVGGElement,
+			      idStr = g.getAttribute("id")!;
+			let id: Int;
+			switch (idStr) {
+			case "Grid":
+				id = -1;
+				break;
+			case "Light":
+				id = -2;
+				break;
+			default:
+				id = ++layerNum;
+			}
+			layers.set(id, g.getAttribute("id")!);
+			return {"id": id, "name": g.getAttribute("data-name")!, "hidden": g.getAttribute("visibility") === "hidden", "mask": parseInt(g.getAttribute("mask")!), folders: {}, items: {}, children: []};
 		})});
 		base.appendChild(root);
 	}));
