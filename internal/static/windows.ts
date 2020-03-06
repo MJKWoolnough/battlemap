abae2c561e916b54a2fdd90332c08ddffe40587c
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-06 17:06:51 +0000

    added an extension to new Window type to handle loading dialog windows

---
+++ internal/static/windows.ts
@@ -0,0 +1,12 @@
+import {createHTML} from './lib/html.js';
+import {div} from './lib/dom.js';
+import {Shell as WindowsShell} from './lib/windows.js';
+
+export class Shell extends WindowsShell {
+	addLoading(w: HTMLDivElement | null, p: Promise<any>, title = "Loading", content?: HTMLElement) {
+		return p.finally(this.removeWindow.bind(this, createHTML(this.addDialog(w, {
+			"showTitlebar": true,
+			"title": title
+		}), content ? content : div({"class": "loadSpinner"}))));
+	}
+}
