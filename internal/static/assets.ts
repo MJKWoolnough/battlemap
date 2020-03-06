ac8c914414b5367a5e8163f160a25e21ba75c4ec
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-06 17:07:13 +0000

    replaced overlays with Windows

--- internal/static/assets.ts
+++ internal/static/assets.ts
@@ -1,15 +1,15 @@
-import {Int, RPC, LayerType} from './types.js';
+import {Int, RPC} from './types.js';
 import {createHTML, clearElement} from './lib/html.js';
 import {audio, button, div, form, h1, img, input, label, progress} from './lib/dom.js';
 import {HTTPRequest} from './lib/conn.js';
+import {Shell} from './windows.js';
 import {showError} from './misc.js';
-import {Root, Item} from './folders.js';
+import {Root, Item, windowOptions} from './folders.js';
 
 class Asset extends Item {
 	show() {
-		const root = this.parent.root,
-		      overlay = root.overlay;
-		return createHTML(root.overlay.addLayer(), {"class": "showAsset"}, [
+		const root = this.parent.root;
+		return createHTML(root.shell.addWindow(this.name, windowOptions), {"class": "showAsset"}, [
 			h1(this.name),
 			root.fileType === "Images" ? [
 				img({"src": `/images/${this.id}`})
@@ -20,18 +20,18 @@ class Asset extends Item {
 	}
 }
 
-export default function (rpc: RPC, overlay: LayerType, base: Node, fileType: "Images" | "Audio") {
+export default function (rpc: RPC, shell: Shell, base: Node, fileType: "Images" | "Audio") {
 	const rpcFuncs = fileType == "Audio" ? rpc["audio"] : rpc["images"];
 	rpcFuncs.list().then(folderList => {
-		const root = new Root(folderList, fileType, rpcFuncs, overlay, Asset);
+		const root = new Root(folderList, fileType, rpcFuncs, shell, Asset);
 		createHTML(clearElement(base), {"id": fileType + "Items", "class": "folders"}, [
-			button(`Upload ${fileType}`, {"onclick": () => createHTML(overlay.addLayer(), {"class": "assetAdd"}, [
+			button(`Upload ${fileType}`, {"onclick": () => createHTML(shell.addWindow(`Upload ${fileType}`, windowOptions), {"class": "assetAdd"}, [
 				h1(`Upload ${fileType}`),
 				form({"enctype": "multipart/form-data", "method": "post"}, [
 					label({"for": "addAssets"}, "Add Asset(s)"),
-					input({"accept": fileType === "Images" ? "image/gif, image/png, image/jpeg, image/webp" : "application/ogg, audio/mpeg", "id": "addAssets", "multiple": "multiple", "name": "asset", "type": "file", "onchange": function(this: Node) {
+					input({"accept": fileType === "Images" ? "image/gif, image/png, image/jpeg, image/webp" : "application/ogg, audio/mpeg", "id": "addAssets", "multiple": "multiple", "name": "asset", "type": "file", "onchange": function(this: HTMLInputElement) {
 						const bar = progress({"style": "width: 100%"}) as HTMLElement;
-						overlay.loading(HTTPRequest(`/${fileType.toLowerCase()}/`, {
+						shell.addLoading(this.parentNode!.parentNode as HTMLDivElement, HTTPRequest(`/${fileType.toLowerCase()}/`, {
 							"data": new FormData(this.parentNode as HTMLFormElement),
 							"method": "POST",
 							"response": "json",
@@ -42,12 +42,12 @@ export default function (rpc: RPC, overlay: LayerType, base: Node, fileType: "Im
 									bar.textContent = Math.floor(e.loaded*100/e.total) + "%";
 								}
 							}
-						}), div({"class": "loadBar"}, [
+						}), "Uploading", div({"class": "loadBar"}, [
 							div("Uploading file(s)"),
 							bar
 						])).then((assets: Record<string, Int>) => {
 							Object.entries(assets).forEach(([name, id]) => root.addItem(id, name))
-							overlay.removeLayer();
+							shell.removeWindow(this.parentNode!.parentNode as HTMLDivElement);
 						}, showError.bind(null, this));
 					}})
 				])
