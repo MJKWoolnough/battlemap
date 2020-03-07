37afb04cb630528a32a85429f0b537c274878607
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-07 10:27:37 +0000

    moved default window placement to css

--- internal/static/windows.ts
+++ internal/static/windows.ts
@@ -3,19 +3,6 @@ import {div} from './lib/dom.js';
 import {Shell as WindowsShell, WindowOptions} from './lib/windows.js';
 
 export class Shell extends WindowsShell {
-	addWindow(title: string, options?: WindowOptions) {
-		const w = super.addWindow(title, options),
-		      p = w.parentNode as HTMLElement;
-		if (options && options.resizeable) {
-			p.style.setProperty("--window-left", "20px");
-			p.style.setProperty("--window-top", "20px");
-		} else {
-			p.style.setProperty("--window-left", "50%");
-			p.style.setProperty("--window-top", "50%");
-			p.style.setProperty("transform", "translateX(-50%) translateY(-50%)");
-		}
-		return w;
-	}
 	addLoading(w: HTMLDivElement | null, p: Promise<any>, title = "Loading", content?: HTMLElement) {
 		return p.finally(this.removeWindow.bind(this, createHTML(this.addDialog(w, {
 			"showTitlebar": true,
