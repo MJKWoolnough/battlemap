7995040fa43d5226dce73ce98c86005345245fb3
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-10 18:48:36 +0000

    restructured to allow passing params instead of setting them afterwards

--- internal/static/layerList.ts
+++ internal/static/layerList.ts
@@ -7,29 +7,28 @@ import {Root, Folder, Item, windowOptions} from './folders.js';
 import {Shell} from './windows.js';
 
 class ItemLayer extends Item {
-	hidden: boolean = false;
-	mask: Int = 0;
+	hidden: boolean;
+	mask: Int;
+	constructor(parent: Folder, id: Int, name: string, hidden = false, mask: Int = 0) {
+		super(parent, id, name);
+		this.hidden = hidden;
+		this.mask = mask;
+	}
+}
+
+function isLayer(c: Layer | LayerFolder): c is Layer {
+	return (c as Layer).mask !== undefined;
 }
 
 class FolderLayer extends Folder {
 	id: Int;
-	hidden: boolean = false;
-	constructor(root: Root, parent: Folder | null, name: string, children: FolderItems) {
+	hidden: boolean;
+	constructor(root: Root, parent: Folder | null, name: string, children: FolderItems, hidden = false) {
 		super(root, parent, name, {folders: {}, items: {}});
+		this.hidden = hidden;
 		const lf = children as LayerFolder;
 		this.id = lf.id;
-		lf.children.forEach(c => {
-			if ((c as Layer).mask !== undefined) {
-				const i = new ItemLayer(this, c.id, c.name);
-				this.children.push(i);
-				i.hidden = c.hidden;
-				i.mask = (c as Layer).mask;
-			} else {
-				const f = new FolderLayer(root, this, c.name, c as LayerFolder);
-				this.children.push(f);
-				f.hidden = c.hidden;
-			}
-		});
+		lf.children.forEach(c => this.children.push(isLayer(c) ? new ItemLayer(this, c.id, c.name, c.hidden, c.mask) : new FolderLayer(root, this, c.name, c as LayerFolder, c.hidden)));
 	}
 	get sorter() {
 		return noSort;
