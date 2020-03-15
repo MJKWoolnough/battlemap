750a18aeb6f6066ca43ac7b1dd6b4be7d4de7a20
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-15 11:05:05 +0000

    added escape-close to dialogs as well

--- internal/static/windows.ts
+++ internal/static/windows.ts
@@ -1,6 +1,22 @@
 import {createHTML} from './lib/html.js';
 import {div} from './lib/dom.js';
-import {Shell as WindowsShell, WindowOptions} from './lib/windows.js';
+import {Shell as WindowsShell, WindowOptions, DialogOptions} from './lib/windows.js';
+
+type hasClose = {
+	showClose?: boolean;
+}
+
+const addEscapeClose = (shell: WindowsShell, window: HTMLDivElement, options?: hasClose) => {
+	if (options && options.showClose) {
+		window.setAttribute("tabindex", "-1");
+		window.addEventListener("keyup", (e: KeyboardEvent) => {
+			if (e.keyCode === 27) {
+				shell.removeWindow(window);
+			}
+		});
+	}
+	return window;
+};
 
 export class Shell extends WindowsShell {
 	addLoading(w: HTMLDivElement | null, p: Promise<any>, title = "Loading", content?: HTMLElement) {
@@ -9,16 +25,10 @@ export class Shell extends WindowsShell {
 			"title": title
 		}), content ? content : div({"class": "loadSpinner"}))));
 	}
-	addWindow(title: string, options?: WindowOptions) {
-		const window = super.addWindow(title, options);
-		if (options && options.showClose) {
-			window.setAttribute("tabindex", "-1");
-			window.addEventListener("keyup", (e: KeyboardEvent) => {
-				if (e.keyCode === 27) {
-					this.removeWindow(window);
-				}
-			});
-		}
-		return window;
+	addWindow(title: string, options?: WindowOptions): HTMLDivElement {
+		return addEscapeClose(this, super.addWindow(title, options), options);
+	}
+	addDialog(parent: HTMLDivElement | null, options?: DialogOptions): HTMLDivElement {
+		return addEscapeClose(this, super.addDialog(parent, options), options);
 	}
 }
