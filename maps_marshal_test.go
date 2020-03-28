028164603372910216521f940c7dcbd169663a64
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-28 09:58:27 +0000

    added layer tests

--- maps_marshal_test.go
+++ maps_marshal_test.go
@@ -15,11 +15,11 @@ func TestMapsMarshal(t *testing.T) {
 		Output levelMap
 		Err    error
 	}{
-		{
+		{ // 1
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg>",
 			Err:   ErrInvalidMapDimensions,
 		},
-		{
+		{ // 2
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"100\" height=\"200\" data-initiative=\"0|1,2|3,3|4\" data-grid-pos=\"1\" data-grid-hidden=\"true\" data-light-pos=\"2\" data-light-hidden=\"false\" data-light-colour=\"rgba(3, 2, 1, 0.498)\"><defs></defs></svg>",
 			Output: levelMap{
 				Width:       100,
@@ -34,31 +34,31 @@ func TestMapsMarshal(t *testing.T) {
 				Masks:       map[string]*mask{},
 			},
 		},
-		{
+		{ // 3
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" width=\"1\" height=\"2\"><path d=\"M 0 200 V 0 H 100\" fill=\"rgba(255, 0, 0, 1.000)\" stroke=\"rgba(0, 0, 0, 1.000)\" stroke-width=\"1\"></path></pattern></defs></svg>",
 			Err:   ErrInvalidPattern,
 		},
-		{
+		{ // 4
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" id=\"gridPattern\" width=\"0\" height=\"2\"><path d=\"M 0 200 V 0 H 100\" fill=\"rgba(255, 0, 0, 1.000)\" stroke=\"rgba(0, 0, 0, 1.000)\" stroke-width=\"1\"></path></pattern></defs></svg>",
 			Err:   ErrInvalidPattern,
 		},
-		{
+		{ // 5
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" id=\"gridPattern\" width=\"1\" height=\"0\"><path d=\"M 0 200 V 0 H 100\" fill=\"rgba(255, 0, 0, 1.000)\" stroke=\"rgba(0, 0, 0, 1.000)\" stroke-width=\"1\"></path></pattern></defs></svg>",
 			Err:   ErrInvalidPattern,
 		},
-		{
+		{ // 6
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" id=\"gridPattern\" width=\"1\" height=\"2\"><path fill=\"rgba(255, 0, 0, 1.000)\" stroke=\"rgba(0, 0, 0, 1.000)\" stroke-width=\"1\"></path></pattern></defs></svg>",
 			Err:   ErrInvalidPattern,
 		},
-		{
+		{ // 7
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" id=\"gridPattern\" width=\"1\" height=\"2\"><path d=\"M 0 200 V 0 H 100\" stroke-width=\"1\"></path></pattern></defs></svg>",
 			Err:   ErrInvalidPattern,
 		},
-		{
+		{ // 8
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" id=\"gridPattern\" width=\"1\" height=\"2\"><path d=\"M 0 200 V 0 H 100\" stroke=\"rgba(0, 0, 0, 1.000)\" stroke-width=\"0\"></path></pattern></defs></svg>",
 			Err:   ErrInvalidPattern,
 		},
-		{
+		{ // 9
 			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs><pattern patternUnits=\"userSpaceOnUse\" id=\"gridPattern\" width=\"1\" height=\"2\"><path d=\"M 0 200 V 0 H 100\" fill=\"rgba(255, 0, 0, 1.000)\" stroke=\"rgba(0, 0, 0, 1.000)\" stroke-width=\"1\"></path></pattern><mask id=\"mask_1\"><image preserveAspectRatio=\"none\" width=\"100\" height=\"200\" xlink:href=\"source.png\" data-token=\"1\"></image></mask></defs></svg>",
 			Output: levelMap{
 				Width:  1,
@@ -90,25 +90,93 @@ func TestMapsMarshal(t *testing.T) {
 				},
 			},
 		},
+		{ // 10
+			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><g></g></svg>",
+			Err:   ErrInvalidLayer,
+		},
+		{ // 11
+			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs></defs><g data-name=\"Test Layer\"></g></svg>",
+			Output: levelMap{
+				Width:    1,
+				Height:   2,
+				Patterns: map[string]*pattern{},
+				Masks:    map[string]*mask{},
+				layer: layer{
+					Layers: []*layer{
+						&layer{
+							Name: "Test Layer",
+						},
+					},
+				},
+			},
+		},
+		{ // 12
+			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs></defs><g data-name=\"Test Layer 1\"><g data-name=\"Test Layer 2\"><g data-name=\"Test Layer 3\"></g><g data-name=\"Test Layer 4\"></g></g></g><g data-name=\"Test Layer 5\"></g></svg>",
+			Output: levelMap{
+				Width:    1,
+				Height:   2,
+				Patterns: map[string]*pattern{},
+				Masks:    map[string]*mask{},
+				layer: layer{
+					Layers: []*layer{
+						&layer{
+							Name: "Test Layer 1",
+							Layers: []*layer{
+								&layer{
+									Name: "Test Layer 2",
+									Layers: []*layer{
+										&layer{
+											Name: "Test Layer 3",
+										},
+										&layer{
+											Name: "Test Layer 4",
+										},
+									},
+								},
+							},
+						},
+						&layer{
+							Name: "Test Layer 5",
+						},
+					},
+				},
+			},
+		},
+		{ // 13
+			Input: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1\" height=\"2\" data-initiative=\"\" data-grid-pos=\"0\" data-grid-hidden=\"false\" data-light-pos=\"0\" data-light-hidden=\"false\" data-light-colour=\"rgba(0, 0, 0, 0.000)\"><defs></defs><g data-name=\"Test Layer 1\" mask=\"url(#mask_1)\" visibility=\"hidden\"></g></svg>",
+			Output: levelMap{
+				Width:    1,
+				Height:   2,
+				Patterns: map[string]*pattern{},
+				Masks:    map[string]*mask{},
+				layer: layer{
+					Layers: []*layer{
+						&layer{
+							Name:   "Test Layer 1",
+							Mask:   "mask_1",
+							Hidden: true,
+						},
+					},
+				},
+			},
+		},
 	} {
 		var m levelMap
 		_, err := m.ReadFrom(strings.NewReader(test.Input))
-		if test.Err != err {
-			t.Errorf("test %d: expecting error %q, got %q", n+1, test.Err, err)
-			continue
-		} else if test.Err != nil {
-			continue
+		if test.Err != nil {
+			if test.Err != err {
+				t.Errorf("test %d: expecting error %q, got %q", n+1, test.Err, err)
+			}
 		} else if err != nil {
 			t.Errorf("test %d: unexpected error: %s", n+1, err)
-			continue
 		} else if !reflect.DeepEqual(test.Output, m) {
 			t.Errorf("test %d: got unexpected output", n+1)
-			continue
+		} else {
+			m.WriteTo(&buf)
+			if string(buf) != test.Input {
+				t.Errorf("test %d: expecting output %q, got %q", n+1, test.Input, buf)
+			}
+			buf = buf[:0]
 		}
-		m.WriteTo(&buf)
-		if string(buf) != test.Input {
-			t.Errorf("test %d: expecting output %q, got %q", n+1, test.Input, buf)
-		}
-		buf = buf[:0]
 	}
 }
