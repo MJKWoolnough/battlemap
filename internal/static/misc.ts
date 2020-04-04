11a5c2b06624d4984cc9556d3abba32ec88cd051
Michael Woolnough <michael.woolnough@gmail.com>
2020-04-04 12:36:04 +0100

    corrected alpha colour conversions

--- internal/static/misc.ts
+++ internal/static/misc.ts
@@ -25,10 +25,10 @@ export const enterKey = function(this: Node, e: KeyboardEvent): void {
 	if (elm.parentNode !== null && elm.nextSibling !== null && (elm.nextSibling as HTMLElement).classList.contains("error")) {
 		elm.parentNode.removeChild(elm.nextSibling);
 	}
-}, hex2Colour = (hex: string): Colour => ({"r": parseInt(hex.slice(1, 3), 16), "g": parseInt(hex.slice(3, 5), 16), "b": parseInt(hex.slice(5, 7), 16), "a": 1}),
+}, hex2Colour = (hex: string): Colour => ({"r": parseInt(hex.slice(1, 3), 16), "g": parseInt(hex.slice(3, 5), 16), "b": parseInt(hex.slice(5, 7), 16), "a": 255}),
 colour2Hex = (c: Colour) => `#${c.r.toString(16).padStart(2, "0")}${c.g.toString(16).padStart(2, "0")}${c.b.toString(16).padStart(2, "0")}`,
 rgba2Colour = (rgba: string): Colour => {
 	const colours = rgba.slice(5, -1).replace(/ /g, "").split(",");
-	return {"r": parseInt(colours[0]), "g": parseInt(colours[1]), "b": parseInt(colours[2]), "a": parseFloat(colours[3])};
+	return {"r": parseInt(colours[0]), "g": parseInt(colours[1]), "b": parseInt(colours[2]), "a": (parseFloat(colours[3]) * 255)|0};
 },
-colour2RGBA = (c: Colour) => `rgba(${c.r.toString()}, ${c.g.toString()}, ${c.b.toString()}, ${c.a.toString()})`;
+colour2RGBA = (c: Colour) => `rgba(${c.r.toString()}, ${c.g.toString()}, ${c.b.toString()}, ${(c.a / 255).toString()})`;
