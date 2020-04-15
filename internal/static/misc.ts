7b2e70cbfda0bcfc1aab8c6a5d2385d9d6539e87
Michael Woolnough <michael.woolnough@gmail.com>
2020-04-15 21:28:20 +0100

    added another special case for rgba2Colour to handle empty strings

--- internal/static/misc.ts
+++ internal/static/misc.ts
@@ -28,7 +28,7 @@ export const enterKey = function(this: Node, e: KeyboardEvent): void {
 }, hex2Colour = (hex: string): Colour => ({"r": parseInt(hex.slice(1, 3), 16), "g": parseInt(hex.slice(3, 5), 16), "b": parseInt(hex.slice(5, 7), 16), "a": 255}),
 colour2Hex = (c: Colour) => `#${c.r.toString(16).padStart(2, "0")}${c.g.toString(16).padStart(2, "0")}${c.b.toString(16).padStart(2, "0")}`,
 rgba2Colour = (rgba: string): Colour => {
-	if (rgba === "transparent") {
+	if (rgba === "transparent" || rgba === "") {
 		return {"r": 0, "g": 0, "b": 0, "a": 0};
 	}
 	const colours = rgba.slice(5, -1).replace(/ /g, "").split(",");
