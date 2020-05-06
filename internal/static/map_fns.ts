06999da084036abfed5e853c11341784423a2b22
Michael Woolnough <michael.woolnough@gmail.com>
2020-05-06 14:31:19 +0100

    need to filter out Grid and Light layers

--- internal/static/map_fns.ts
+++ internal/static/map_fns.ts
@@ -89,4 +89,4 @@ ratio = (mDx: Int, mDy: Int, width: Int, height: Int, dX: (-1 | 0 | 1), dY: (-1 
 	return [mDx * dX, mDy * dY];
 },
 noop = <T>(e: T) => e,
-makeLayerContext = (folder: SVGFolder, fn: (path: string) => void, disabled = "", path = "/"): List => (folder.children as SortNode<SVGFolder | SVGLayer>).map(e => isSVGFolder(e) ? menu(e.name, makeLayerContext(e, fn, disabled, path + e.name + "/")) : (e.name === "disabled" ? disable : noop)(item(e.name, fn.bind(e, path + e.name))));
+makeLayerContext = (folder: SVGFolder, fn: (path: string) => void, disabled = "", path = "/"): List => (folder.children as SortNode<SVGFolder | SVGLayer>).filter(e => e.id >= 0).map(e => isSVGFolder(e) ? menu(e.name, makeLayerContext(e, fn, disabled, path + e.name + "/")) : (e.name === "disabled" ? disable : noop)(item(e.name, fn.bind(e, path + e.name))));
