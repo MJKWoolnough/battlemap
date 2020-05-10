b65445ce5bdd09bdcb0a2decbee8ed84de81de29
Michael Woolnough <michael.woolnough@gmail.com>
2020-05-10 21:17:31 +0100

    added logout button

--- internal/static/settings.ts
+++ internal/static/settings.ts
@@ -1,5 +1,8 @@
+import {createHTML, button} from './lib/html.js';
 import {RPC} from './types.js';
 
 export default function (rpc: RPC, base: HTMLElement, loggedIn: boolean) {
-
+	createHTML(base, [
+		button({"onclick": () => rpc.logout().then(() => window.location.reload())}, "Logout")
+	]);
 };
