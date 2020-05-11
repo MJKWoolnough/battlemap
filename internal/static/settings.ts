4e875366ad474d54b3bfb28b8fab92bb2d4f00d7
Michael Woolnough <michael.woolnough@gmail.com>
2020-05-11 17:28:11 +0100

    use HTTP endpoint for logout (for now)

--- internal/static/settings.ts
+++ internal/static/settings.ts
@@ -1,8 +1,9 @@
+import {HTTPRequest} from './lib/conn.js';
 import {createHTML, button} from './lib/html.js';
 import {RPC} from './types.js';
 
 export default function (rpc: RPC, base: HTMLElement, loggedIn: boolean) {
 	createHTML(base, [
-		button({"onclick": () => rpc.logout().then(() => window.location.reload())}, "Logout")
+		button({"onclick": () => HTTPRequest("login/logout").then(() => window.location.reload())}, "Logout")
 	]);
 };
