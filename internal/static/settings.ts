8d47e6467a0debd3a66c4730d705d2ddbb071b3a
Michael Woolnough <michael.woolnough@gmail.com>
2020-05-11 20:25:38 +0100

    added button for switching between light and dark themes

--- internal/static/settings.ts
+++ internal/static/settings.ts
@@ -1,9 +1,23 @@
 import {HTTPRequest} from './lib/conn.js';
-import {createHTML, button} from './lib/html.js';
+import {createHTML, button, br} from './lib/html.js';
 import {RPC} from './types.js';
 
 export default function (rpc: RPC, base: HTMLElement, loggedIn: boolean) {
+	const htmlElement = document.getElementsByTagName("html")[0];
+	if (window.localStorage.getItem("invert") !== null) {
+		htmlElement.classList.add("invert");
+	}
 	createHTML(base, [
-		loggedIn ? button({"onclick": () => HTTPRequest("login/logout").then(() => window.location.reload())}, "Logout") : button({"onclick": () => HTTPRequest("login/login").then(() => window.location.reload())}, "Login")
+		loggedIn ? button({"onclick": () => HTTPRequest("login/logout").then(() => window.location.reload())}, "Logout") : button({"onclick": () => HTTPRequest("login/login").then(() => window.location.reload())}, "Login"),
+		br(),
+		button({"onclick": function(this: HTMLButtonElement) {
+			if (htmlElement.classList.toggle("invert")) {
+				window.localStorage.setItem("invert", "");
+				this.innerText = "Light Mode";
+			} else {
+				window.localStorage.removeItem("invert");
+				this.innerText = "Dark Mode";
+			}
+		}}, htmlElement.classList.contains("invert") ? "Light Mode" : "Dark Mode")
 	]);
 };
