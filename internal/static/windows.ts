a7dc6140356f92927493f759fedc099bbc558531
Michael Woolnough <michael.woolnough@gmail.com>
2020-05-18 14:40:31 +0100

    corrected hiding of window until size is set

--- internal/static/windows.ts
+++ internal/static/windows.ts
@@ -10,12 +10,13 @@ export const loadingWindow = (p: Promise<any>, parent: ShellElement|WindowElemen
         return p.finally(() => w.remove());
 },
 windows: DOMBind<WindowElement> = (props?: Props | Children, children?: Props | Children) => {
-	const w = createHTML(awindows({"style": "visibility: invisible", "hide-maximise": "true", "tabindex": "-1", "onkeyup": function(this: WindowElement, e: KeyboardEvent) {
+	const w = createHTML(awindows({"hide-maximise": "true", "tabindex": "-1", "onkeyup": function(this: WindowElement, e: KeyboardEvent) {
 		if (e.key === "Escape") {
 			this.remove();
 		}
 	}}), props, children);
 	if (!(w.style.getPropertyValue("--window-width") || w.style.getPropertyValue("--windows-height"))) {
+		w.style.setProperty("visibility", "hidden");
 		window.setTimeout(() => {
 			const {offsetWidth: width, offsetHeight: height} = w,
 			      {offsetWidth: swidth, offsetHeight: sheight} = w.parentNode as ShellElement;
