3199d563966d2626a3e773c4a97e57235e23c8ea
Michael Woolnough <michael.woolnough@gmail.com>
2020-06-01 17:41:04 +0100

    updated setTokenLayer call

--- internal/static/rpc.ts
+++ internal/static/rpc.ts
@@ -133,7 +133,7 @@ export default function (url: string): Promise<Readonly<RPCType>>{
 			"setTokenPattern": (path, pos)                                => rpc.request("maps.setTokenPattern", {path, pos}),
 			"setTokenImage":   (path, pos)                                => rpc.request("maps.setTokenImage", {path, pos}),
 			"setTokenSource":  (path, pos, source)                        => rpc.request("maps.setTokenSource", {path, pos, source}),
-			"setTokenLayer":   (from, fromPos, to, toPos)                 => rpc.request("maps.setTokenLayer", {from, fromPos, to, toPos}),
+			"setTokenLayer":   (from, pos, to)                            => rpc.request("maps.setTokenLayer", {from, pos, to}),
 			"setTokenPos":     (path, pos, newPos)                        => rpc.request("maps.setTokenPos", {path, pos, newPos}),
 			"setInitiative":    initiative                                => rpc.request("maps.setInitiative", initiative),
 
