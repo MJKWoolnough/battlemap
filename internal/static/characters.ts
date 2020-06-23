043dcf0a446420c0b517479aef67f925154b7cf2
Michael Woolnough <michael.woolnough@gmail.com>
2020-06-23 19:59:35 +0100

    can now drag an image asset to the image placeholder to set the icon for a new character

--- internal/static/characters.ts
+++ internal/static/characters.ts
@@ -1,6 +1,6 @@
 import {Int, RPC} from './types.js';
 import {autoFocus, clearElement} from './lib/dom.js';
-import {createHTML, br, button, div, h1, input, label} from './lib/html.js';
+import {createHTML, br, button, div, h1, img, input, label} from './lib/html.js';
 import {ShellElement, loadingWindow, windows} from './windows.js';
 import {showError} from './misc.js';
 import {Root, Folder, Item} from './folders.js';
@@ -17,15 +17,24 @@ export default function (rpc: RPC, shell: ShellElement, base: Node) {
 		const root = new Root(folderList, "Characters", rpcFuncs, shell, Character);
 		createHTML(clearElement(base), {"id": "characters", "class": "folders"}, [
 			button(`New Character`, {"onclick": () => {
-				const name = autoFocus(input({"id": "characterName"})),
-				      icon = 0,
+				const name = autoFocus(input({"id": "characterName"}));
+				let icon = 0,
 				      w = shell.appendChild(windows({"window-title": "New Character"}, [
 					h1("New Character"),
 					label({"for": "characterName"}, "Character Name: "),
 					name,
 					br(),
-					label("Character Image"),
-					div(),
+					label("Character Image: "),
+					div({"style": "display: inline-block; user-select: none; width: 200px; height: 200px; border: 1px solid #888; text-align: center", "ondragover": (e: DragEvent) => {
+						e.preventDefault();
+						if (e.dataTransfer && e.dataTransfer.getData("imageAsset")) {
+							e.dataTransfer.dropEffect = "link";
+						}
+					}, "ondrop": function(this: HTMLDivElement, e: DragEvent) {
+						const tokenData = JSON.parse(e.dataTransfer!.getData("imageAsset"));
+						icon = tokenData.id;
+						clearElement(this).appendChild(img({"src": `/images/${tokenData.id}`, "style": "max-width: 100%; max-height: 100%"}));
+					}}, "Drag icon here"),
 					br(),
 					button("Create", {"onclick": function(this: HTMLButtonElement) {
 						this.setAttribute("disabled", "disabled");
