e706e0d51ca0ead376216f5b34056b4854ee25d8
Michael Woolnough <michael.woolnough@gmail.com>
2020-06-23 10:35:56 +0100

    rewrote character creation to allow for the setting of an icon for the character

--- internal/static/characters.ts
+++ internal/static/characters.ts
@@ -1,7 +1,6 @@
 import {Int, RPC} from './types.js';
-import {createHTML, clearElement, autoFocus} from './lib/dom.js';
-import {audio, button, div, form, h1, img, input, label, progress} from './lib/html.js';
-import {HTTPRequest} from './lib/conn.js';
+import {autoFocus, clearElement} from './lib/dom.js';
+import {createHTML, br, button, div, h1, input, label} from './lib/html.js';
 import {ShellElement, loadingWindow, windows} from './windows.js';
 import {showError} from './misc.js';
 import {Root, Folder, Item} from './folders.js';
@@ -17,11 +16,29 @@ export default function (rpc: RPC, shell: ShellElement, base: Node) {
 	rpcFuncs.list().then(folderList => {
 		const root = new Root(folderList, "Characters", rpcFuncs, shell, Character);
 		createHTML(clearElement(base), {"id": "characters", "class": "folders"}, [
-			button(`New Character`, {"onclick": () => shell.prompt("New Character", "Please enter the character name: ").then(name => {
-				if (name) {
-					rpc.characterCreate(name).then(({id, name}) => root.addItem(id, name));
-				}
-			})}),
+			button(`New Character`, {"onclick": () => {
+				const name = autoFocus(input({"id": "characterName"})),
+				      icon = 0,
+				      w = shell.appendChild(windows({"window-title": "New Character"}, [
+					h1("New Character"),
+					label({"for": "characterName"}, "Character Name: "),
+					name,
+					br(),
+					label("Character Image"),
+					div(),
+					br(),
+					button("Create", {"onclick": function(this: HTMLButtonElement) {
+						this.setAttribute("disabled", "disabled");
+						loadingWindow(rpc.characterCreate(name.value).then(({id, name}) => {
+							root.addItem(id, name);
+							return rpc.characterSet(id, {"store-image-icon": icon.toString()});
+						}), w).catch(e => {
+							showError(name, e);
+							this.removeAttribute("disabled");
+						});
+					}})
+				      ]));
+			}}),
 			root.node
 		]);
 	});
