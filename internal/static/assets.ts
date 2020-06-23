19e31ba6bb1a71a34201e9c7a4a1cd8987eb83b4
Michael Woolnough <michael.woolnough@gmail.com>
2020-06-23 09:42:53 +0100

    need to disabled the button during upload to prevent double sending

--- internal/static/assets.ts
+++ internal/static/assets.ts
@@ -67,6 +67,7 @@ export default function (rpc: RPC, shell: ShellElement, base: Node, fileType: "I
 					form({"enctype": "multipart/form-data", "method": "post"}, [
 						label({"for": "addAssets"}, "Add Asset(s)"),
 						autoFocus(input({"accept": fileType === "Images" ? "image/gif, image/png, image/jpeg, image/webp" : "application/ogg, audio/mpeg", "id": "addAssets", "multiple": "multiple", "name": "asset", "type": "file", "onchange": function(this: HTMLInputElement) {
+							this.setAttribute("disabled", "disabled");
 							const bar = progress({"style": "width: 100%"}) as HTMLElement;
 							loadingWindow(HTTPRequest(`/${fileType.toLowerCase()}/`, {
 								"data": new FormData(this.parentNode as HTMLFormElement),
@@ -85,7 +86,10 @@ export default function (rpc: RPC, shell: ShellElement, base: Node, fileType: "I
 							])).then((assets: IDName[]) => {
 								assets.forEach(({id, name}) => root.addItem(id, name));
 								window.remove();
-							}, showError.bind(null, this));
+							}, e => {
+								showError(this, e);
+								this.removeAttribute("disabled");
+							});
 						}}))
 					])
 				]));
