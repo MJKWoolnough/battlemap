772303399e19fc9d877b46972771674d18041ac2
Michael Woolnough <michael.woolnough@gmail.com>
2020-07-03 08:49:45 +0100

    actually use the GetAll variant func

--- internal/static/characterList.ts
+++ internal/static/characterList.ts
@@ -11,7 +11,7 @@ let rpc: RPC, n = 0;
 class Character extends DraggableItem {
 	constructor(parent: Folder, id: Int, name: string) {
 		super(parent, id, name);
-		rpc.characterGet(id, []).then(d => {
+		rpc.characterGetAll(id).then(d => {
 			characterData.set(id, d);
 			if (d["store-image-icon"]) {
 				this.setIcon(parseInt(d["store-image-icon"].data));
