f65c2532ddc4eaa5017d94582d19f96843d7a175
Michael Woolnough <michael.woolnough@gmail.com>
2020-07-05 10:02:55 +0100

    removed old error handling code

--- internal/static/misc.ts
+++ internal/static/misc.ts
@@ -11,21 +11,6 @@ export const enterKey = function(this: Node, e: KeyboardEvent): void {
 			}
 		}
 	}
-}, showError = (elm: Node, e: Error | string): void => {
-	const msg =  e instanceof Error ? e.message : e;
-	if (elm.nextSibling !== null) {
-		if ((elm.nextSibling as HTMLElement).classList.contains("error")) {
-			elm.nextSibling.textContent = msg;
-		} else if (elm.parentNode !== null) {
-			elm.parentNode.insertBefore(createHTML("span", {"class": "error"}, msg), elm.nextSibling);
-		}
-	} else if (elm.parentNode !== null) {
-		elm.parentNode.appendChild(createHTML("span", {"class": "error"}, msg));
-	}
-}, clearError = (elm: Node): void => {
-	if (elm.parentNode !== null && elm.nextSibling !== null && (elm.nextSibling as HTMLElement).classList.contains("error")) {
-		elm.parentNode.removeChild(elm.nextSibling);
-	}
 }, hex2Colour = (hex: string): Colour => ({"r": parseInt(hex.slice(1, 3), 16), "g": parseInt(hex.slice(3, 5), 16), "b": parseInt(hex.slice(5, 7), 16), "a": 255}),
 colour2Hex = (c: Colour) => `#${c.r.toString(16).padStart(2, "0")}${c.g.toString(16).padStart(2, "0")}${c.b.toString(16).padStart(2, "0")}`,
 rgba2Colour = (rgba: string): Colour => {
