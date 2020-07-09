a3767cca5e26e9f293fad46385fbcd67c1d499b3
Michael Woolnough <michael.woolnough@gmail.com>
2020-07-09 16:37:07 +0100

    modified handleError to stringify any objects it receives

--- internal/static/misc.ts
+++ internal/static/misc.ts
@@ -24,5 +24,5 @@ colour2RGBA = (c: Colour) => `rgba(${c.r.toString()}, ${c.g.toString()}, ${c.b.t
 noColour = {"r": 0, "g": 0, "b": 0, "a": 0},
 handleError = (e: Error | string) => {
 	console.log(e);
-	(document.body.getElementsByTagName("windows-shell")[0] as ShellElement).alert("Error", e instanceof Error ? e.message : e);
+	(document.body.getElementsByTagName("windows-shell")[0] as ShellElement).alert("Error", e instanceof Error ? e.message : typeof e  === "object" ? JSON.stringify(e) : e);
 };
