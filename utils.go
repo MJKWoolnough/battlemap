78bccd21196ea34b34b67f4945583054c1b88363
Michael Woolnough <michael.woolnough@gmail.com>
2020-07-14 19:02:18 +0100

    embedded switch

--- utils.go
+++ utils.go
@@ -15,20 +15,6 @@ const (
 	fileTypeMap
 )
 
-func getType(mime string) fileType {
-	switch mime {
-	case "image/gif", "image/png", "image/jpeg", "image/webp", "video/apng":
-		return fileTypeImage
-	case "application/ogg", "audio/mpeg":
-		return fileTypeAudio
-	case "application/x-gzip":
-		return fileTypeKeystore
-	case "text/plain; charset=utf-8":
-		return fileTypeMap
-	}
-	return fileTypeUnknown
-}
-
 type getFileType struct {
 	Buffer [512]byte
 	BufLen int
@@ -41,7 +27,18 @@ func (g *getFileType) ReadFrom(r io.Reader) (int64, error) {
 		return int64(n), err
 	}
 	g.BufLen = n
-	g.Type = getType(http.DetectContentType(g.Buffer[:n]))
+	switch http.DetectContentType(g.Buffer[:n]) {
+	case "image/gif", "image/png", "image/jpeg", "image/webp", "video/apng":
+		g.Type = fileTypeImage
+	case "application/ogg", "audio/mpeg":
+		g.Type = fileTypeAudio
+	case "application/x-gzip":
+		g.Type = fileTypeKeystore
+	case "text/plain; charset=utf-8":
+		g.Type = fileTypeMap
+	default:
+		g.Type = fileTypeUnknown
+	}
 	return int64(n), nil
 }
 
