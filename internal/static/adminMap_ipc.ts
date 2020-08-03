665fac19694b6726e45dc6d7b17a527d01311df1
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-03 14:49:23 +0100

    added a 'selected' request to get selectedLayer and selectedToken from adminMap

--- internal/static/adminMap_ipc.ts
+++ internal/static/adminMap_ipc.ts
@@ -1,5 +1,11 @@
 import {LayerRPC, Uint} from './types.js';
-import {Pipe} from './lib/inter.js';
+import {SVGLayer, SVGToken, SVGShape} from './map.js';
+import {Pipe, Requester} from './lib/inter.js';
 
 export const {send: mapLoadSend, receive: mapLoadReceive} = new Pipe<Uint>(),
-{send: mapLayersSend, receive: mapLayersReceive} = new Pipe<LayerRPC>();
+{send: mapLayersSend, receive: mapLayersReceive} = new Pipe<LayerRPC>(),
+{request: requestSelected, responder: respondWithSelected} = new Requester<{
+	layer: SVGLayer | null;
+	layerPath: string;
+	token: SVGToken | SVGShape | null;
+}>();
