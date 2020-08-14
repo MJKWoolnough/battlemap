170c45368fe30777be9f19a4d0aaaf1e0096a7fc
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-14 09:22:05 +0100

    removed unnecessary reset funcs

--- internal/static/tools_zoom.ts
+++ internal/static/tools_zoom.ts
@@ -36,7 +36,6 @@ let zoomMode: 1 | -1 = -1;
 export default Object.freeze({
 	"name": "Zoom",
 	"icon": "iVBORw0KGgoAAAANSUhEUgAAAEoAAABLAQMAAADtQoOXAAAABlBMVEUAAAAAAAClZ7nPAAAAAXRSTlMAQObYZgAAAMBJREFUeAHN0YFGBEEcBvDfbIs9LlYC4FZCgOsJmnToMXqUkRCgR1p6kXuSFXZ9OAJQf4wfw5j/9yHD0xLOZd7UMW7cH5b9xuO5fG984dhWfjBUUAb6CXRHrkbQv1E+V1asHBpOYAf34CaHDka5213yJHyFvv0p87PfmIWy5sXyiSRBJb4vEqpE/SAFnFLL47uUVVLhtZZiWwXOZa7TysNiGm1jHMKhD/su7EpYtLjVsE7h/3t4jlsLnyN3kdvI4Af7VSj1YtdmoAAAAABJRU5ErkJggg==",
-	"reset": () => {},
 	"options": div([
 		label({"for": "zoomIn"}, "Zoom In: "),
 		zoomIn,
