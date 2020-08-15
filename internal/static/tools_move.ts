df206cdf5082e39ddc72d5f4abff561532a12264
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-15 15:36:18 +0100

    converted move tool icon to SVG

--- internal/static/tools_move.ts
+++ internal/static/tools_move.ts
@@ -1,5 +1,6 @@
 import {RPC, LayerRPC} from './types.js';
 import {div} from './lib/html.js';
+import {svg, line, polygon} from './lib/svg.js';
 import {SVGToken} from './map.js';
 import {requestSelected, requestMapUndo, mapLayersReceive} from './comms.js';
 import {handleError} from './misc.js';
@@ -68,7 +69,14 @@ const startDrag = function(this: SVGElement, e: MouseEvent, rpc: RPC) {
 
 export default Object.freeze({
 	"name": "Move All",
-	"icon": "iVBORw0KGgoAAAANSUhEUgAAAFYAAABVCAMAAADkHONrAAAAS1BMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADmYDp0AAAAGXRSTlMAAk2Bnv/t5uLo7PH0/vf6/PL1/UyAS8AEV9+5ZgAAAe5JREFUeAHN2Y2KqzAUBGCjnWq1qVVjet//SS8gA7thgIQBusN/gK+HpJBzTFeb0A996LwI9QbgJlxbla6v3oXrq6Oo11cnQLiu+gBcV6gz4LpCXQDXFeoTTHTcQn0BkabvUl2JefUW6pug71KlIqLdOpVZt2U/bJeqiOtS3X23pVa6vuq7PeqS2tihkj3/RLUhnzXJof5CDulM1UeReXBVf4Ksfk392AnpalUdRQKyZrUrVJwS0KvSFWo7K1yhtrPCFWo7S1erBktXqAZbuEJtZ3cg0lWqUe2brlANFitdoRosXnQLNXosnnSl2s6y9VnoarWd3S5gpivVdpat6oMuTwsWy3YC0+VSLdmQzt/5AJ9iKQWyOMCMAPrrMhR7m1CRTPZn7sDQDcAdjMeyLlY7gjnIFnev3oTMTSjCvZ24sLcfmVLLsWux2AiqdOdrcXNYqsXoFYHVYKnq4aud1Srdl8dSFQOYwQqV7ttkqYohbP/+zev3CX5X880ezO8Yv9Df+t24bvLt2SHkHOxJp3Yw+8dpq+6TakJd+q4pZyU7tLF+tf4HBds11Ap316rrGqpwj33ZVkulqxMdVXfUelYyXfYoVE2XdhRzkunKKcl0mcVXhTv7qnAfvircyVeFO/qq/wTlP5iZrq96j5H/AbtSR3ts3avHAAAAAElFTkSuQmCC",
+	"icon": svg({"viewBox": "0 0 22 22", "style": "background-color: transparent"}, [
+		line({"x1": 11, "y1": 6, "x2": 11, "y2": 16, "stroke-width": 1, "stroke": "#000", "stroke-linecap": "round"}),
+		line({"x1": 6, "y1": 11, "x2": 16, "y2": 11, "stroke-width": 1, "stroke": "#000", "stroke-linecap": "round"}),
+		polygon({"points": "11,0 6,5 16,5"}),
+		polygon({"points": "0,11 5,6 5,16"}),
+		polygon({"points": "11,22 6,17 16,17"}),
+		polygon({"points": "22,11 17,16 17,6"}),
+	]),
 	"mapMouseOver": mouseCursor,
 	"tokenMouseOver": mouseCursor,
 	"mapMouseDown": startDrag,
