bd213ef9bf00597665ae3bd90d840d16c43802c0
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-22 08:56:24 +0100

    pulled out common params into parent svg element

--- internal/static/tools_move.ts
+++ internal/static/tools_move.ts
@@ -1,6 +1,6 @@
 import {RPC, LayerRPC} from './types.js';
 import {div} from './lib/html.js';
-import {svg, line, polygon} from './lib/svg.js';
+import {svg, g, line, polygon} from './lib/svg.js';
 import {SVGToken} from './map.js';
 import {requestSelected, requestMapUndo, mapLayersReceive} from './comms.js';
 import {handleError} from './misc.js';
@@ -70,8 +70,10 @@ const startDrag = function(this: SVGElement, e: MouseEvent, rpc: RPC) {
 export default Object.freeze({
 	"name": "Move All",
 	"icon": svg({"viewBox": "0 0 22 22"}, [
-		line({"x1": 11, "y1": 6, "x2": 11, "y2": 16, "stroke-width": 1, "stroke": "#000", "stroke-linecap": "round"}),
-		line({"x1": 6, "y1": 11, "x2": 16, "y2": 11, "stroke-width": 1, "stroke": "#000", "stroke-linecap": "round"}),
+		g({"stroke-width": 1, "stroke": "#000", "stroke-linecap": "round"}, [
+			line({"x1": 11, "y1": 6, "x2": 11, "y2": 16}),
+			line({"x1": 6, "y1": 11, "x2": 16, "y2": 11}),
+		]),
 		polygon({"points": "11,0 6,5 16,5"}),
 		polygon({"points": "0,11 5,6 5,16"}),
 		polygon({"points": "11,22 6,17 16,17"}),
