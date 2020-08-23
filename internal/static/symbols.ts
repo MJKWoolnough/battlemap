1b078d7a1124946033a69e8081cfa80e65aa391f
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-23 08:31:34 +0100

    added a simple symbol store to replace CSS PNGs with SVG symbols

---
+++ internal/static/symbols.ts
@@ -0,0 +1,17 @@
+import {Props} from './lib/dom.js';
+import {svg, symbol, use} from './lib/svg.js';
+
+const symbols = svg({"style": "display: none"}),
+      symbolMap = new Map<string, (props?: Props) => SVGSVGElement>();
+
+export const addSymbol = (id: string, s: SVGSymbolElement) => {
+	s.setAttribute("id", id)
+	symbols.appendChild(s);
+	const fn = (props: Props = {}) => svg(props, use({"href": `#${id}`}));
+	symbolMap.set(id, fn)
+	return fn;
+},
+getSymbol = (id: string) => symbolMap.get(id);
+
+export default symbols;
+
