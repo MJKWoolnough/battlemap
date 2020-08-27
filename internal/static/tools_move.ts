cf9aa82862fd09b2dd5757ba4bba679c8381b96e
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-27 08:50:50 +0100

    updated symbol to use current text colour as colour of symbol

--- internal/static/tools_move.ts
+++ internal/static/tools_move.ts
@@ -69,8 +69,8 @@ const startDrag = function(this: SVGElement, e: MouseEvent, rpc: RPC) {
 
 export default Object.freeze({
 	"name": "Move All",
-	"icon": svg({"viewBox": "0 0 22 22"}, [
-		g({"stroke-width": 1, "stroke": "#000", "stroke-linecap": "round"}, [
+	"icon": svg({"viewBox": "0 0 22 22", "style": "fill: currentColor"}, [
+		g({"stroke-width": 1, "style": "stroke: currentColor", "stroke-linecap": "round"}, [
 			line({"x1": 11, "y1": 6, "x2": 11, "y2": 16}),
 			line({"x1": 6, "y1": 11, "x2": 16, "y2": 11}),
 		]),
