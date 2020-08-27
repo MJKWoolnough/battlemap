58f64dff74467256daf9442c9f4340880839032c
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-27 08:50:45 +0100

    updated symbol to use current text colour as colour of symbol

--- internal/static/tools_mask.ts
+++ internal/static/tools_mask.ts
@@ -11,6 +11,6 @@ export default Object.freeze({
 			use({"x": 22, "href": "#e"}),
 			path({"d": "M20,35 q10,5 20,0 q-10,10 -20,0"}),
 		]),
-		path({"d": "M0,0 Q30,15 60,0 Q30,100 0,0", "stroke": "none", "mask": "url(#m)"})
+		path({"d": "M0,0 Q30,15 60,0 Q30,100 0,0", "stroke": "none", "style": "fill: currentColor", "mask": "url(#m)"})
 	])
 });
