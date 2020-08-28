9877eb34f25568f9ed47f794a20265acf50f9d65
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-28 08:22:03 +0100

    simplified mask icon

--- internal/static/tools_mask.ts
+++ internal/static/tools_mask.ts
@@ -2,15 +2,5 @@ import {svg, defs, mask, rect, path, use} from './lib/svg.js';
 
 export default Object.freeze({
 	"name": "Layer Mask",
-	"icon": svg({"viewBox": "0 0 60 50"}, [
-		defs(path({"id": "e", "d": "M32,20 q9,-10 18,0 q-9,-3 -18,0"})),
-		mask({"id": "m"}, [
-			rect({"width": 100, "height": 100, "fill": "#fff"}),
-			path({"d": "M10,20 q9,-10 18,0 q-9,-3 -18,0"}),
-			use({"href": "#e"}),
-			use({"x": 22, "href": "#e"}),
-			path({"d": "M20,35 q10,5 20,0 q-10,10 -20,0"}),
-		]),
-		path({"d": "M0,0 Q30,15 60,0 Q30,100 0,0", "stroke": "none", "style": "fill: currentColor", "mask": "url(#m)"})
-	])
+	"icon": svg({"viewBox": "0 0 60 50"}, path({"d": "M0,0 Q30,15 60,0 Q30,100 0,0 M32,20 q9,-10 18,0 q-9,-3 -18,0 M10,20 q9,-10 18,0 q-9,-3 -18,0 M20,35 q10,5 20,0 q-10,10 -20,0", "stroke": "none", "style": "fill: currentColor", "fill-rule": "evenodd"}))
 });
