b9d8559bc9e7099c9662c23b24ac9ff37f0bb0f9
Michael Woolnough <michael.woolnough@gmail.com>
2020-08-29 09:00:24 +0100

    remove now unused Requesters from comms

--- internal/static/comms.ts
+++ internal/static/comms.ts
@@ -6,12 +6,4 @@ import Undo from './undo.js';
 
 export const {send: mapLoadSend, receive: mapLoadReceive} = new Pipe<Uint>(),
 {send: mapLayersSend, receive: mapLayersReceive} = new Pipe<LayerRPC>(),
-{request: requestSelected, responder: respondWithSelected} = new Requester<{
-	layer: SVGLayer | null;
-	layerPath: string;
-	token: SVGToken | SVGShape | null;
-	outline: SVGGElement;
-	deselectToken: () => void;
-}>(),
-{request: requestMapUndo, responder: respondWithMapUndo} = new Requester<Undo>(),
 {request: requestShell, responder: respondWithShell} = new Requester<ShellElement>();
