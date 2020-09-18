e5c6fbeb01fb7588df43c3111192b64c8c4613b2
Michael Woolnough <michael.woolnough@gmail.com>
2020-09-18 08:05:25 +0100

    updated to new globals layout

--- internal/static/tools_draw.ts
+++ internal/static/tools_draw.ts
@@ -39,7 +39,7 @@ const draw = (root: SVGElement, e: MouseEvent, rpc: RPC) => {
 				clearup();
 				const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked),
 				      dr = isEllipse ? 2 : 1,
-				      {selectedLayerPath, selectedLayer} = globals,
+				      {selectedLayer} = globals,
 				      width = Math.abs(cx - x),
 				      height = Math.abs(cy - y),
 				      token = {"id": 0, "x": isEllipse ? cx - width : Math.min(cx, x), "y": isEllipse ? cy - height : Math.min(cy, y), "width": width * dr, "height": height * dr, "rotation": 0, "snap": snap.checked, "fill": fillColour, "stroke": strokeColour, "strokeWidth": parseInt(strokeWidth.value), "tokenType": 1, isEllipse, "lightColour": noColour, "lightIntensity": 0};
@@ -47,7 +47,7 @@ const draw = (root: SVGElement, e: MouseEvent, rpc: RPC) => {
 					const doIt = () => {
 						const pos = selectedLayer.tokens.length;
 						selectedLayer.tokens.push(SVGShape.from(token));
-						rpc.addToken(selectedLayerPath, token).then(id => token.id = id).catch(handleError);
+						rpc.addToken(selectedLayer.path, token).then(id => token.id = id).catch(handleError);
 						return () => {
 							selectedLayer.tokens.pop();
 							rpc.removeToken(token.id).catch(handleError);
@@ -114,13 +114,13 @@ const draw = (root: SVGElement, e: MouseEvent, rpc: RPC) => {
 				c.x -= minX;
 				c.y -= minY;
 			}
-			const {selectedLayerPath, selectedLayer} = globals,
+			const {selectedLayer} = globals,
 			      token = {"id": 0, "x": minX, "y": minY, "width": maxX - minX, "height": maxY - minY, "rotation": 0, "snap": snap.checked, "fill": fillColour, "stroke": strokeColour, "strokeWidth": parseInt(strokeWidth.value), "tokenType": 2, points, "lightColour": noColour, "lightIntensity": 0};
 			if (selectedLayer) {
 				const doIt = () => {
 					const pos = selectedLayer.tokens.length;
 					selectedLayer.tokens.push(SVGDrawing.from(token));
-					rpc.addToken(selectedLayerPath, token).then(id => token.id = id).catch(handleError);
+					rpc.addToken(selectedLayer.path, token).then(id => token.id = id).catch(handleError);
 					return () => {
 						selectedLayer.tokens.pop();
 						rpc.removeToken(token.id);
