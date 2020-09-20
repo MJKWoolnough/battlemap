93892fe4a5c6fa1a5b3a7f66b3cc18e6db4b1e0f
Michael Woolnough <michael.woolnough@gmail.com>
2020-09-20 10:31:06 +0100

    added ability for plugins to provide settings

--- internal/static/plugins.ts
+++ internal/static/plugins.ts
@@ -1,9 +1,21 @@
+import {Uint} from './types.js';
+import {h1} from './lib/html.js';
 import {HTTPRequest} from './lib/conn.js';
 
-type plugin = {
+type owp<T extends Function = () => void> = {
+	priority: Uint;
+	fn: T;
 }
 
-const plugins = new Map<string, plugin>();
+type plugin = {
+	settings?: owp<() => HTMLElement>;
+}
+
+
+const plugins = new Map<string, plugin>(),
+      owpSort = (key: keyof plugin) => (a: [string, plugin], b: [string, plugin]) => a[1][key]!.priority - b[1][key]!.priority;
+
+export const settings = () => Array.from(plugins.entries()).filter(p => p[1].settings).sort(owpSort("settings")).map(([name, plugin]) => [h1(name), plugin["settings"]!["fn"]()]);
 
 export default function (name: string, p: plugin) {
 	plugins.set(name, p);
