50e4ff3b82e702d0e35629565e0c3518c4dede32
Michael Woolnough <michael.woolnough@gmail.com>
2020-09-21 10:22:51 +0100

    added characterEdit plugin to modifiy how character and token editing happens

--- internal/static/plugins.ts
+++ internal/static/plugins.ts
@@ -1,4 +1,4 @@
-import {Uint} from './types.js';
+import {Uint, KeystoreData} from './types.js';
 import {h1} from './lib/html.js';
 import {HTTPRequest} from './lib/conn.js';
 
@@ -9,13 +9,23 @@ type owp<T extends Function = () => void> = {
 
 type plugin = {
 	settings?: owp<() => HTMLElement>;
+	characterEdit?: owp<(id: Uint, data: Record<string, KeystoreData>, isCharacter: boolean, changes: Record<string, KeystoreData>, removes: Set<string>, save: () => Promise<void>) => HTMLElement[] | null>;
 }
 
 
 const plugins = new Map<string, plugin>(),
       filterSortPlugins = <K extends keyof plugin>(key: K) => Array.from(plugins.entries()).filter(p => p[1][key]).sort((a: [string, plugin], b: [string, plugin]) => a[1][key]!.priority - b[1][key]!.priority) as [string, Required<Pick<plugin, K>> & Omit<plugin, K>][];
 
-export const settings = () => filterSortPlugins("settings").map(([name, plugin]) => [h1(name), plugin["settings"].fn()]);
+export const settings = () => filterSortPlugins("settings").map(([name, plugin]) => [h1(name), plugin["settings"].fn()]),
+       characterEdit = (id: Uint, data: Record<string, KeystoreData>, isCharacter: boolean, changes: Record<string, KeystoreData>, removes: Set<string>, save: () => Promise<void>) => {
+	for (const p of filterSortPlugins("characterEdit")) {
+		const h = p[1]["characterEdit"].fn(id, data, isCharacter, changes, removes, save);
+		if (h) {
+			return h;
+		}
+	}
+	return null;
+       };
 
 export default function (name: string, p: plugin) {
 	plugins.set(name, p);
