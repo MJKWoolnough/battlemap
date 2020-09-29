2e3f1473e7865cfa19d131eafe7ea1146ce4e5c7
Michael Woolnough <michael.woolnough@gmail.com>
2020-09-29 09:18:43 +0100

    replaced all displayed text with text in selected language module

--- internal/static/settings.ts
+++ internal/static/settings.ts
@@ -5,7 +5,7 @@ import {Int, RPC} from './types.js';
 import {ShellElement} from './windows.js';
 import {BoolSetting, IntSetting} from './settings_types.js';
 import {settings as pluginSettings} from './plugins.js';
-import {language, languages} from './language.js';
+import lang, {language, languages} from './language.js';
 
 export const autosnap = new BoolSetting("autosnap"),
 hideMenu = new BoolSetting("menuHide"),
@@ -15,32 +15,32 @@ invert = new BoolSetting("invert");
 
 export default function (rpc: RPC, shell: ShellElement, base: HTMLElement, loggedIn: boolean) {
 	createHTML(base, [
-		h1("Authentication"),
-		loggedIn ? button({"onclick": () => HTTPRequest("login/logout").then(() => window.location.reload())}, "Logout") : button({"onclick": () => HTTPRequest("login/login").then(() => window.location.reload())}, "Login"),
+		h1(lang["AUTH"]),
+		loggedIn ? button({"onclick": () => HTTPRequest("login/logout").then(() => window.location.reload())}, lang["LOGOUT"]) : button({"onclick": () => HTTPRequest("login/login").then(() => window.location.reload())}, lang["Login"]),
 		br(),
-		h1("Language"),
-		label({"for": "language_select"}, "Select Language: "),
+		h1(lang["LANGUAGE"]),
+		label({"for": "language_select"}, `${lang["SELECT_LANGUAGE"]}: `),
 		select({"id": "language_select", "onchange": function(this: HTMLSelectElement) {
 			language.set(this.value);
 		}}, languages.map(l => option({"selected": l === language.value}, l))),
-		h1("Theme"),
+		h1(lang["THEME"]),
 		button({"onclick": function(this: HTMLButtonElement) {
-			this.innerText = invert.set(!invert.value) ? "Light Mode" : "Dark Mode"
-		}}, invert.value ? "Light Mode" : "Dark Mode"),
-		h1("Map Settings"),
+			this.innerText = invert.set(!invert.value) ? lang["LIGHT_MODE"] : lang["DARK_MODE"];
+		}}, invert.value ? lang["LIGHT_MODE"] : lang["DARK_MODE"]),
+		h1(lang["MAP_SETTINGS"]),
 		loggedIn ? [
 			input({"type": "checkbox", "id": "autosnap", "class": "settings_ticker", "checked": autosnap.value, "onchange": function(this: HTMLInputElement) {
 				autosnap.set(this.checked);
 			}}),
-			label({"for": "autosnap"}, "Autosnap: "),
+			label({"for": "autosnap"}, `${lang["AUTOSNAP"]}: `),
 			br()
 		] : [],
-		label({"for": "scrollAmount"}, "Scroll (zero is square width): "),
+		label({"for": "scrollAmount"}, `${lang["SCROLL_AMOUNT"]}: `),
 		input({"id": "scrollAmount", "type": "number", "value": scrollAmount.value, "step": 1, "onchange": function(this: HTMLInputElement) {
 			scrollAmount.set(parseInt(this.value));
 		}}),
 		br(),
-		label({"for": "undoLimit"}, "Undo Limit (-1 for infinite, 0 to disable): "),
+		label({"for": "undoLimit"}, `${lang["UNDO_LIMIT"]}: `),
 		input({"id": "undoLimit", "type": "number", "value": undoLimit.value, "step": 1, "min": "-1", "onchange": function(this: HTMLInputElement) {
 			undoLimit.set(parseInt(this.value));
 		}}),
@@ -48,15 +48,15 @@ export default function (rpc: RPC, shell: ShellElement, base: HTMLElement, logge
 		input({"type": "checkbox", "id": "menuHide", "class": "settings_ticker", "checked": hideMenu.value, "onchange": function(this: HTMLInputElement) {
 			hideMenu.set(this.checked);
 		}}),
-		label({"for": "menuHide"}, "Hide Menu Button?: "),
+		label({"for": "menuHide"}, `${lang["HIDE_MENU"]}: `),
 		pluginSettings(),
-		h1("Reset"),
-		button({"onclick": () => shell.confirm("Are you sure?", "Are you sure that you wish to clear all settings? This cannot be undone").then(v => {
+		h1(lang["SETTINGS_RESET"]),
+		button({"onclick": () => shell.confirm(lang["ARE_YOU_SURE"], lang["SETTINGS_RESET_CONFIRM"]).then(v => {
 			if (!v) {
 				return;
 			}
 			window.localStorage.clear();
 			window.location.reload();
-		})}, "Clear All Settings")
+		})}, lang["SETTINGS_CLEAR"])
 	]);
 };
