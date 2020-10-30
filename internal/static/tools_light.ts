6888fec279a5ee8248943a0b02f04c920836befd
Michael Woolnough <michael.woolnough@gmail.com>
2020-10-30 19:56:22 +0000

    simplified light-bulb SVG

--- internal/static/tools_light.ts
+++ internal/static/tools_light.ts
@@ -216,10 +216,9 @@ mapLayersReceive(l => {
 addTool({
 	"name": lang["TOOL_LIGHT"],
 	"icon": svg({"viewBox": "0 0 44 75"}, [
-		defs(path({"id": "c", "d": "M12,61 q-2,2 0,4 q10,3 20,0 q2,-2 0,-4", "stroke-width": 1})),
 		g({"style": "stroke: currentColor", "fill": "none", "stroke-linejoin": "round"}, [
 			path({"d": "M12,61 c0,-20 -30,-58 10,-60 c40,2 10,40 10,60 q-10,3 -20,0 Z", "stroke-width": 2}),
-			use({"href": "#c"}),
+			path({"id": "c", "d": "M12,61 q-2,2 0,4 q10,3 20,0 q2,-2 0,-4", "stroke-width": 1}),
 			use({"href": "#c", "y": 4}),
 			use({"href": "#c", "y": 8}),
 		])
