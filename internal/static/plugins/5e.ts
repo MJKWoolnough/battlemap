75d0b2f6ea08ad3c919b80d4b152c0e77663fe63
Michael Woolnough <michael.woolnough@gmail.com>
2020-10-31 21:22:36 +0000

    corrected conditions positioning

--- internal/static/plugins/5e.ts
+++ internal/static/plugins/5e.ts
@@ -169,7 +169,7 @@ class SVGToken5E extends SVGToken {
 				this.conditions.appendChild(use({"href": `#5e-condition-${conditions[i]}`, "x": col * w, "y": row * h, "width": w, "height": h}));
 			}
 		}
-		this.conditions.setAttribute("transform", `translate(${this.height}, 0)`);
+		this.conditions.setAttribute("transform", `translate(0, ${this.height})`);
 	}
 
 }
