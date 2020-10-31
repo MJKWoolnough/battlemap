62e57948c6ba0b7394de0b961afbaa078853940f
Michael Woolnough <michael.woolnough@gmail.com>
2020-10-31 19:43:46 +0000

    need to set correct text fill colour on init

--- internal/static/plugins/5e.ts
+++ internal/static/plugins/5e.ts
@@ -79,7 +79,7 @@ class SVGToken5E extends SVGToken {
 				this.hp = g({"style": currentHP === null || maxHP === null ? "display: none" : undefined}, [
 					this.hpBack = use({"href": "#5e-hp-back", "width": this.width / 4, "height": this.width / 4}),
 					this.hpBar = use({"href": "#5e-hp", "width": this.width / 4, "height": this.width / 4, "stroke-dasharray": `${Math.PI * 19 * 0.75 * Math.min(currentHP || 0, maxHP || 0) / (maxHP || 1)} 60`, "style": `color: rgba(${Math.round(255 * Math.min(currentHP || 0, maxHP || 0) / (maxHP || 1))}, 0, 0, 1)`}),
-					this.hpValue = text({"x": this.width / 8, "y": "1.2em", "text-anchor": "middle"}, currentHP?.toString() ?? "")
+					this.hpValue = text({"x": this.width / 8, "y": "1.2em", "text-anchor": "middle", "fill": `rgba(${Math.round(255 * Math.min(currentHP || 0, maxHP || 0) / (maxHP || 1))}, 0, 0, 1)`}, currentHP?.toString() ?? "")
 				])
 			])
 		]);
