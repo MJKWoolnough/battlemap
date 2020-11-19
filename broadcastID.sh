95f3bae7ad9952c0d43d22925dc2ec3265a420d2
Michael Woolnough <michael.woolnough@gmail.com>
2020-11-19 08:59:25 +0000

    simplified grabbing of broadcast IDs

--- broadcastID.sh
+++ broadcastID.sh
@@ -14,7 +14,7 @@ data="$(cat "$rpcFile")";
 				fi;
 				let "num++";
 				echo -n " $c = -$num";
-			done < <(sed -n '/^const ($/{p; :loop n; p; /^)$/q; b loop}' ./socket_broadcast.go | head -n-1 | tail -n+2 | grep -v "^$" | sed -e 's/^	//' -e 's/ .*//');
+			done < <(sed -n '/^const ($/{:loop n;/^)$/q; p; b loop}' ./socket_broadcast.go | grep -v "^$" | sed -e 's/^	//' -e 's/ .*//');
 			echo -e ";\n\n";
 			break;
 		fi;
