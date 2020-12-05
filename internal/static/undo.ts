38c584a2437e31af6adabd9f5af7a43f23849339
Michael Woolnough <michael.woolnough@gmail.com>
2020-12-05 18:15:18 +0000

    added optional descipription param for undo fns (should come in useful when debigging later)

--- internal/static/undo.ts
+++ internal/static/undo.ts
@@ -3,17 +3,19 @@ import {undoLimit} from './settings.js';
 
 type Fn = () => Fn;
 
+type FnDesc = [Fn, string];
+
 type undoData = {
 	undos: Fn[];
 	redos: Fn[];
 	limit: Int;
 }
 
-const undos: Fn[] = [],
-      redos: Fn[] = [];
+const undos: FnDesc[] = [],
+      redos: FnDesc[] = [];
 
 export default {
-	"add": (undo: Fn) => {
+	"add": (undo: Fn, description: string) => {
 		redos.splice(0, redos.length);
 		if (undoLimit.value === 0) {
 			undos.splice(0, undos.length);
@@ -22,22 +24,24 @@ export default {
 		while (undoLimit.value !== -1 && undos.length >= undoLimit.value) {
 			undos.shift();
 		}
-		undos.push(undo);
+		undos.push([undo, description]);
 	},
 	"clear": () => {
 		undos.splice(0, undos.length);
 		redos.splice(0, redos.length);
 	},
 	"undo": () => {
-		const fn = undos.pop();
-		if (fn) {
-			redos.push(fn());
+		const fnDesc = undos.pop();
+		if (fnDesc) {
+			fnDesc[0] = fnDesc[0]();
+			redos.push(fnDesc);
 		}
 	},
 	"redo": () => {
-		const fn = redos.pop();
-		if (fn) {
-			undos.push(fn());
+		const fnDesc = redos.pop();
+		if (fnDesc) {
+			fnDesc[0] = fnDesc[0]();
+			undos.push(fnDesc);
 		}
 	}
 }
