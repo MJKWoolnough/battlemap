fa42d1f5d2c53fa481966cee2b3651c07cf9b9ad
Michael Woolnough <michael.woolnough@gmail.com>
2020-12-08 18:32:10 +0000

    created undo debug window

--- internal/static/undo.ts
+++ internal/static/undo.ts
@@ -1,7 +1,11 @@
-import {Int} from './types.js';
+import {Int, Uint} from './types.js';
 import {undoLimit} from './settings.js';
 import {SortNode} from './lib/ordered.js';
-import {ul, li} from './lib/html.js';
+import {ul, li, h1} from './lib/html.js';
+import {BoolSetting, JSONSetting} from './settings_types.js';
+import {isInt, isUint, requestShell} from './misc.js';
+import {WindowElement, windows} from './windows.js';
+import lang from './language.js';
 
 type Fn = () => Fn;
 
@@ -10,14 +14,32 @@ type FnDesc = {
 	node: HTMLLIElement;
 };
 
-type undoData = {
-	undos: Fn[];
-	redos: Fn[];
-	limit: Int;
-}
+type WindowData = [Int, Int, Uint, Uint];
 
 const undos = new SortNode<FnDesc>(ul()),
-      redos = new SortNode<FnDesc>(ul());
+      redos = new SortNode<FnDesc>(ul()),
+      undoWindowSettings = new JSONSetting<WindowData>("undo-window-settings", [0, 0, 200, 600], (v: any): v is WindowData => v instanceof Array && v.length === 4 && isInt(v[0]) && isInt(v[1]) && isUint(v[2]) && isUint(v[3])),
+      showWindow = new BoolSetting("undo-window-show"),
+      saveWindowData = function (this: WindowElement) {
+	undoWindowSettings.set([parseInt(this.style.getPropertyValue("--window-left") || "0"), parseInt(this.style.getPropertyValue("--window-top") || "0"), parseInt(this.style.getPropertyValue("--window-width") || "200"), parseInt(this.style.getPropertyValue("--window-height") || "600")]);
+      },
+      w = windows({"window-title": lang["UNDO_WINDOW_TITLE"], "--window-left": undoWindowSettings.value[0] + "px", "--window-top": undoWindowSettings.value[1] + "px", "--window-width": undoWindowSettings.value[2] + "px", "--window-height": undoWindowSettings.value[3] + "px", "resizable": true, "onmoved": saveWindowData, "onresized": saveWindowData, "onexit": () => showWindow.set(false)}, [
+	h1(lang["UNDO_WINDOW_UNDOS"]),
+	undos.node,
+	h1(lang["UNDO_WINDOW_REDOS"]),
+	redos.node
+      ]);
+
+if (showWindow.value) {
+	requestShell().appendChild(w);
+}
+
+Object.defineProperty(window, "showUndoWindow", {
+	"value": () => {
+		showWindow.set(true);
+		requestShell().appendChild(w);
+	}
+});
 
 export default {
 	"add": (fn: Fn, description: string) => {
