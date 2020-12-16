916367f899c14e4fc2623a51dee77b8a4986036e
Michael Woolnough <michael.woolnough@gmail.com>
2020-12-16 17:19:10 +0000

    rewrote queue to simply use promises

--- internal/static/queue.ts
+++ internal/static/queue.ts
@@ -1,22 +1,3 @@
-const queue: (() => Promise<any>)[] = [],
-      done = () => {
-	const fn = queue.shift();
-	if (fn) {
-		fn().finally(done);
-	} else {
-		locked = false;
-	}
-      };
+let p = Promise.resolve();
 
-let locked = false;
-
-export default (fn: () => Promise<any>) => {
-	if (locked) {
-		queue.push(fn);
-	} else {
-		locked = true;
-		fn().finally(done);
-	}
-};
-
-
+export default (fn: () => Promise<any>) => p = p.finally(fn);
