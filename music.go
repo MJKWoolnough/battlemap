2cc63df25ff14a7a6de6a37076350a8359d5c195
Michael Woolnough <michael.woolnough@gmail.com>
2021-01-01 14:01:02 +0000

    added pack writer

--- music.go
+++ music.go
@@ -1,6 +1,11 @@
 package battlemap
 
-import "time"
+import (
+	"compress/gzip"
+	"io"
+
+	"vimagination.zapto.org/byteio"
+)
 
 type musicTrack struct {
 	AssetID uint64
@@ -11,8 +16,27 @@ type musicTrack struct {
 type musicPack struct {
 	Tracks   []musicTrack
 	Repeat   uint64
-	PlayTime time.Time
+	PlayTime uint64
 	Playing  bool
+}
+
+func (m *musicPack) WriteTo(w io.Writer) (int64, error) {
+	g, err := gzip.NewWriterLevel(w, gzip.BestCompression)
+	if err != nil {
+		return 0, err
+	}
+	bw := byteio.StickyLittleEndianWriter{Writer: g}
+	bw.WriteUint64(uint64(len(m.Tracks)))
+	for _, t := range m.Tracks {
+		bw.WriteUint64(t.AssetID)
+		bw.WriteUint32(t.Volume)
+		bw.WriteInt32(t.Repeat)
+	}
+	bw.WriteUint64(m.Repeat)
+	bw.WriteUint64(m.PlayTime)
+	bw.WriteBool(m.Playing)
+	g.Close()
+	return bw.Count, bw.Err
 }
 
 type musicPacksDir struct {
