a2fd35da588e4140b6d86676ee72e63860a3fb73
Michael Woolnough <michael.woolnough@gmail.com>
2021-02-03 13:12:14 +0000

    added new fields to maps structure to store admin-set map start coords

--- maps_structure.go
+++ maps_structure.go
@@ -13,6 +13,8 @@ import (
 type levelMap struct {
 	Width      uint64                     `json:"width"`
 	Height     uint64                     `json:"height"`
+	StartX     uint64                     `json:"startX"`
+	StartY     uint64                     `json:"startY"`
 	GridType   uint8                      `json:"gridType"`
 	GridSize   uint64                     `json:"gridSize"`
 	GridStroke uint64                     `json:"gridStroke"`
@@ -59,6 +61,8 @@ func (l *levelMap) writeJSON() {
 	l.JSON = l.JSON[:0]
 	l.JSON = strconv.AppendUint(append(l.JSON[:0], "{\"width\":"...), l.Width, 10)
 	l.JSON = strconv.AppendUint(append(l.JSON, ",\"height\":"...), l.Height, 10)
+	l.JSON = strconv.AppendUint(append(l.JSON, ",\"startX\":"...), l.StartX, 10)
+	l.JSON = strconv.AppendUint(append(l.JSON, ",\"startY\":"...), l.StartY, 10)
 	l.JSON = appendNum(append(l.JSON, ",\"gridType\":"...), l.GridType)
 	l.JSON = strconv.AppendUint(append(l.JSON, ",\"gridSize\":"...), l.GridSize, 10)
 	l.JSON = strconv.AppendUint(append(l.JSON, ",\"gridStroke\":"...), l.GridStroke, 10)
