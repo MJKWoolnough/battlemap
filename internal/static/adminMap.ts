5b55535bbde3d0cdff3cfa60e0af213270b3a06f
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-09 08:28:02 +0000

    updated to new class setting in dom module

--- internal/static/adminMap.ts
+++ internal/static/adminMap.ts
@@ -554,7 +554,7 @@ export default function(base: HTMLElement) {
 	      },
 	      selectToken = (newToken: SVGToken | SVGShape | SVGDrawing) => {
 		globals.selected.token = newToken;
-		autoFocus(createSVG(outline, {"transform": newToken.transformString(false), "style": undefined, "class!": `cursor_${((newToken.rotation + 143) >> 5) % 4}`}));
+		autoFocus(createSVG(outline, {"transform": newToken.transformString(false), "style": undefined, "class": `cursor_${((newToken.rotation + 143) >> 5) % 4}`}));
 		window.setTimeout(() => outline.setAttribute("style", `--outline-width: ${newToken.width}px; --outline-height: ${newToken.height}px; --zoom: ${panZoom.zoom}`)); // TODO: Remove once Firefox bug is fixed!
 		tokenMousePos.x = newToken.x;
 		tokenMousePos.y = newToken.y;
