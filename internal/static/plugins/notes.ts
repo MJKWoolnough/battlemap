8cd3f92dcbdd317e9289851c1d2d6f162244ae92
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-10 17:12:37 +0000

    all keys in plugin data will be string-encoded IDs, which we'll store in a Map to save conversions

--- internal/static/plugins/notes.ts
+++ internal/static/plugins/notes.ts
@@ -23,7 +23,6 @@ if (isAdmin()) {
 			if (id > lastID) {
 				lastID = id;
 			}
-			pages.set(id, this);
 		}
 		show() {
 			// code here
@@ -39,7 +38,7 @@ if (isAdmin()) {
 	      lang = langs[language.value] ?? defaultLanguage,
 	      importName = (import.meta as MetaURL).url.split("/").pop()!,
 	      icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84 96"%3E%3Crect x="60" y="6" width="24" height="90" fill="%23888" rx="10" /%3E%3Crect x="1" y="6" width="80" height="90" stroke="%23000" fill="%23fff" rx="10" /%3E%3Cg id="h"%3E%3Ccircle cx="16" cy="11" r="2" fill="%23333" /%3E%3Cellipse cx="15" cy="6" rx="3" ry="5" stroke="%23aaa" stroke-width="2" fill="none" stroke-dasharray="0 5 15" /%3E%3C/g%3E%3Cuse href="%23h" x="10" /%3E%3Cuse href="%23h" x="20" /%3E%3Cuse href="%23h" x="30" /%3E%3Cuse href="%23h" x="40" /%3E%3Cuse href="%23h" x="50" /%3E%3Cpath d="M11,25 h60 M11,40 h60 M11,55 h60 M11,70 h30" stroke="%23000" stroke-width="4" stroke-linecap="round" /%3E%3C/svg%3E',
-	      pages = new Map<Uint, NoteItem>(),
+	      pages = new Map<Uint, KeystoreData<string>>(),
 	      defaultSettings = {"": {"user": false, "data": {"folders": {}, "items": {}}}} as Settings,
 	      isFolderItems = (data: any): data is FolderItems => {
 		if (!(data instanceof Object) || !(data["folders"] instanceof Object) || !(data["items"] instanceof Object)) {
@@ -65,8 +64,12 @@ if (isAdmin()) {
 		for (const k in data) {
 			if (k === "") {
 				continue;
-			} else if (!(data[k] instanceof Object) || data[k].user !== false || typeof data[k].data !== "string") {
-				return defaultSettings
+			} else {
+				const id = parseInt(k);
+				if (isNaN(id) || !(data[k] instanceof Object) || data[k].user !== false || typeof data[k].data !== "string") {
+					return defaultSettings
+				}
+				pages.set(id, data[k]);
 			}
 		}
 		return data as Settings;
