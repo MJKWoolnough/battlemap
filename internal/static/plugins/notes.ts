1968a1703e74d904bfe7640975f585e5ce624063
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-10 08:52:08 +0000

    implemented createFolder

--- internal/static/plugins/notes.ts
+++ internal/static/plugins/notes.ts
@@ -1,4 +1,4 @@
-import type {FolderItems, IDName, Uint} from '../types.js';
+import type {FolderItems, KeystoreData, IDName, Uint} from '../types.js';
 import type {Folder} from '../folders.js';
 import {addPlugin, getSettings} from '../plugins.js';
 import {div} from '../lib/html.js';
@@ -6,13 +6,14 @@ import {Subscription} from '../lib/inter.js';
 import {isAdmin, isUint} from '../shared.js';
 import {language} from '../language.js';
 import {Item, Root} from '../folders.js';
+import {rpc} from '../rpc.js';
 
 type MetaURL = {
 	url: string;
 }
 
-type Settings = Record<string, string> & {
-	"": FolderItems;
+type Settings = Record<string, KeystoreData<string>> & {
+	"": KeystoreData<FolderItems>;
 }
 
 if (isAdmin()) {
@@ -39,7 +40,7 @@ if (isAdmin()) {
 	      importName = (import.meta as MetaURL).url.split("/").pop()!,
 	      icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84 96"%3E%3Crect x="60" y="6" width="24" height="90" fill="%23888" rx="10" /%3E%3Crect x="1" y="6" width="80" height="90" stroke="%23000" fill="%23fff" rx="10" /%3E%3Cg id="h"%3E%3Ccircle cx="16" cy="11" r="2" fill="%23333" /%3E%3Cellipse cx="15" cy="6" rx="3" ry="5" stroke="%23aaa" stroke-width="2" fill="none" stroke-dasharray="0 5 15" /%3E%3C/g%3E%3Cuse href="%23h" x="10" /%3E%3Cuse href="%23h" x="20" /%3E%3Cuse href="%23h" x="30" /%3E%3Cuse href="%23h" x="40" /%3E%3Cuse href="%23h" x="50" /%3E%3Cpath d="M11,25 h60 M11,40 h60 M11,55 h60 M11,70 h30" stroke="%23000" stroke-width="4" stroke-linecap="round" /%3E%3C/svg%3E',
 	      pages = new Map<Uint, NoteItem>(),
-	      defaultSettings = {"": {"folders": {}, "items": {}}},
+	      defaultSettings = {"": {"user": false, "data": {"folders": {}, "items": {}}}} as Settings,
 	      isFolderItems = (data: any): data is FolderItems => {
 		if (!(data instanceof Object) || !(data["folders"] instanceof Object) || !(data["items"] instanceof Object)) {
 			return false;
@@ -81,9 +82,22 @@ if (isAdmin()) {
 	      waitFolderAdded = subFn<string>(),
 	      unusedWait = new Subscription<any>(() => {}),
 	      settings = checkSettings(getSettings(importName)),
-	      root = new Root(settings[""], lang["MENU_TITLE"], {
-		"list":        ()          => Promise.resolve(settings[""]),
-		"createFolder": path       => Promise.resolve(path),
+	      root = new Root(settings[""].data, lang["MENU_TITLE"], {
+		"list":        ()          => Promise.resolve(settings[""].data),
+		"createFolder": path       => {
+			const parts = path.split("/");
+			let currPath = settings[""].data;
+			Loop:
+			for (const p of parts) {
+				if (!currPath.folders[p]) {
+					currPath = currPath.folders[p] = {"folders": {}, "items": {}};
+				} else {
+					currPath = currPath.folders[p];
+				}
+			}
+			rpc.pluginSetting(importName, {"": settings[""]}, []);
+			return Promise.resolve(path)
+		},
 		"move":        (_from, to) => Promise.resolve(to),
 		"moveFolder":  (_from, to) => Promise.resolve(to),
 		"remove":       _path      => Promise.resolve(),
