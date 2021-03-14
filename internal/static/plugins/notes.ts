7095baa9544061c44123bc25a80bc473e6009530
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-14 10:13:52 +0000

    made the notes draggable (to be dragged to link in other notes)

--- internal/static/plugins/notes.ts
+++ internal/static/plugins/notes.ts
@@ -7,7 +7,7 @@ import {createHTML, br, button, div, input, span, style, textarea} from '../lib/
 import {Subscription} from '../lib/inter.js';
 import {isAdmin, isUint, labels} from '../shared.js';
 import {language} from '../language.js';
-import {Folder, Item, Root} from '../folders.js';
+import {Folder, DraggableItem, Root} from '../folders.js';
 import {rpc, handleError} from '../rpc.js';
 import {shell, windows} from '../windows.js';
 import {all} from '../lib/bbcode_tags.js';
@@ -45,18 +45,22 @@ const defaultLanguage = {
 	      "en-GB": defaultLanguage
       },
       lang = langs[language.value] ?? defaultLanguage,
-      icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84 96"%3E%3Crect x="60" y="6" width="24" height="90" fill="%23888" rx="10" /%3E%3Crect x="1" y="6" width="80" height="90" stroke="%23000" fill="%23fff" rx="10" /%3E%3Cg id="h"%3E%3Ccircle cx="16" cy="11" r="2" fill="%23333" /%3E%3Cellipse cx="15" cy="6" rx="3" ry="5" stroke="%23aaa" stroke-width="2" fill="none" stroke-dasharray="0 5 15" /%3E%3C/g%3E%3Cuse href="%23h" x="10" /%3E%3Cuse href="%23h" x="20" /%3E%3Cuse href="%23h" x="30" /%3E%3Cuse href="%23h" x="40" /%3E%3Cuse href="%23h" x="50" /%3E%3Cpath d="M11,25 h60 M11,40 h60 M11,55 h60 M11,70 h30" stroke="%23000" stroke-width="4" stroke-linecap="round" /%3E%3C/svg%3E';
+      icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84 96" width="84" height="96" %3E%3Crect x="60" y="6" width="24" height="90" fill="%23888" rx="10" /%3E%3Crect x="1" y="6" width="80" height="90" stroke="%23000" fill="%23fff" rx="10" /%3E%3Cg id="h"%3E%3Ccircle cx="16" cy="11" r="2" fill="%23333" /%3E%3Cellipse cx="15" cy="6" rx="3" ry="5" stroke="%23aaa" stroke-width="2" fill="none" stroke-dasharray="0 5 15" /%3E%3C/g%3E%3Cuse href="%23h" x="10" /%3E%3Cuse href="%23h" x="20" /%3E%3Cuse href="%23h" x="30" /%3E%3Cuse href="%23h" x="40" /%3E%3Cuse href="%23h" x="50" /%3E%3Cpath d="M11,25 h60 M11,40 h60 M11,55 h60 M11,70 h30" stroke="%23000" stroke-width="4" stroke-linecap="round" /%3E%3C/svg%3E';
 
 register("plugin-notes", [icon, lang["NOTE"]]);
 
 if (isAdmin()) {
-	class NoteItem extends Item {
+	class NoteItem extends DraggableItem {
 		window: WindowElement | null = null;
+		share: (() => void) | null = null;
 		constructor(parent: Folder, id: Uint, name: string) {
 			super(parent, id, name);
+			this.image.setAttribute("src", icon);
 			notes.set(id, this);
 		}
-		share: (() => void) | null = null;
+		dragName() {
+			return "pluginnote";
+		}
 		show() {
 			if (this.window) {
 				this.window.focus();
