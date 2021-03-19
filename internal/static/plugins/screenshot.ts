c625b2f9d2ad986a92b2f628e1bbe50e1b9426fd
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-19 18:21:00 +0000

    adjusted for current zoom level

--- internal/static/plugins/screenshot.ts
+++ internal/static/plugins/screenshot.ts
@@ -2,8 +2,9 @@ import {canvas, img} from '../lib/html.js';
 import {shell, windows} from '../windows.js';
 import {colour2RGBA} from '../colours.js';
 import {globals} from '../shared.js';
+import {panZoom} from '../tools_default.js';
 
-const walkElements = (n: Element, ctx: CanvasRenderingContext2D, p: Promise<void>) => {
+const walkElements = (n: Element, ctx: CanvasRenderingContext2D, ctm: DOMMatrix, p: Promise<void>) => {
 	const styles = window.getComputedStyle(n);
 	for (const s of styles) {
 		if (s === "display") {
@@ -16,14 +17,14 @@ const walkElements = (n: Element, ctx: CanvasRenderingContext2D, p: Promise<void
 	switch (n.nodeName) {
 	case "image":
 		p = p.then(() => {
-			ctx.setTransform((n as SVGImageElement).getCTM()!);
+			ctx.setTransform(ctm.multiply((n as SVGImageElement).getCTM()!));
 			ctx.drawImage(n as SVGImageElement, 0, 0, parseInt(n.getAttribute("width")!), parseInt(n.getAttribute("height")!));
 			ctx.resetTransform();
 		});
 		break;
 	case "rect":
 		if (n.getAttribute("fill")?.startsWith("url(#Pattern_")) {
-			const pattern = globals.definitions.list.get(n.getAttribute("fill")!.slice(5, -1))?.firstChild
+			const pattern = globals.definitions.list.get(n.getAttribute("fill")!.slice(5, -1))?.firstChild;
 			if (pattern && pattern instanceof SVGImageElement) {
 				p = p.then(() => {
 					const fs = ctx.fillStyle,
@@ -32,7 +33,7 @@ const walkElements = (n: Element, ctx: CanvasRenderingContext2D, p: Promise<void
 					      oc = canvas({width, height});
 					oc.getContext("2d")!.drawImage(pattern, 0, 0, width, height);
 					ctx.fillStyle = ctx.createPattern(oc, "repeat")!;
-					ctx.setTransform((n as SVGRectElement).getCTM()!);
+					ctx.setTransform(ctm.multiply((n as SVGRectElement).getCTM()!));
 					ctx.fillRect(0, 0, parseInt(n.getAttribute("width")!), parseInt(n.getAttribute("height")!));
 					ctx.resetTransform();
 					ctx.fillStyle = fs;
@@ -70,7 +71,7 @@ const walkElements = (n: Element, ctx: CanvasRenderingContext2D, p: Promise<void
 		return p;
 	}
 	for (const c of n.children) {
-		p = walkElements(c, ctx, p);
+		p = walkElements(c, ctx, ctm, p);
 	}
 	return p;
 };
@@ -79,10 +80,11 @@ document.body.addEventListener("keydown", (e: KeyboardEvent) => {
 	if (e.key === "PrintScreen") {
 		const {root, mapData: {width, height}} = globals,
 		      c = canvas({width, height}),
-		      ctx = c.getContext("2d")!;
+		      ctx = c.getContext("2d")!,
+		      ctm = new DOMMatrix().scaleSelf(panZoom.zoom, panZoom.zoom, 1, width / 2, height / 2).inverse();
 		let p = Promise.resolve();
 		for (const c of root.children) {
-			p = walkElements(c, ctx, p);
+			p = walkElements(c, ctx, ctm, p);
 		}
 		p.then(() => shell.appendChild(windows(c)));
 		e.preventDefault();
