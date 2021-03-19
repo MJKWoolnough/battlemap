7b8dc76307f29809746092cfa59ab40eadac55bd
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-19 15:01:04 +0000

    used promises to order drawing events properly

--- internal/static/plugins/screenshot.ts
+++ internal/static/plugins/screenshot.ts
@@ -3,36 +3,40 @@ import {shell, windows} from '../windows.js';
 import {colour2RGBA} from '../colours.js';
 import {globals} from '../shared.js';
 
-const walkElements = (n: Element, ctx: CanvasRenderingContext2D) => {
+const walkElements = (n: Element, ctx: CanvasRenderingContext2D, p: Promise<void>) => {
 	const styles = window.getComputedStyle(n);
 	for (const s of styles) {
 		if (s === "display") {
 			if (styles.getPropertyValue(s) === "none") {
-				return;
+				return p;
 			}
 		}
 	}
 	const id = n.getAttribute("id");
 	switch (n.nodeName) {
 	case "image":
-		ctx.setTransform((n as SVGImageElement).getCTM()!);
-		ctx.drawImage(n as SVGImageElement, 0, 0, parseInt(n.getAttribute("width")!), parseInt(n.getAttribute("height")!));
-		ctx.resetTransform();
+		p = p.then(() => {
+			ctx.setTransform((n as SVGImageElement).getCTM()!);
+			ctx.drawImage(n as SVGImageElement, 0, 0, parseInt(n.getAttribute("width")!), parseInt(n.getAttribute("height")!));
+			ctx.resetTransform();
+		});
 		break;
 	case "rect":
 		if (n.getAttribute("fill")?.startsWith("url(#Pattern_")) {
-			const p = globals.definitions.list.get(n.getAttribute("fill")!.slice(5, -1));
-			if (p && p.firstChild instanceof SVGImageElement) {
-				const fs = ctx.fillStyle,
-				      width = parseInt(p.firstChild.getAttribute("width")!),
-				      height = parseInt(p.firstChild.getAttribute("height")!),
-				      oc = canvas({width, height});
-				oc.getContext("2d")!.drawImage(p.firstChild, 0, 0, width, height);
-				ctx.fillStyle = ctx.createPattern(oc, "repeat")!;
-				ctx.setTransform((n as SVGRectElement).getCTM()!);
-				ctx.fillRect(0, 0, parseInt(n.getAttribute("width")!), parseInt(n.getAttribute("height")!));
-				ctx.resetTransform();
-				ctx.fillStyle = fs;
+			const pattern = globals.definitions.list.get(n.getAttribute("fill")!.slice(5, -1))?.firstChild
+			if (pattern && pattern instanceof SVGImageElement) {
+				p = p.then(() => {
+					const fs = ctx.fillStyle,
+					      width = parseInt(pattern.getAttribute("width")!),
+					      height = parseInt(pattern.getAttribute("height")!),
+					      oc = canvas({width, height});
+					oc.getContext("2d")!.drawImage(pattern, 0, 0, width, height);
+					ctx.fillStyle = ctx.createPattern(oc, "repeat")!;
+					ctx.setTransform((n as SVGRectElement).getCTM()!);
+					ctx.fillRect(0, 0, parseInt(n.getAttribute("width")!), parseInt(n.getAttribute("height")!));
+					ctx.resetTransform();
+					ctx.fillStyle = fs;
+				});
 			}
 			break;
 		}
@@ -41,26 +45,32 @@ const walkElements = (n: Element, ctx: CanvasRenderingContext2D) => {
 		break;
 	case "g":
 		if (id === "layerGrid") {
-			const {width, height} = globals.mapData;
-			img({"src": `data:image/svg+xml,${encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}"><defs>${globals.definitions.list.get("grid")!.outerHTML}</defs>${n.innerHTML}</svg>`)}`, width, height, "onload": function(this: HTMLImageElement) {
-				ctx.drawImage(this, 0, 0);
-			}});
-			return;
+			p = p.then(() => new Promise(sfn => {
+				const {width, height} = globals.mapData;
+				img({"src": `data:image/svg+xml,${encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}"><defs>${globals.definitions.list.get("grid")!.outerHTML}</defs>${n.innerHTML}</svg>`)}`, width, height, "onload": function(this: HTMLImageElement) {
+					ctx.drawImage(this, 0, 0);
+					sfn();
+				}});
+			}));
+			return p;
 		} else if (id === "layerLight") {
-			const {lightColour, width, height} = globals.mapData,
-			      fs = ctx.fillStyle;
-			ctx.fillStyle = colour2RGBA(lightColour);
-			ctx.fillRect(0, 0, width, height);
-			ctx.fillStyle = fs;
-			return;
+			p = p.then(() => {
+				const {lightColour, width, height} = globals.mapData,
+				      fs = ctx.fillStyle;
+				ctx.fillStyle = colour2RGBA(lightColour);
+				ctx.fillRect(0, 0, width, height);
+				ctx.fillStyle = fs;
+			});
+			return p;
 		}
 		break;
 	case "defs":
-		return;
+		return p;
 	}
 	for (const c of n.children) {
-		walkElements(c, ctx);
+		p = walkElements(c, ctx, p);
 	}
+	return p;
 };
 
 document.body.addEventListener("keydown", (e: KeyboardEvent) => {
@@ -68,10 +78,11 @@ document.body.addEventListener("keydown", (e: KeyboardEvent) => {
 		const {root, mapData: {width, height}} = globals,
 		      c = canvas({width, height}),
 		      ctx = c.getContext("2d")!;
+		let p = Promise.resolve();
 		for (const c of root.children) {
-			walkElements(c, ctx);
+			p = walkElements(c, ctx, p);
 		}
-		shell.appendChild(windows(c));
+		p.then(() => shell.appendChild(windows(c)));
 		e.preventDefault();
 	}
 });
