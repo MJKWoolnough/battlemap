8d77faf0b5b1a5fa11b9e1d82efa77e6007cc59f
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-26 08:10:26 +0000

    used new checkInt to check parsed numbers

--- internal/static/colours.ts
+++ internal/static/colours.ts
@@ -2,17 +2,17 @@ import type {Colour} from './types.js';
 import type {ShellElement, WindowElement} from './windows.js';
 import {br, button, div, h1, input} from './lib/html.js';
 import {windows, shell} from './windows.js';
-import {isUint, labels} from './shared.js';
+import {checkInt, isUint, labels} from './shared.js';
 import lang from './language.js';
 
-export const hex2Colour = (hex: string, a = 255): Colour => Object.freeze({"r": parseInt(hex.slice(1, 3), 16) || 0, "g": parseInt(hex.slice(3, 5), 16) || 0, "b": parseInt(hex.slice(5, 7), 16) || 0, a}),
+export const hex2Colour = (hex: string, a = 255): Colour => Object.freeze({"r": checkInt(parseInt(hex.slice(1, 3), 16), 0, 255), "g": checkInt(parseInt(hex.slice(3, 5), 16), 0, 255), "b": checkInt(parseInt(hex.slice(5, 7), 16), 0, 255), a}),
 colour2Hex = (c: Colour) => `#${c.r.toString(16).padStart(2, "0")}${c.g.toString(16).padStart(2, "0")}${c.b.toString(16).padStart(2, "0")}`,
 colour2RGBA = (c: Colour) => `rgba(${c.r.toString()}, ${c.g.toString()}, ${c.b.toString()}, ${(c.a / 255).toString()})`,
 noColour = Object.freeze({"r": 0, "g": 0, "b": 0, "a": 0}),
 colourPicker = (parent: WindowElement | ShellElement, title: string, colour: Colour = noColour, icon?: string) => new Promise<Colour>((resolve, reject) => {
 	const checkboard = div({"class": "checkboard"}),
 	      preview = checkboard.appendChild(div({"style": `background-color: ${colour2RGBA(colour)}`})),
-	      updatePreview = () => preview.style.setProperty("background-color", colour2RGBA(hex2Colour(colourInput.value, parseInt(alphaInput.value)))),
+	      updatePreview = () => preview.style.setProperty("background-color", colour2RGBA(hex2Colour(colourInput.value, checkInt(parseInt(alphaInput.value), 0, 255, 255)))),
 	      colourInput = input({"id": "colourPick_", "type": "color", "value": colour2Hex(colour), "onchange": updatePreview}),
 	      alphaInput = input({"id": "alphaPick_", "type": "range", "min": "0", "max": "255", "step": "1","value": colour.a, "oninput": updatePreview}),
 	      window = windows({"window-icon": icon, "window-title": title, "class": "lightChange", "onremove": reject}, [
@@ -25,7 +25,7 @@ colourPicker = (parent: WindowElement | ShellElement, title: string, colour: Col
 		button(lang["COLOUR_UPDATE"], {"onclick": function(this: HTMLButtonElement) {
 			this.toggleAttribute("disabled", true);
 			window.remove();
-			resolve(hex2Colour(colourInput.value, parseInt(alphaInput.value)));
+			resolve(hex2Colour(colourInput.value, checkInt(parseInt(alphaInput.value), 0, 255, 255)));
 		}})
 	      ]);
 	parent.addWindow(window);
