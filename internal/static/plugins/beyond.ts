2935b29e44535d8398bb57e41f76248ac812b218
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-13 17:23:35 +0100

    added button to remove character to reset plugin

--- internal/static/plugins/beyond.ts
+++ internal/static/plugins/beyond.ts
@@ -6,6 +6,7 @@ import mainLang, {language} from '../language.js';
 import {enterKey} from '../shared.js';
 import {shell} from '../windows.js';
 import {JSONSetting} from '../settings_types.js';
+import {getSymbol} from '../symbols.js';
 
 type BeyondData = {
 	name: string;
@@ -19,6 +20,7 @@ const defaultLanguage = {
 	"BEYOND_INVALID_ID_LONG": "Unable to determine Beyond ID, please re-enter",
 	"BEYOND_LOAD": "Load Beyond Data",
 	"ERROR_INVALID_FILE": "Invalid Beyond JSON file",
+	"DELETE": "Delete Character",
 	"DOWNLOAD": "Download Beyond Data",
 	"PLUGIN_NAME": "Beyond",
 	"UPLOAD": "Upload Beyond Data"
@@ -27,6 +29,7 @@ const defaultLanguage = {
 	"en-GB": defaultLanguage
       },
       lang = langs[language.value] ?? defaultLanguage,
+      remove = getSymbol("remove")!,
       icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 950 950"%3E%3Crect width="100%25" height="100%25" fill="%23000" /%3E%3Cpath d="M30,35 l78,81 v276 h-106 l107,137 v302 l-78,81 h553 c104,0 194,-18 257,-56 c63,-38 93,-92 93,-165 v-50 c-1,-42 -17,-80 -49,-112 c-33,-32 -77,-56 -134,-72 c23,-6 44,-15 63,-27 c20,-12 35,-24 48,-39 s23,-30 30,-47 c8,-15 11,-30 11,-45 v-29 c0,-35 -8,-68 -24,-96 c-17,-29 -39,-54 -69,-74 c-30,-21 -72,-36 -114 -48 c-42,-11 -89,-17 -140,-17 z M333,163 h141 c41,0 74,6 99,18 c26,12 38,25 38,68 v29 c0,26 -9,45 -29,60 c-20,15 -45,23 -78,23 h-171 z M333,558 h179 c33,0 62,8 89,23 c26,15 39,35 39,60 v42 c0,18 -3,33 -11,44 c-8,11 -17,20 -29,27 c-12,6 -26,11 -42,14 c-17,2 -33,3 -50,3 h-176 z" fill="%23f00" stroke="%23000" /%3E%3C/svg%3E',
       beyondData = new JSONSetting<BeyondData | null>("plugin-beyond", null, (data: any): data is BeyondData => {
 	if (typeof data !== "object") {
@@ -78,7 +81,13 @@ const defaultLanguage = {
       })(),
       show = (data: BeyondData) => {
 	createHTML(clearElement(baseDiv), [
-		h1(data.name)
+		h1([
+			data.name,
+			remove({"title": lang["DELETE"], "style": "width: 1em; height: 1em;cursor: pointer", "onclick": () => {
+				beyondData.remove();
+				noData();
+			}})
+		])
 	]);
       };
 
