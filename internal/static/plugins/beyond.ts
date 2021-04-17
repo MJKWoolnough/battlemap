3061aab5c536e61139604d257c53c99ffea3244a
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-17 18:00:05 +0100

    added speed parsing (needs checking)

--- internal/static/plugins/beyond.ts
+++ internal/static/plugins/beyond.ts
@@ -331,6 +331,10 @@ const defaultLanguage = {
 								const bonusAttr = bonusAttrs2Attrs[bonus];
 								if (bonusAttr) {
 									parsed.attrs[bonusAttr] += mod["value"];
+								} else if (bonus === "speed") {
+									for (const speed in parsed.speed) {
+										parsed.speed[speed as Speed]! += mod["value"];
+									}
 								}
 							}
 						}
