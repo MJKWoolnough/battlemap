5d68e078a25e4b539b949031fe80ff2faa10228e
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-22 11:47:57 +0100

    added tool options

--- internal/static/plugins/spells.ts
+++ internal/static/plugins/spells.ts
@@ -1,3 +1,4 @@
+import {br, div, input, label} from '../lib/html.js';
 import {svg, g, path, use} from '../lib/svg.js';
 import {isAdmin} from '../shared.js';
 import {addTool} from '../tools.js';
@@ -5,13 +6,21 @@ import {language} from '../language.js';
 
 if (isAdmin()) {
 	const defaultLanguage = {
+		"SPELL_SIZE": "Spell Size",
+		"SPELL_TYPE_CIRCLE": "Circle Spell",
+		"SPELL_TYPE_CONE": "Cone Spell",
+		"SPELL_TYPE_CUBE": "Cube Spell",
 		"TITLE": "Spell Effects",
 	      },
 	      langs: Record<string, typeof defaultLanguage> = {
 		"en-GB": defaultLanguage
 	      },
 	      lang = langs[language.value] ?? defaultLanguage,
-	      sparkID = "plugin-spell-spark";
+	      sparkID = "plugin-spell-spark",
+	      circleSpell = input({"type": "radio", "id": "plugin-spell-type-circle", "name": "plugin-spell-type", "checked": true}),
+	      coneSpell = input({"type": "radio", "id": "plugin-spell-type-cone", "name": "plugin-spell-type"}),
+	      cubeSpell = input({"type": "radio", "id": "plugin-spell-type-cube", "name": "plugin-spell-type"}),
+	      size = input({"type": "number", "id": "plugin-spell-size", "min": 0, "value": 10});
 	addTool({
 		"name": lang["TITLE"],
 		"icon": svg({"viewBox": "0 0 100 100"}, g({"style": "fill: currentColor; stroke: currentColor"}, [
@@ -21,5 +30,18 @@ if (isAdmin()) {
 			use({"href": `#${sparkID}`, "transform": "translate(-45, 30) scale(1.2)"}),
 			use({"href": `#${sparkID}`, "transform": "translate(-30, -5) scale(0.8)"}),
 		])),
+		"options": div([
+			label({"for": "plugin-spell-type-circle"}, `${lang["SPELL_TYPE_CIRCLE"]}: `),
+			circleSpell,
+			br(),
+			label({"for": "plugin-spell-type-cone"}, `${lang["SPELL_TYPE_CONE"]}: `),
+			coneSpell,
+			br(),
+			label({"for": "plugin-spell-type-cube"}, `${lang["SPELL_TYPE_CUBE"]}: `),
+			cubeSpell,
+			br(),
+			label({"for": "plugin-spell-size"}, `${lang["SPELL_SIZE"]}: `),
+			size
+		]),
 	});
 }
