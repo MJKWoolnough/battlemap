03b91721b0cb5d684e0bb2b2eac1c228504756e1
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-22 11:34:06 +0100

    added tool icon

--- internal/static/plugins/spells.ts
+++ internal/static/plugins/spells.ts
@@ -1,4 +1,4 @@
-import {svg} from '../lib/svg.js';
+import {svg, g, path, use} from '../lib/svg.js';
 import {isAdmin} from '../shared.js';
 import {addTool} from '../tools.js';
 import {language} from '../language.js';
@@ -10,9 +10,16 @@ if (isAdmin()) {
 	      langs: Record<string, typeof defaultLanguage> = {
 		"en-GB": defaultLanguage
 	      },
-	      lang = langs[language.value] ?? defaultLanguage;
+	      lang = langs[language.value] ?? defaultLanguage,
+	      sparkID = "plugin-spell-spark";
 	addTool({
 		"name": lang["TITLE"],
-		"icon": svg()
+		"icon": svg({"viewBox": "0 0 100 100"}, g({"style": "fill: currentColor; stroke: currentColor"}, [
+			path({"d": "M60,35 v70 h-20 v-100 h20 v30 h-20 v-30 h20", "fill-rule": "evenodd", "transform": "rotate(-45, 50, 50)"}),
+			path({"d": "M50,10 q0,10 10,10 q-10,0 -10,10 q0,-10 -10,-10 q10,0 10,-10", "id": sparkID}),
+			use({"href": `#${sparkID}`, "transform": "translate(5, 0) scale(1.5)"}),
+			use({"href": `#${sparkID}`, "transform": "translate(-45, 30) scale(1.2)"}),
+			use({"href": `#${sparkID}`, "transform": "translate(-30, -5) scale(0.8)"}),
+		])),
 	});
 }
