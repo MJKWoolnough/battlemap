881f672e2b6c3d74593e673d19ee74b50eb5af8a
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-02 10:00:54 +0100

    correctd bbcode regen not creating correct HTML structure

--- internal/static/plugins/notes.ts
+++ internal/static/plugins/notes.ts
@@ -3,7 +3,7 @@ import type {WindowElement} from '../windows.js';
 import type {Parsers, Tokeniser} from '../lib/bbcode.js';
 import {addPlugin, getSettings, pluginName} from '../plugins.js';
 import {clearElement} from '../lib/dom.js';
-import {createHTML, br, button, div, input, span, textarea} from '../lib/html.js';
+import {br, button, div, input, span, textarea} from '../lib/html.js';
 import {Subscription} from '../lib/inter.js';
 import {addCSS, isAdmin, isUint, labels} from '../shared.js';
 import mainLang, {language} from '../language.js';
@@ -106,7 +106,7 @@ if (isAdmin()) {
 							page.data = {"contents": contents.value, "share": share.checked};
 							pages.set(this.id, page);
 							rpc.pluginSetting(importName, {[this.id+""]: page}, []);
-							clearElement(this.window!).appendChild(bbcode(createHTML(null), allTags, contents.value));
+							clearElement(this.window!).appendChild(bbcode(div({"class": "plugin-notes"}), allTags, contents.value));
 							this.setShareButton();
 						}}, lang["NOTE_SAVE"])
 					      ]);
