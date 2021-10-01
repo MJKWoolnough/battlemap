9502fa3565f42ea58c2a3371f39db2c7e003262f
Michael Woolnough <michael.woolnough@gmail.com>
2021-10-01 11:18:52 +0100

    added multi-place icon

--- internal/static/tools_multiplace.ts
+++ internal/static/tools_multiplace.ts
@@ -1,10 +1,17 @@
-import {svg, title} from './lib/svg.js';
+import {svg, circle, path, title} from './lib/svg.js';
 import {addTool} from './tools.js';
 import {defaultMouseWheel} from './tools_default.js';
 import lang from './language.js';
 
 addTool({
 	"name": lang["TOOL_MULTIPLACE"],
-	"icon": svg(title(lang["TOOL_MULTIPLACE"])),
+	"icon": svg({"viewBox": "0 0 100 100", "style": "stroke: currentColor", "fill": "none"}, [
+		title(lang["TOOL_MULTIPLACE"]),
+		circle({"cx": 15, "cy": 15, "r": 10, "stroke-dasharray": "50 40", "stroke-dashoffset": 75}),
+		circle({"cx": 28, "cy": 28, "r": 10, "stroke-dasharray": "50 40", "stroke-dashoffset": 75}),
+		circle({"cx": 42, "cy": 42, "r": 10}),
+		path({"d": "M28,77 l26,-26 a3,2 0,0,1 4,0 l40,40 M74,67 l10,-10 a3,2 0,0,1 5,0 l9,9 M14,63 l15,-15 M1,50 l15,-15"}),
+		path({"d": "M98,39 s0,-10 -10,-10 h-49 s-10,0 -10,10 v49 s0,10 10,10 h49 s10,0 10,-10 z M84,28 v-3 s0,-10 -10,-10 h-49 s-10,0 -10,10 v49 s0,10 10,10 h3 M71,15 v-3 s0,-10 -10,-10 h-49 s-10,0 -10,10 v49 s0,10 10,10 h3", "stroke-width": 4})
+	]),
 	"mapMouseWheel": defaultMouseWheel
 });
