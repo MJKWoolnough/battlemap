2633795b4293cb90bd16edb5e0dd3fa8dbc70d60
Michael Woolnough <michael.woolnough@gmail.com>
2021-10-02 10:14:44 +0100

    added main GUI options for multiplace tool

--- internal/static/tools_multiplace.ts
+++ internal/static/tools_multiplace.ts
@@ -1,7 +1,15 @@
+import type {CharacterToken} from './types.js';
+import {button, div, img, label} from './lib/html.js';
 import {svg, circle, path, title} from './lib/svg.js';
 import {addTool} from './tools.js';
 import {defaultMouseWheel} from './tools_default.js';
+import {getToken} from './map_fns.js';
 import lang from './language.js';
+
+const i = img(),
+      cursor = img();
+
+let token: CharacterToken | undefined;
 
 addTool({
 	"name": lang["TOOL_MULTIPLACE"],
@@ -12,6 +20,22 @@ addTool({
 		circle({"cx": 42, "cy": 42, "r": 10}),
 		path({"d": "M28,77 l26,-26 a3,2 0,0,1 4,0 l40,40 M74,67 l10,-10 a3,2 0,0,1 5,0 l9,9 M14,63 l15,-15 M1,50 l15,-15"}),
 		path({"d": "M98,39 s0,-10 -10,-10 h-49 s-10,0 -10,10 v49 s0,10 10,10 h49 s10,0 10,-10 z M84,28 v-3 s0,-10 -10,-10 h-49 s-10,0 -10,10 v49 s0,10 10,10 h3 M71,15 v-3 s0,-10 -10,-10 h-49 s-10,0 -10,10 v49 s0,10 10,10 h3", "stroke-width": 4})
+	]),
+	"options": div({"style": {"overflow": "hidden"}}, [
+		label(`${lang["TOKEN"]}: `),
+		div({"class": "tokenSelector"}, [
+			button({"onclick": () => {
+				const data = getToken();
+				if (!data) {
+					return;
+				}
+				token = data;
+				const url = `/images/${data["src"]}`;
+				i.setAttribute("src", url);
+				cursor.setAttribute("src", url);
+			}}, lang["TOKEN_USE_SELECTED"]),
+			i
+		])
 	]),
 	"mapMouseWheel": defaultMouseWheel
 });
