0062468d5d0b88921551274fdfbd104a15efc380
Michael Woolnough <michael.woolnough@gmail.com>
2021-10-08 11:41:54 +0100

    added global no-right-click handler

--- internal/static/script.ts
+++ internal/static/script.ts
@@ -218,7 +218,7 @@ panelOnTop.wait((p: boolean) => document.documentElement.classList.toggle("panel
 pageLoad.then(() => RPC(`ws${window.location.protocol.slice(4)}//${window.location.host}/socket`).then(pluginInit).then(() => {
 	rpc.ready();
 	const admin = isAdmin;
-	document.body.classList.add(admin ? "isAdmin" : "isUser");
+	createHTML(document.body, {"class": [admin ? "isAdmin" : "isUser"], "oncontextmenu": (e: MouseEvent) => e.preventDefault()});
 	if (admin) {
 		assets(tabs.add(lang["TAB_IMAGES"], spinner("imagesLoading"), true, imageIcon), "IMAGES");
 		assets(tabs.add(lang["TAB_AUDIO"], spinner("audioLoading"), true, audioIcon), "AUDIO");
