deb00e9ccbfc2dcfd7cd2863cb59e01744d0c823
Michael Woolnough <michael.woolnough@gmail.com>
2021-11-21 09:04:19 +0000

    added tool options for wall tool

--- internal/static/tools_wall.ts
+++ internal/static/tools_wall.ts
@@ -1,6 +1,15 @@
+import type {Colour} from './colours.js';
+import {br, div, fieldset, input, label, legend, span} from './lib/html.js';
 import {defs, path, pattern, svg} from './lib/svg.js';
+import {hex2Colour, makeColourPicker} from './colours.js';
 import lang from './language.js';
+import {labels} from './shared.js';
 import {addTool} from './tools.js';
+
+let wallColour = hex2Colour("#000");
+
+const selectWall = input({"type": "radio", "name": "wallTool", "class": "settings_ticker", "checked": true}),
+      placeWall = input({"type": "radio", "name": "wallTool", "class": "settings_ticker"});
 
 addTool({
 	"name": lang["TOOL_WALL"],
@@ -9,4 +18,14 @@ addTool({
 		path({"d": "M60.5,14.5 V0 H0 V60 H90 V14.5 Z", "fill": "url(#brick)"}),
 		path({"d": "M60,15 V0.5 H0 V59.5 H90 V15 Z", "fill": "none", "style": "stroke: currentColor", "stroke-width": 2})
 	]),
+	"options": div([
+		fieldset([
+			legend(lang["TOOL_WALL_MODE"]),
+			labels(`${lang["TOOL_WALL_SELECT"]}: `, selectWall, false),
+			br(),
+			labels(`${lang["TOOL_WALL_PLACE"]}: `, placeWall, false)
+		]),
+		label(`${lang["TOOL_WALL_COLOUR"]}: `),
+		span({"class": "checkboard colourButton"}, makeColourPicker(null, lang["TOOL_LIGHT_COLOUR"], () => wallColour, (c: Colour) => wallColour = c, "wallColour")),
+	])
 });
