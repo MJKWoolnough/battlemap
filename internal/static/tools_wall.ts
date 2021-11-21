12026407ce61b23b6c85e4f37f5fbf65e00db49a
Michael Woolnough <michael.woolnough@gmail.com>
2021-11-21 11:27:44 +0000

    added snap toggle

--- internal/static/tools_wall.ts
+++ internal/static/tools_wall.ts
@@ -1,15 +1,20 @@
 import type {Colour} from './colours.js';
-import {br, div, fieldset, input, label, legend, span} from './lib/html.js';
+import {keyEvent} from './lib/events.js';
+import {createHTML, br, div, fieldset, input, label, legend, span} from './lib/html.js';
 import {defs, path, pattern, svg} from './lib/svg.js';
 import {hex2Colour, makeColourPicker} from './colours.js';
 import lang from './language.js';
+import {autosnap} from './settings.js';
 import {labels} from './shared.js';
 import {addTool} from './tools.js';
 
 let wallColour = hex2Colour("#000");
 
 const selectWall = input({"type": "radio", "name": "wallTool", "class": "settings_ticker", "checked": true}),
-      placeWall = input({"type": "radio", "name": "wallTool", "class": "settings_ticker"});
+      placeWall = input({"type": "radio", "name": "wallTool", "class": "settings_ticker"}),
+      snap = input({"type": "checkbox", "class": "settings_ticker"}),
+      shiftSnap = () => snap.click(),
+      [setupShiftSnap, cancelShiftSnap] = keyEvent("Shift", shiftSnap, shiftSnap);
 
 addTool({
 	"name": lang["TOOL_WALL"],
@@ -25,7 +30,16 @@ addTool({
 			br(),
 			labels(`${lang["TOOL_WALL_PLACE"]}: `, placeWall, false)
 		]),
+		labels(`${lang["TOOL_WALL_SNAP"]}: `, snap, false),
+		br(),
 		label(`${lang["TOOL_WALL_COLOUR"]}: `),
 		span({"class": "checkboard colourButton"}, makeColourPicker(null, lang["TOOL_LIGHT_COLOUR"], () => wallColour, (c: Colour) => wallColour = c, "wallColour")),
-	])
+	]),
+	"set": () => {
+		createHTML(snap, {"checked": autosnap.value});
+		setupShiftSnap();
+	},
+	"unset": () => {
+		cancelShiftSnap();
+	}
 });
