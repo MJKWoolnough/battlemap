44921605234863be5ad06e9087e20978ca6675cb
Michael Woolnough <michael.woolnough@gmail.com>
2021-11-22 09:44:01 +0000

    added marker cursor

--- internal/static/tools_wall.ts
+++ internal/static/tools_wall.ts
@@ -1,12 +1,13 @@
 import type {Colour} from './colours.js';
-import {keyEvent} from './lib/events.js';
+import {keyEvent, mouseMoveEvent} from './lib/events.js';
 import {createHTML, br, div, fieldset, input, label, legend, span} from './lib/html.js';
-import {defs, path, pattern, svg} from './lib/svg.js';
+import {createSVG, defs, path, pattern, svg} from './lib/svg.js';
 import {hex2Colour, makeColourPicker} from './colours.js';
 import lang from './language.js';
+import {root, screen2Grid} from './map.js';
 import {autosnap} from './settings.js';
 import {labels} from './shared.js';
-import {addTool} from './tools.js';
+import {addTool, marker} from './tools.js';
 
 let wallColour = hex2Colour("#000");
 
@@ -14,7 +15,11 @@ const selectWall = input({"type": "radio", "name": "wallTool", "class": "setting
       placeWall = input({"type": "radio", "name": "wallTool", "class": "settings_ticker"}),
       snap = input({"type": "checkbox", "class": "settings_ticker"}),
       shiftSnap = () => snap.click(),
-      [setupShiftSnap, cancelShiftSnap] = keyEvent("Shift", shiftSnap, shiftSnap);
+      [setupShiftSnap, cancelShiftSnap] = keyEvent("Shift", shiftSnap, shiftSnap),
+      [startCursorMove, cancelCursorMove] = mouseMoveEvent((e: MouseEvent) => {
+	const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
+	createSVG(marker, {"transform": `translate(${x - 10}, ${y - 10})`});
+      });
 
 addTool({
 	"name": lang["TOOL_WALL"],
@@ -34,11 +39,18 @@ addTool({
 		label(`${lang["TOOL_WALL_COLOUR"]}: `),
 		span({"class": "checkboard colourButton"}, makeColourPicker(null, lang["TOOL_LIGHT_COLOUR"], () => wallColour, (c: Colour) => wallColour = c, "wallColour")),
 	]),
+	"mapMouseOver": () => {
+		startCursorMove();
+		return false;
+	},
 	"set": () => {
 		createHTML(snap, {"checked": autosnap.value});
+		createSVG(root, {"style": {"cursor": "none"}}, marker);
 		setupShiftSnap();
 	},
 	"unset": () => {
+		createSVG(root, {"style": {"cursor": undefined}});
 		cancelShiftSnap();
+		cancelCursorMove();
 	}
 });
