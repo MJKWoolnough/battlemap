f8394ebd553389bf5f18c02201e21a5c2e31ddaf
Michael Woolnough <michael.woolnough@gmail.com>
2021-11-30 08:49:22 +0000

    do not need a ref to checkboard div

--- internal/static/colours.ts
+++ internal/static/colours.ts
@@ -24,14 +24,13 @@ export class Colour {
 export const hex2Colour = (hex: string, a = 255) => Colour.from({"r": checkInt(parseInt(hex.slice(1, 3), 16), 0, 255), "g": checkInt(parseInt(hex.slice(3, 5), 16), 0, 255), "b": checkInt(parseInt(hex.slice(5, 7), 16), 0, 255), a}),
 noColour = Colour.from({"r": 0, "g": 0, "b": 0, "a": 0}),
 colourPicker = (parent: WindowElement | ShellElement, title: string, colour: Colour = noColour, icon?: string) => new Promise<Colour>((resolve, reject) => {
-	const checkboard = div({"class": "checkboard"}),
-	      preview = checkboard.appendChild(div({"style": `background-color: ${colour}`})),
+	const preview = div({"style": `background-color: ${colour}`}),
 	      updatePreview = () => createHTML(preview, {"style": {"background-color": hex2Colour(colourInput.value, checkInt(parseInt(alphaInput.value), 0, 255, 255)) + ""}}),
 	      colourInput = input({"type": "color", "value": colour.toHexString(), "onchange": updatePreview}),
 	      alphaInput = input({"type": "range", "min": 0, "max": 255, "step": 1,"value": colour.a, "oninput": updatePreview}),
 	      window = windows({"window-icon": icon, "window-title": title, "class": "lightChange", "onremove": reject}, [
 		h1(title),
-		checkboard,
+		div({"class": "checkboard"}, preview),
 		labels(`${lang["COLOUR"]}: `, colourInput),
 		br(),
 		labels(`${lang["COLOUR_ALPHA"]}: `, alphaInput),
