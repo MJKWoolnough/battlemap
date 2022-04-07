b74101425c42644a1d2eb6d0dae799b3afe7c6ea
Michael Woolnough <michael.woolnough@gmail.com>
2022-04-07 18:07:58 +0100

    normalise walls so that they all follow in angle order

--- internal/static/map_lighting.ts
+++ internal/static/map_lighting.ts
@@ -31,11 +31,14 @@ const point2Line = (px: Int, py: Int, x1: Int, y1: Int, x2: Int, y2: Int) => {
 export const makeLight = (l: LightSource, walls: Wall[]) => {
 	const [_c, _i, x, y] = l,
 	      vertices: Vertex[] = [];
-	for (const wall of walls) {
-		const {x1, y1, x2, y2} = wall,
-		      a1 = Math.atan2(y1 - y, x1 - x),
+	for (let {id, x1, y1, x2, y2, colour, scattering} of walls) {
+		let a1 = Math.atan2(y1 - y, x1 - x),
 		      a2 = Math.atan2(y2 - y, x2 - x);
+		if (a1 > a2) {
+			[a1, a2, x1, y1, x2, y2] = [a2, a1, x2, y2, x1, y1]
+		} 
 		if (a1 !== a2) {
+			const wall = {id, x1, y1, x2, y2, colour, scattering};
 			vertices.push({
 				wall,
 				x: x1,
