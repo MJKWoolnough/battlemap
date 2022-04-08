495026a82766f8e1129f3091229671d87782f4f5
Michael Woolnough <michael.woolnough@gmail.com>
2022-04-08 11:18:46 +0100

    created a map of points that walls share

--- internal/static/map_lighting.ts
+++ internal/static/map_lighting.ts
@@ -1,12 +1,11 @@
 import type {Int, Uint, Wall} from './types.js';
 import type {Colour} from './colours.js';
+import {setAndReturn} from './shared.js';
 
 type Vertex = {
-	wall: Wall;
 	x: Int;
 	y: Int;
 	angle: number;
-	other: number;
 }
 
 export type LightSource = [Colour, Uint, Int, Int];
@@ -24,7 +23,8 @@ const vertexSort = (a: Vertex, b: Vertex) => {
 export const makeLight = (l: LightSource, walls: Wall[]) => {
 	const [_c, _i, x, y] = l,
 	      vertices: Vertex[] = [],
-	      ranges: Range[] = [];
+	      ranges: Range[] = [],
+	      points = new Map<string, Wall[]>();
 	for (let {id, x1, y1, x2, y2, colour, scattering} of walls) {
 		let a1 = Math.atan2(y1 - y, x1 - x),
 		      a2 = Math.atan2(y2 - y, x2 - x);
@@ -32,21 +32,28 @@ export const makeLight = (l: LightSource, walls: Wall[]) => {
 			[a1, a2, x1, y1, x2, y2] = [a2, a1, x2, y2, x1, y1]
 		}
 		if (a1 !== a2) {
-			const wall = {id, x1, y1, x2, y2, colour, scattering};
+			const wall = {id, x1, y1, x2, y2, colour, scattering},
+			      p1 = `${x1},${y1}`,
+			      p2 = `${x2},${y2}`,
+			      points1 = points.get(p1) ?? setAndReturn(points, p1, []),
+			      points2 = points.get(p2) ?? setAndReturn(points, p2, []);
 			ranges.push({wall, "min": a1, "max": a2});
-			vertices.push({
-				wall,
-				x: x1,
-				y: y1,
-				angle: a1,
-				other: a2
-			}, {
-				wall,
-				x: x2,
-				y: y2,
-				angle: a2,
-				other: a1
-			});
+			if (!points1.length) {
+				vertices.push({
+					x: x1,
+					y: y1,
+					angle: a1
+				});
+			}
+			if (!points2.length) {
+				vertices.push({
+					x: x2,
+					y: y2,
+					angle: a2
+				});
+			};
+			points1.push(wall);
+			points2.push(wall);
 		}
 	}
 	vertices.sort(vertexSort);
