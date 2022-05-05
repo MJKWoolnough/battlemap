d71ec1ad8a1004612adc8483b67b9b9fab15f47c
Michael Woolnough <michael.woolnough@gmail.com>
2022-05-05 09:21:31 +0100

    only need to pass-through light if there's light to pass through

--- internal/static/map_lighting.ts
+++ internal/static/map_lighting.ts
@@ -243,13 +243,19 @@ export const makeLight = (l: LightSource, walls: Wall[], lens?: Wall) => {
 						      };
 						if (cd < i) {
 							if (a < 255) {
-								const inva = 1 - (la / 255);
-								ret.push(makeLight([
-									Colour.from({"r": Math.pow(r * lr, 0.5) * inva, "g": Math.pow(g * lg, 0.5) * inva, "b": Math.pow(b * lb, 0.5) * inva, "a": inva * a}),
-									cd + (i - cd) * inva,
-									lightX,
-									lightY
-								], walls, fw));
+								const inva = 1 - (la / 255),
+								      nr = Math.round(Math.pow(r * lr, 0.5) * inva),
+								      ng = Math.round(Math.pow(g * lg, 0.5) * inva),
+								      nb = Math.round(Math.pow(b * lb, 0.5) * inva),
+								      na = Math.round(a * inva);
+								if (na && (nr || ng || nb || na)) {
+									ret.push(makeLight([
+										Colour.from({"r": nr, "g": ng, "b": nb, "a": na}),
+										cd + (i - cd) * inva,
+										lightX,
+										lightY
+									], walls, fw));
+								}
 							}
 						}
 					}
