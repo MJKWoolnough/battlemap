a9f550bc3b31c6cef2962614b584900a0a67a1f3
Michael Woolnough <michael.woolnough@gmail.com>
2022-05-10 18:27:36 +0100

    use array spreading to avoid usage of concat

--- internal/static/folders.ts
+++ internal/static/folders.ts
@@ -28,7 +28,7 @@ export type FolderDragItem = IDName & WidthHeight;
 const stringSorter = (a: Item | Folder, b: Item | Folder) => stringSort(a.name, b.name),
       idSorter = (a: Item, b: Item) => b.id - a.id,
       sorts = new WeakMap<FolderSorter, WeakMap<ItemSorter, Sorter>>(),
-      getPaths = (folder: Folder, breadcrumb: string): string[] => [breadcrumb].concat(...(Array.from(folder.children.values()).filter(c => c instanceof Folder) as Folder[]).flatMap(p => getPaths(p, breadcrumb + p.name + "/")).sort(stringSort)),
+      getPaths = (folder: Folder, breadcrumb: string): string[] => [breadcrumb, ...(Array.from(folder.children.values()).filter(c => c instanceof Folder) as Folder[]).flatMap(p => getPaths(p, breadcrumb + p.name + "/")).sort(stringSort)],
       folderIcon = div({"style": {"transform": "translateX(-9999px)", "display": "inline-block"}}, folder({"style": "width: 2em; height: 2em"})),
       clearDragOver = ({root: {[node]: n}}: Folder) => {
 	amendNode(n, {"class": ["!folderDragging"]});
