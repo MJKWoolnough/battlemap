0a85935623520e2b20b1a7a3b3662c26e2c7bc15
Michael Woolnough <michael.woolnough@gmail.com>
2022-05-17 17:23:02 +0100

    make sure that pasted items are wholly on the map

--- internal/static/adminMap.ts
+++ internal/static/adminMap.ts
@@ -367,8 +367,9 @@ export default (base: HTMLElement) => {
 	})[0]();
 	keyEvent("v", () => {
 		if (copiedToken && selected.layer) {
-			const [x, y] = copiedToken.snap ? snapTokenToGrid(pasteCoords[0] - (copiedToken.width >> 1), pasteCoords[1] - (copiedToken.height >> 1), copiedToken.width, copiedToken.height) : [pasteCoords[0] - (copiedToken.width >> 1), pasteCoords[1] - (copiedToken.width >> 1)],
-			      tk: Token  = Object.assign(cloneObject(copiedToken), {"id": 0, x, y});
+			const {snap, width, height} = copiedToken,
+			      [x, y] = snap ? snapTokenToGrid(pasteCoords[0] - (width >> 1), pasteCoords[1] - (height >> 1), width, height) : [pasteCoords[0] - (width >> 1), pasteCoords[1] - (height >> 1)],
+			      tk: Token  = Object.assign(cloneObject(copiedToken), {"id": 0, "x": Math.min(Math.max(0, x), mapData.width - width), "y": Math.min(Math.max(0, y), mapData.height - height)});
 			doTokenAdd(selected.layer.path, tk);
 		}
 	})[0]();
