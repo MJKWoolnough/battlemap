d63805b397f14a0cf8f5480b9b805f33fa5225c0
Michael Woolnough <michael.woolnough@gmail.com>
2022-06-13 16:05:47 +0100

    start of code to made changeable key bindings

---
+++ internal/static/keys.ts
@@ -0,0 +1,22 @@
+import {JSONSetting} from './lib/settings.js';
+
+const keys = new JSONSetting<Record<string, string>>("keys", {}, (v: any): v is Record<string, string> => {
+	for (const k in v) {
+		if (typeof v[k] !== "string") {
+			return false;
+		}
+	}
+	return true;
+      }),
+      names: Record<string, string> = {};
+
+export const registerKey = (id: string, name: string, defaultKey = "") => {
+	names[id] = name;
+	return keys.value[id] ?? defaultKey;
+},
+setKey = (id: string, key: string) => {
+	keys.set(Object.assign(keys.value, {[id]: key}));
+},
+getKey = (id: string) => keys.value[id],
+getKeyName = (id: string) => names[id],
+getKeyIDs = () => Object.keys(names);
