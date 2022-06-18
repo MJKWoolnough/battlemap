9807bc84dbc15aa667308c5bb3467a3e016c3431
Michael Woolnough <michael.woolnough@gmail.com>
2022-06-18 09:40:12 +0100

    no need for style object when creating node

--- internal/static/tools_wall.ts
+++ internal/static/tools_wall.ts
@@ -101,7 +101,7 @@ const updateCursorState = () => {
 	}
       },
       makeLayerContext = (fn: (sl: SVGLayer) => void, disabled = "", folder: SVGFolder = layerList): List => (folder.children as NodeArray<SVGFolder | SVGLayer>).map(e => e.id < 0 ? [] : isSVGFolder(e) ? menu(e.name, makeLayerContext(fn, disabled, e)) : item(e.name, () => fn(e), {"disabled": e.name === disabled})),
-      wallOverlay = div({"style": {"position": "absolute", "height": "10px"}, "draggable": "true", "ondragstart": (e: DragEvent) => {
+      wallOverlay = div({"style": "position: absolute; height: 10px", "draggable": "true", "ondragstart": (e: DragEvent) => {
 	if (walls.has(selectedWall)) {
 		dragColour.set(e, colourDragKey, iconImg);
 		dragScattering.set(e, scatteringDragKey);
@@ -146,7 +146,7 @@ const updateCursorState = () => {
       fWallOverlay = foreignObject({"height": 10}, wallOverlay),
       setOverlay = (x1: Uint, y1: Uint, x2: Uint, y2: Uint) => {
 	const width = Math.round(Math.hypot(x1 - x2, y1 - y2));
-	amendNode(wallOverlay, {"style": {"width": width + "px"}});
+	amendNode(wallOverlay, {"style": `width: ${width}px`});
 	amendNode(root, [
 		amendNode(fWallOverlay, {width, "x": x1, "y": y1 - 5, "transform": `rotate(${Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI}, ${x1}, ${y1})`}),
 		amendNode(draggableMarker1, {"transform": `translate(${x1 - 10}, ${y1 - 10})`}),
