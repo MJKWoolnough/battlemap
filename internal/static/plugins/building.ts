4d20d1de5d7045e1a57f4cc72197f052ea5e0b0e
Michael Woolnough <michael.woolnough@gmail.com>
2022-08-12 17:28:06 +0100

    added basic folders setup

--- internal/static/plugins/building.ts
+++ internal/static/plugins/building.ts
@@ -1,5 +1,10 @@
+import type {FolderItems, FolderRPC, Uint} from '../types.js';
 import type {PluginType, SVGDrawingConstructor, SVGShapeConstructor} from '../plugins.js';
+import {clearNode} from '../lib/dom.js';
+import {Subscription} from '../lib/inter.js';
 import {div} from '../lib/html.js';
+import {node} from '../lib/nodes.js';
+import {Folder, Item, Root} from '../folders.js';
 import {language} from '../language.js';
 import {addPlugin} from '../plugins.js'; 
 import {isAdmin} from '../rpc.js';
@@ -19,15 +24,50 @@ const upgradeClass = <T extends SVGShapeConstructor | SVGDrawingConstructor>(s: 
       };
 
 if (isAdmin) {
+	class BuildingItem extends Item {
+		constructor(parent: Folder, id: Uint, name: string) {
+			super(parent, id, name);
+			clearNode(this[node], this.nameElem);
+		}
+		show(){}
+	}
+
+	class BuildingFolder extends Folder {
+		constructor(root: Root, parent: Folder | null, name: string, children: FolderItems) {
+			super(root, parent, name, children);
+			this.renamer.remove();
+			this.newer.remove();
+			this.remover.remove();
+		}
+	}
+
+	class BuildingRoot extends Root {
+		constructor() {
+			super ({"folders": {}, "items": {}}, "", {
+				"waitAdded": unusedWaitFn,
+				"waitMoved": unusedWaitFn,
+				"waitRemoved": unusedWaitFn,
+				"waitCopied": unusedWaitFn,
+				"waitFolderAdded": unusedWaitFn,
+				"waitFolderMoved": unusedWaitFn,
+				"waitFolderRemoved": unusedWaitFn
+			} as FolderRPC, BuildingItem, BuildingFolder);
+			this.folder.newer.remove();
+		}
+	}
+
 	const icon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 61 64'%3E%3Cg transform='translate(20, 0) rotate(45, 28, 28)'%3E%3Cpath d='M20,50 v25 h10 s3,0 4,-3 l5,-25 s1,-3 -3,-3 h-10 z' fill='%23aac' /%3E%3Cpath d='M20,50 v25 h-10 s-3,0 -4,-3 l-5,-25 s-1,-3 3,-3 h10 z' fill='%2388a' /%3E%3Cpath d='M20,50 v-6 h6 z' fill='%23446' /%3E%3Cpath d='M20,50 v-6 h-6 z' fill='%23668' /%3E%3Cpath d='M21,45 v-1 c0,-5 -3,-5 -3,-10' stroke='%23446' stroke-width='2' /%3E%3Cpath d='M19,45 v-1 c0,-5 -3,-5 -3,-10' stroke='%23668' stroke-width='2' /%3E%3Cpath d='M17,35 s5,0 5,-3 v-25 s0,-5 -5,-5 z' fill='%23f80' /%3E%3Cpath d='M17,35 s-5,0 -5,-3 v-25 s0,-5 5,-5 z' fill='%23d60' /%3E%3C/g%3E%3C/svg%3E",
 	      defaultLanguage = {
+		      "MENU_TITLE": "Building"
 	      },
 	      langs: Record<string, typeof defaultLanguage> = {
 		"en-GB": defaultLanguage
 	      },
-	      lang = langs[language.value] ?? defaultLanguage;
+	      lang = langs[language.value] ?? defaultLanguage,
+	      unusedWait = new Subscription<any>(() => {}),
+	      unusedWaitFn = () => unusedWait;
 	plugin["menuItem"] = {
-		"fn": ["", div(), true, icon]
+		"fn": [lang["MENU_TITLE"], div(new BuildingRoot()[node]), true, icon]
 	};
 }
 
