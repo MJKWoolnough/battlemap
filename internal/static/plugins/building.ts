42e1c1ecd1f9fae297235dd92bd1a57797dfe06c
Michael Woolnough <michael.woolnough@gmail.com>
2022-08-12 15:42:06 +0100

    start of admin menu

--- internal/static/plugins/building.ts
+++ internal/static/plugins/building.ts
@@ -1,5 +1,7 @@
 import type {PluginType, SVGDrawingConstructor, SVGShapeConstructor} from '../plugins.js';
+import {div} from '../lib/html.js';
 import {addPlugin} from '../plugins.js'; 
+import {isAdmin} from '../rpc.js';
 
 let shapeClass: SVGShapeConstructor,
     drawingClass: SVGDrawingConstructor;
@@ -14,5 +16,11 @@ const upgradeClass = <T extends SVGShapeConstructor | SVGDrawingConstructor>(s: 
 		"fn": (s: SVGDrawingConstructor) => drawingClass = upgradeClass(s)
 	}
       };
+
+if (isAdmin) {
+	plugin["menuItem"] = {
+		"fn": ["", div(), true, ""]
+	};
+}
 
 addPlugin("building", plugin);
