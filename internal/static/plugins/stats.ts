b5ef10f28132291ebb1dcab36b3bae43cbe0c220
Michael Woolnough <michael.woolnough@gmail.com>
2022-08-16 11:46:37 +0100

    no need for underscore to ignore var, can just elided

--- internal/static/plugins/stats.ts
+++ internal/static/plugins/stats.ts
@@ -65,7 +65,7 @@ if (isAdmin) {
 						}
 					}
 					let totalSize = 0;
-					for (const [_, {token: tk}] of tokens) {
+					for (const [, {token: tk}] of tokens) {
 						if (tk instanceof SVGToken) {
 							const id = tk.src;
 							if (done.has(id)) {
