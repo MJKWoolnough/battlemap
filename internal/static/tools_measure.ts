013226386d6c83d4133df796ecec61ed1d67fc65
Michael Woolnough <michael.woolnough@gmail.com>
2022-09-14 18:13:50 +0100

    only run the most tool code if admin

--- internal/static/tools_measure.ts
+++ internal/static/tools_measure.ts
@@ -10,13 +10,16 @@ import {autosnap} from './settings.js';
 import {checkInt, isUint, labels, mapLoadedReceive} from './shared.js';
 import {addTool, ignore, marker} from './tools.js';
 
-let send = false;
 
 const grid2Screen = (x: Uint, y: Uint): [number, number] => {
 	const {width, height} = mapData;
 	return [panZoom.zoom * x - (panZoom.zoom - 1) * width / 2 + panZoom.x, panZoom.zoom * y - (panZoom.zoom - 1) * height / 2 + panZoom.y];
       },
-      snap = input({"type": "checkbox", "class": "settings_ticker"}),
+      info = div({"style": "border: 1px solid #000; padding: 5px; background-color: #fff; color: #000; position: absolute"}),
+      spot = circle({"r": 8, "fill": "#000", "stroke": "#fff", "stroke-width": 2}),
+      lone = polyline({"stroke": "#fff", "stroke-width": 8, "stroke-linecap": "square", "stroke-linejoin": "round", "fill": "none"}),
+      ltwo = polyline({"stroke": "#000", "stroke-width": 6, "stroke-linejoin": "round", "fill": "none"}),
+      drawnLine = g([lone, ltwo, spot]),
       cellValue = input({"type": "number", "value": 1, "min": 0, "onchange": () => {
 	const v = parseInt(cellValue.value);
 	if (isUint(v)) {
@@ -24,45 +27,7 @@ const grid2Screen = (x: Uint, y: Uint): [number, number] => {
 	}
       }}),
       diagonals = input({"type": "checkbox", "checked": true, "class": "settings_ticker", "onchange": () => rpc.setGridDiagonal(diagonals.checked)}),
-      multiPoint = input({"type": "checkbox", "checked": false, "class": "settings_ticker"}),
-      shiftSnap = () => snap.click(),
-      info = div({"style": "border: 1px solid #000; padding: 5px; background-color: #fff; color: #000; position: absolute"}),
-      spot = circle({"r": 8, "fill": "#000", "stroke": "#fff", "stroke-width": 2}),
-      lone = polyline({"stroke": "#fff", "stroke-width": 8, "stroke-linecap": "square", "stroke-linejoin": "round", "fill": "none"}),
-      ltwo = polyline({"stroke": "#000", "stroke-width": 6, "stroke-linejoin": "round", "fill": "none"}),
-      drawnLine = g([lone, ltwo, spot]),
-      coords: [number, number, ...number[]] = [NaN, NaN],
-      [setupMouse0, cancelMouse0] = mouseDragEvent(0, undefined, (e: MouseEvent) => {
-	if (!e.ctrlKey) {
-	      stopMeasurement()
-	}
-      }),
-      [setupMouse2, cancelMouse2] = mouseDragEvent(2, undefined, () => {
-	if (send) {
-		rpc.signalMeasure(null);
-		send = false;
-	}
-      }),
-      [startMouseMove, cancelMouseMove] = mouseMoveEvent((e: MouseEvent) => {
-	const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
-	amendNode(marker, {"transform": `translate(${x - 10 / panZoom.zoom}, ${y - 10 / panZoom.zoom}) scale(${1/panZoom.zoom})`});
-	if (!isNaN(coords[0])) {
-		measureDistance(x, y);
-		if (send) {
-			rpc.signalMeasure([...(coords as [Uint, Uint]), x, y]);
-		}
-	}
-      }),
-      [setupShiftSnap, cancelShiftSnap] = keyEvent("Shift", shiftSnap, shiftSnap),
-      [setupEscape, cancelEscape] = keyEvent("Escape", () => {
-	if (coords.length === 2) {
-		stopMeasurement();
-	} else {
-		const y = coords.pop()!,
-		      x = coords.pop()!;
-		measureDistance(x, y);
-	}
-      });
+      coords: [number, number, ...number[]] = [NaN, NaN];
 
 export const startMeasurement = (x1: Uint, y1: Uint) => {
 	coords.splice(0, coords.length, x1, y1);
@@ -120,74 +85,7 @@ stopMeasurement = () => {
 	coords.splice(0, coords.length, NaN, NaN);
 };
 
-addTool({
-	"name": lang["TOOL_MEASURE"],
-	"icon": svg({"viewBox": "0 0 50 50"}, [title(lang["TOOL_MEASURE"]), path({"d": "M0,40 l10,10 l40,-40 l-10,-10 z m5,-5 l5,5 m-3,-7 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l5,5 m-3,-7 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l5,5 m-3,-7 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l5,5", "style": "stroke: currentColor", "stroke-linejoin": "round", "fill": "none"})]),
-	"options": div([
-		labels(snap, `${lang["TOOL_MEASURE_SNAP"]}: `),
-		br(),
-		labels(diagonals, `${lang["TOOL_MEASURE_DIAGONALS"]}: `),
-		br(),
-		labels(multiPoint, `${lang["TOOL_MEASURE_MULTI"]}: `),
-		br(),
-		labels(`${lang["TOOL_MEASURE_CELL"]}: `, cellValue)
-	]),
-	"mapMouseOver": () => {
-		startMouseMove();
-		return false;
-	},
-	"tokenMouseOver": function(this: SVGElement) {
-		amendNode(root, {"style": {"--outline-cursor": "none"}});
-		amendNode(this, {"onmouseout": event(() => amendNode(root, {"style": {"--outline-cursor": undefined}}), eventOnce)});
-		return false;
-	},
-	"tokenMouse0": ignore,
-	"tokenMouse2": ignore,
-	"mapMouse0": (e: MouseEvent) => {
-		const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
-		if (isNaN(coords[0])) {
-			startMeasurement(x, y);
-		} else {
-			coords.push(x, y);
-		}
-		if (!e.ctrlKey && !multiPoint.checked) {
-			setupMouse0();
-		}
-		return false;
-	},
-	"mapMouse2": (e: MouseEvent) => {
-		const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
-		rpc.signalMeasure([coords[0], coords[1], x, y]);
-		send = true;
-		setupMouse2();
-		return false;
-	},
-	"set": () => {
-		amendNode(root, {"style": {"cursor": "none"}}, marker);
-		amendNode(snap, {"checked": autosnap.value});
-		setupShiftSnap();
-		setupEscape();
-	},
-	"unset": () => {
-		amendNode(root, {"style": {"cursor": undefined}});
-		marker.remove();
-		cancelMouse0();
-		cancelMouse2();
-		cancelMouseMove();
-		cancelShiftSnap();
-		cancelEscape();
-		stopMeasurement();
-	}
-});
-
-mapLoadedReceive(() => {
-	cellValue.value = mapData.gridDistance + "";
-	diagonals.checked = mapData.gridDiagonal;
-});
-
 inited.then(() => {
-	rpc.waitGridDistanceChange().then(distance => cellValue.value = distance + "");
-	rpc.waitGridDiagonalChange().then(diagonal => diagonals.checked = diagonal);
 	if (!isAdmin) {
 		rpc.waitSignalMeasure().then(data => {
 			if (!data) {
@@ -201,5 +99,109 @@ inited.then(() => {
 			coords.push(...additional);
 			measureDistance(xn, yn);
 		});
+		return;
 	}
+	let send = false;
+	const snap = input({"type": "checkbox", "class": "settings_ticker"}),
+	      multiPoint = input({"type": "checkbox", "checked": false, "class": "settings_ticker"}),
+	      shiftSnap = () => snap.click(),
+	      [setupMouse0, cancelMouse0] = mouseDragEvent(0, undefined, (e: MouseEvent) => {
+		if (!e.ctrlKey) {
+		      stopMeasurement()
+		}
+	      }),
+	      [setupMouse2, cancelMouse2] = mouseDragEvent(2, undefined, () => {
+		if (send) {
+			rpc.signalMeasure(null);
+			send = false;
+		}
+	      }),
+	      [startMouseMove, cancelMouseMove] = mouseMoveEvent((e: MouseEvent) => {
+		const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
+		amendNode(marker, {"transform": `translate(${x - 10 / panZoom.zoom}, ${y - 10 / panZoom.zoom}) scale(${1/panZoom.zoom})`});
+		if (!isNaN(coords[0])) {
+			measureDistance(x, y);
+			if (send) {
+				rpc.signalMeasure([...(coords as [Uint, Uint]), x, y]);
+			}
+		}
+	      }),
+	      [setupShiftSnap, cancelShiftSnap] = keyEvent("Shift", shiftSnap, shiftSnap),
+	      [setupEscape, cancelEscape] = keyEvent("Escape", () => {
+		if (coords.length === 2) {
+			stopMeasurement();
+		} else {
+			const y = coords.pop()!,
+			      x = coords.pop()!;
+			measureDistance(x, y);
+		}
+	      });
+
+	addTool({
+		"name": lang["TOOL_MEASURE"],
+		"icon": svg({"viewBox": "0 0 50 50"}, [title(lang["TOOL_MEASURE"]), path({"d": "M0,40 l10,10 l40,-40 l-10,-10 z m5,-5 l5,5 m-3,-7 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l5,5 m-3,-7 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l5,5 m-3,-7 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l3,3 m-1,-5 l5,5", "style": "stroke: currentColor", "stroke-linejoin": "round", "fill": "none"})]),
+		"options": div([
+			labels(snap, `${lang["TOOL_MEASURE_SNAP"]}: `),
+			br(),
+			labels(diagonals, `${lang["TOOL_MEASURE_DIAGONALS"]}: `),
+			br(),
+			labels(multiPoint, `${lang["TOOL_MEASURE_MULTI"]}: `),
+			br(),
+			labels(`${lang["TOOL_MEASURE_CELL"]}: `, cellValue)
+		]),
+		"mapMouseOver": () => {
+			startMouseMove();
+			return false;
+		},
+		"tokenMouseOver": function(this: SVGElement) {
+			amendNode(root, {"style": {"--outline-cursor": "none"}});
+			amendNode(this, {"onmouseout": event(() => amendNode(root, {"style": {"--outline-cursor": undefined}}), eventOnce)});
+			return false;
+		},
+		"tokenMouse0": ignore,
+		"tokenMouse2": ignore,
+		"mapMouse0": (e: MouseEvent) => {
+			const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
+			if (isNaN(coords[0])) {
+				startMeasurement(x, y);
+			} else {
+				coords.push(x, y);
+			}
+			if (!e.ctrlKey && !multiPoint.checked) {
+				setupMouse0();
+			}
+			return false;
+		},
+		"mapMouse2": (e: MouseEvent) => {
+			const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
+			rpc.signalMeasure([coords[0], coords[1], x, y]);
+			send = true;
+			setupMouse2();
+			return false;
+		},
+		"set": () => {
+			amendNode(root, {"style": {"cursor": "none"}}, marker);
+			amendNode(snap, {"checked": autosnap.value});
+			setupShiftSnap();
+			setupEscape();
+		},
+		"unset": () => {
+			amendNode(root, {"style": {"cursor": undefined}});
+			marker.remove();
+			cancelMouse0();
+			cancelMouse2();
+			cancelMouseMove();
+			cancelShiftSnap();
+			cancelEscape();
+			stopMeasurement();
+		}
+	});
+
+	mapLoadedReceive(() => {
+		cellValue.value = mapData.gridDistance + "";
+		diagonals.checked = mapData.gridDiagonal;
+	});
+
+	rpc.waitGridDistanceChange().then(distance => cellValue.value = distance + "");
+	rpc.waitGridDiagonalChange().then(diagonal => diagonals.checked = diagonal);
 });
