cee5efa0e1f9e711c7bf5181e3ad929b81b30671
Michael Woolnough <michael.woolnough@gmail.com>
2022-09-29 16:40:20 +0100

    updated to use new CSS module

--- internal/static/plugins/building.ts
+++ internal/static/plugins/building.ts
@@ -1,6 +1,7 @@
 import type {FolderItems, FolderRPC, TokenDrawing, TokenShape, Uint} from '../types.js';
 import type {SVGShape} from '../map_tokens.js';
 import type {PluginType, SVGDrawingConstructor, SVGShapeConstructor} from '../plugins.js';
+import {add, id} from '../lib/css.js';
 import {amendNode, clearNode} from '../lib/dom.js';
 import {div} from '../lib/html.js';
 import {Subscription} from '../lib/inter.js';
@@ -10,7 +11,6 @@ import {makeLangPack} from '../language.js';
 import {selected} from '../map_tokens.js';
 import {addPlugin} from '../plugins.js';
 import {isAdmin} from '../rpc.js';
-import {addCSS} from '../shared.js';
 
 type BuildingType = SVGShape & {};
 
@@ -85,8 +85,6 @@ if (isAdmin) {
 		}
 	}
 
-	addCSS("#pluginBuilding ul{padding-left: 1em;list-style: none}#pluginBuilding>div>ul{padding:0}");
-
 	const icon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 61 64'%3E%3Cg transform='translate(20, 0) rotate(45, 28, 28)'%3E%3Cpath d='M20,50 v25 h10 s3,0 4,-3 l5,-25 s1,-3 -3,-3 h-10 z' fill='%23aac' /%3E%3Cpath d='M20,50 v25 h-10 s-3,0 -4,-3 l-5,-25 s-1,-3 3,-3 h10 z' fill='%2388a' /%3E%3Cpath d='M20,50 v-6 h6 z' fill='%23446' /%3E%3Cpath d='M20,50 v-6 h-6 z' fill='%23668' /%3E%3Cpath d='M21,45 v-1 c0,-5 -3,-5 -3,-10' stroke='%23446' stroke-width='2' /%3E%3Cpath d='M19,45 v-1 c0,-5 -3,-5 -3,-10' stroke='%23668' stroke-width='2' /%3E%3Cpath d='M17,35 s5,0 5,-3 v-25 s0,-5 -5,-5 z' fill='%23f80' /%3E%3Cpath d='M17,35 s-5,0 -5,-3 v-25 s0,-5 5,-5 z' fill='%23d60' /%3E%3C/g%3E%3C/svg%3E",
 	      lang = makeLangPack({
 		"FLOOR": "Floor",
@@ -96,10 +94,20 @@ if (isAdmin) {
 		"WALLS": "Walls"
 	      }),
 	      unusedWait = new Subscription<any>(() => {}),
-	      unusedWaitFn = () => unusedWait;
+	      unusedWaitFn = () => unusedWait,
+	      pluginBuilding = id();
+	add(`#${pluginBuilding}`, {
+		" ul": {
+			"padding-left": "1em",
+			"list-style": "none"
+		},
+		">div>ul": {
+			"padding": 0
+		}
+	});
 
 	plugin["menuItem"] = {
-		"fn": [lang["MENU_TITLE"], div({"id": "pluginBuilding"}, new BuildingRoot()[node]), true, icon]
+		"fn": [lang["MENU_TITLE"], div({"id": pluginBuilding}, new BuildingRoot()[node]), true, icon]
 	};
 }
 
