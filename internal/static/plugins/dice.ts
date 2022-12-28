b50f994f329d4af9540d8614a5b18a8cc92dc2df
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-28 14:59:11 +0000

    added icon

--- internal/static/plugins/dice.ts
+++ internal/static/plugins/dice.ts
@@ -1,5 +1,5 @@
 import {div} from '../lib/html.js';
-import {svg} from '../lib/svg.js';
+import {path, svg} from '../lib/svg.js';
 import {makeLangPack} from '../language.js';
 import {isAdmin} from '../rpc.js';
 import {addTool} from '../tools.js';
@@ -10,7 +10,7 @@ if (isAdmin) {
 	      });
 	addTool({
 		"name": lang["TITLE"],
-		"icon": svg(),
+		"icon": svg({"viewBox": "0 0 50 58"}, path({"d": "M24,1 L0,13 0,42 7,38 0,13 24,8 48,13 41,38 49,42 48,13 24,1 24,8 7,38 41,38 24,8 M7,38 L24,57 0,42 M41,38 L24,57 49,42", "stroke": "currentColor", "fill": "none", "stroke-linecap": "round", "stroke-linejoin": "round"})),
 		"options": div(),
 	});
 }
