d38c6bb95fa85900f168b789d15effce9083d311
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-01 16:25:59 +0000

    can now press enter to roll dice quicker

--- internal/static/plugins/dice.ts
+++ internal/static/plugins/dice.ts
@@ -5,7 +5,7 @@ import {checkInt} from '../lib/misc.js';
 import {path, svg} from '../lib/svg.js';
 import {makeLangPack} from '../language.js';
 import {isAdmin} from '../rpc.js';
-import {labels} from '../shared.js';
+import {enterKey, labels} from '../shared.js';
 import {addTool} from '../tools.js';
 
 if (isAdmin) {
@@ -27,11 +27,11 @@ if (isAdmin) {
 		"name": lang["TITLE"],
 		"icon": svg({"viewBox": "0 0 50 58"}, path({"d": "M24,1 L0,13 0,42 7,38 0,13 24,8 48,13 41,38 49,42 48,13 24,1 24,8 7,38 41,38 24,8 M7,38 L24,57 0,42 M41,38 L24,57 49,42", "stroke": "currentColor", "fill": "none", "stroke-linecap": "round", "stroke-linejoin": "round"})),
 		"options": div([
-			labels([lang["DICE"], ": "], input({"type": "number", "min": 1, "max": 100, "value": 6, "onchange": function(this: HTMLInputElement) {
+			labels([lang["DICE"], ": "], input({"type": "number", "min": 1, "max": 100, "value": 6, "onkeypress": enterKey, "onchange": function(this: HTMLInputElement) {
 				dieNum = checkInt(parseInt(this.value), 1, 100, 6);
 			}})),
 			br(),
-			labels([lang["NUMBER"], ": "], input({"type": "number", "min": 1, "max": 100, "value": 1, "onchange": function(this: HTMLInputElement) {
+			labels([lang["NUMBER"], ": "], input({"type": "number", "min": 1, "max": 100, "value": 1, "onkeyPress": enterKey, "onchange": function(this: HTMLInputElement) {
 				numDice = checkInt(parseInt(this.value), 1, 100, 1);
 			}})),
 			br(),
