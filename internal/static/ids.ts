db51a046e8f387228608d54d51af6cf202ac0a45
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-04 11:38:59 +0000

    combined *most* ids calls into a single call

--- internal/static/ids.ts
+++ internal/static/ids.ts
@@ -1,10 +1,4 @@
-import {id, ids} from './lib/css.js';
+import {ids} from './lib/css.js';
 
-export const [settingsTicker, invertID, adminHideLight] = ids(3),
-selectedLayerID = id(),
-tokenSelector = id(),
-[folderDragging, dragOver, folders, foldersItem, itemControl, imageIcon] = ids(6),
-[layerLight, layerGrid, hiddenLayer, mapID, hideZoomSlider, zoomSliderID, zooming] = ids(7),
-cursors = ids(4),
-[lighting, mapMask, gridPattern]  = ids(3),
-psuedoLink = id();
+export const [settingsTicker, invertID, adminHideLight, selectedLayerID, tokenSelector, psuedoLink, folderDragging, dragOver, folders, foldersItem, itemControl, imageIcon, layerLight, layerGrid, hiddenLayer, mapID, hideZoomSlider, zoomSliderID, zooming, lighting, mapMask, gridPattern] = ids(22),
+cursors = ids(4);
