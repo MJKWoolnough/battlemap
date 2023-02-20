8e8ec4b8db823c023b7a3c8f47f8e64b9ff06775
Michael Woolnough <michael.woolnough@gmail.com>
2023-02-20 17:42:10 +0000

    marker now scales with zoom level

--- internal/static/tools_draw.ts
+++ internal/static/tools_draw.ts
@@ -8,7 +8,7 @@ import {ellipse, path, polygon, polyline, rect, svg, svgData, title} from './lib
 import {Colour, makeColourPicker, noColour} from './colours.js';
 import {settingsTicker} from './ids.js';
 import lang from './language.js';
-import {root, screen2Grid} from './map.js';
+import {panZoom, root, screen2Grid} from './map.js';
 import {doTokenAdd} from './map_fns.js';
 import {deselectToken, selected} from './map_tokens.js';
 import {inited, isAdmin} from './rpc.js';
@@ -101,7 +101,7 @@ inited.then(() => {
 	      }),
 	      [startCursorMove, cancelCursorMove] = mouseMoveEvent((e: MouseEvent) => {
 		const [x, y] = screen2Grid(e.clientX, e.clientY, snap.checked);
-		amendNode(marker, {"transform": `translate(${x - 10}, ${y - 10})`});
+		amendNode(marker, {"transform": `translate(${x - 10 / panZoom.zoom}, ${y - 10 / panZoom.zoom}) scale(${1/panZoom.zoom})`});
 	      }),
 	      coords: [Uint, Uint, ...Uint[]] = [0, 0],
 	      icon = svg({"viewBox": "0 0 70 70", "fill": "none", "stroke": "currentColor"}, [
