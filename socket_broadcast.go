73c3952ca50e94885ae729fc0521b516c03915ca
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-11 20:32:05 +0100

    reformatted

--- socket_broadcast.go
+++ socket_broadcast.go
@@ -109,12 +109,13 @@ const (
 
 func (s *socket) KickAdmins(except ID) {
 	s.mu.RLock()
+
 	for c := range s.conns {
-		id := c.ID
-		if id > 0 && id != except {
+		if id := c.ID; id > 0 && id != except {
 			go c.kickAdmin()
 		}
 	}
+
 	s.mu.RUnlock()
 }
 
@@ -128,38 +129,50 @@ const broadcastStart = "{\"id\": -0,\"result\":"
 func buildBroadcast(id int, data json.RawMessage) []byte {
 	l := len(broadcastStart) + len(data) + 1
 	dat := make([]byte, l)
+
 	copy(dat, broadcastStart)
 	copy(dat[len(broadcastStart):], data)
+
 	id = -id
+
 	if id > 9 {
 		dat[6] = '-'
 		dat[7] = byte('0' + id/10)
 	}
+
 	dat[8] = byte('0' + id%10)
 	dat[l-1] = '}'
+
 	return dat
 }
 
 func (s *socket) SetCurrentUserMap(currentUserMap uint64, data, mData json.RawMessage, except ID) {
 	s.mu.RLock()
+
 	var dat, mdat json.RawMessage
+
 	for c := range s.conns {
 		id := c.ID
+
 		if c.IsAdmin() {
 			if except != id {
 				if len(dat) == 0 {
 					dat = buildBroadcast(broadcastCurrentUserMap, data)
 				}
+
 				go c.rpc.SendData(dat)
 			}
 		} else {
 			if len(mdat) == 0 {
 				mdat = buildBroadcast(broadcastCurrentUserMapData, mData)
 			}
+
 			atomic.StoreUint64(&c.CurrentMap, currentUserMap)
+
 			go c.rpc.SendData(mdat)
 		}
 	}
+
 	s.mu.RUnlock()
 }
 
@@ -173,30 +186,39 @@ const (
 
 func (s *socket) broadcastMapChange(cd ConnData, id int, data json.RawMessage, user userStatus) {
 	s.mu.RLock()
+
 	var dat json.RawMessage
+
 	for c := range s.conns {
 		currentMap := atomic.LoadUint64(&c.CurrentMap)
+
 		if c.ID != cd.ID && (currentMap == cd.CurrentMap || cd.CurrentMap == 0) && (user == userAny || ((user == userAdmin) && c.IsAdmin()) || ((user == userNotAdmin) && !c.IsAdmin())) {
 			if len(dat) == 0 {
 				dat = buildBroadcast(id, data)
 			}
+
 			go c.rpc.SendData(dat)
 		}
 	}
+
 	s.mu.RUnlock()
 }
 
 func (s *socket) broadcastAdminChange(id int, data json.RawMessage, except ID) {
 	s.mu.RLock()
+
 	var dat json.RawMessage
+
 	for c := range s.conns {
 		if c.IsAdmin() && c.ID != except {
 			if len(dat) == 0 {
 				dat = buildBroadcast(id, data)
 			}
+
 			go c.rpc.SendData(dat)
 		}
 	}
+
 	s.mu.RUnlock()
 }
 
