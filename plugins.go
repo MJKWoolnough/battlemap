2a8d850c87027292294163d682a579aac0561e83
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-11 20:31:59 +0100

    reformatted

--- plugins.go
+++ plugins.go
@@ -28,12 +28,15 @@ type plugin struct {
 func (p *plugin) ReadFrom(r io.Reader) (int64, error) {
 	rc := rwcount.Reader{Reader: r}
 	err := json.NewDecoder(&rc).Decode(&p)
+
 	return rc.Count, err
 }
 
 func (p *plugin) WriteTo(w io.Writer) (int64, error) {
 	wc := rwcount.Writer{Writer: w}
+
 	p.WriteToUser(&wc, true)
+
 	return wc.Count, wc.Err
 }
 
@@ -51,13 +54,17 @@ var (
 
 func (p *plugin) WriteToUser(w io.Writer, isAdmin bool) {
 	w.Write(pluginStart)
+
 	if p.Enabled {
 		w.Write(pluginTrue)
 	} else {
 		w.Write(pluginFalse)
 	}
+
 	w.Write(pluginMid)
+
 	first := true
+
 	for key, val := range p.Data {
 		if isAdmin || val.User {
 			if first {
@@ -65,18 +72,22 @@ func (p *plugin) WriteToUser(w io.Writer, isAdmin bool) {
 			} else {
 				w.Write(pluginComma)
 			}
+
 			fmt.Fprintf(w, "%q", key) // TODO: need to replace with JSON specific code
 			w.Write(pluginDataStart)
+
 			if val.User {
 				w.Write(pluginTrue)
 			} else {
 				w.Write(pluginFalse)
 			}
+
 			w.Write(pluginDataMid)
 			w.Write(val.Data)
 			w.Write(pluginEnd[:1])
 		}
 	}
+
 	w.Write(pluginEnd)
 }
 
@@ -94,51 +105,69 @@ type pluginsDir struct {
 
 func (p *pluginsDir) Init(b *Battlemap, links links) error {
 	var pd keystore.String
+
 	err := b.config.Get("PluginsDir", &pd)
 	if err != nil {
 		return fmt.Errorf("error retrieving plugins location: %w", err)
 	}
+
 	base := filepath.Join(b.config.BaseDir, string(pd))
+
 	p.FileStore, err = keystore.NewFileStore(base, base, keystore.NoMangle)
 	if err != nil {
 		return fmt.Errorf("error creating plugins keystore: %w", err)
 	}
+
 	p.plugins = make(map[string]*plugin)
 	hd := httpdir.New(time.Now())
 	g, _ := gzip.NewWriterLevel(nil, gzip.BestCompression)
+
 	for _, file := range p.FileStore.Keys() {
 		if !strings.HasSuffix(file, ".js") {
 			continue
 		}
+
 		st, err := p.FileStore.Stat(file)
 		if err != nil {
 			return fmt.Errorf("error stat'ing plugin (%s): %w", file, err)
 		}
+
 		buf := make(memio.Buffer, 0, st.Size())
 		if err := p.FileStore.Get(file, &buf); err != nil {
 			return fmt.Errorf("error reading plugin: %w", err)
 		}
+
 		var gBuf memio.Buffer
+
 		g.Reset(&gBuf)
 		g.Write(buf)
 		g.Close()
+
 		ft := st.ModTime()
+
 		hd.Create(file, httpdir.FileBytes(buf, ft))
 		hd.Create(file+".gz", httpdir.FileBytes(gBuf, ft))
+
 		s := file + pluginConfigExt
+
 		if p.FileStore.Exists(s) {
 			var plugin plugin
+
 			if err := p.FileStore.Get(s, &plugin); err != nil {
 				return fmt.Errorf("error reading plugin: %w", err)
 			}
+
 			p.plugins[file] = &plugin
 		} else {
 			p.plugins[file] = &plugin{Data: make(map[string]keystoreData)}
 		}
 	}
+
 	p.Battlemap = b
 	p.Handler = httpgzip.FileServer(hd)
+
 	p.updateJSON()
+
 	return nil
 }
 
@@ -146,6 +175,7 @@ func (p *pluginsDir) updateJSON() {
 	wa := append(memio.Buffer{}, '{')
 	wu := append(memio.Buffer{}, '{')
 	first := true
+
 	for id, plugin := range p.plugins {
 		if first {
 			first = false
@@ -153,8 +183,10 @@ func (p *pluginsDir) updateJSON() {
 			wa = append(wa, ',')
 			wu = append(wu, ',')
 		}
+
 		wa = append(appendString(wa, id), ':')
 		wu = append(appendString(wu, id), ':')
+
 		if plugin.Enabled {
 			plugin.WriteToUser(&wa, true)
 			plugin.WriteToUser(&wu, false)
@@ -163,6 +195,7 @@ func (p *pluginsDir) updateJSON() {
 			wu = append(wu, pluginDisabled...)
 		}
 	}
+
 	wa = append(wa, '}')
 	wu = append(wu, '}')
 	p.json = json.RawMessage(wa)
@@ -177,13 +210,17 @@ func (p *pluginsDir) RPCData(cd ConnData, method string, data json.RawMessage) (
 	switch method {
 	case "list":
 		var j json.RawMessage
+
 		p.mu.RLock()
+
 		if cd.IsAdmin() {
 			j = p.json
 		} else {
 			j = p.userJSON
 		}
+
 		p.mu.RUnlock()
+
 		return j, nil
 	case "set":
 		var toSet struct {
@@ -191,23 +228,33 @@ func (p *pluginsDir) RPCData(cd ConnData, method string, data json.RawMessage) (
 			Setting  map[string]keystoreData `json:"setting"`
 			Removing []string                `json:"removing"`
 		}
+
 		if err := json.Unmarshal(data, &toSet); err != nil {
 			return nil, err
 		}
+
 		if len(toSet.Setting) == 0 && len(toSet.Removing) == 0 {
 			return nil, nil
 		}
+
 		p.mu.Lock()
+
 		plugin, ok := p.plugins[toSet.ID]
 		if !ok {
 			p.mu.Unlock()
+
 			return nil, ErrUnknownPlugin
 		}
+
 		p.socket.broadcastAdminChange(broadcastPluginSettingChange, data, cd.ID)
+
 		buf := appendString(append(data[:0], "{\"id\":"...), toSet.ID)
 		buf = append(buf, ",\"setting\":{"...)
+
 		var userRemoves []string
+
 		send := false
+
 		for key, val := range toSet.Setting {
 			if val.User {
 				buf = append(append(append(appendString(append(buf, ','), key), ":{\"user\":true,\"data\":"...), val.Data...), '}')
@@ -216,65 +263,87 @@ func (p *pluginsDir) RPCData(cd ConnData, method string, data json.RawMessage) (
 				userRemoves = append(userRemoves, key)
 				send = true
 			}
+
 			plugin.Data[key] = val
 		}
+
 		buf = append(buf, "},\"removing\":["...)
 		first := true
+
 		for _, key := range toSet.Removing {
 			val, ok := plugin.Data[key]
 			if !ok {
 				continue
 			}
+
 			if val.User {
 				send = true
+
 				if !first {
 					buf = append(buf, ',')
 				} else {
 					first = false
 				}
+
 				buf = appendString(buf, key)
 			}
+
 			delete(plugin.Data, key)
 		}
+
 		if err := p.FileStore.Set(toSet.ID+pluginConfigExt, plugin); err != nil {
 			return nil, err
 		}
+
 		for _, key := range userRemoves {
 			if !first {
 				buf = append(buf, ',')
 			} else {
 				first = false
 			}
+
 			buf = appendString(buf, key)
 		}
+
 		if send {
 			buf = append(buf, ']', '}')
 			cd.CurrentMap = 0
+
 			p.socket.broadcastMapChange(cd, broadcastPluginSettingChange, buf, userNotAdmin)
 		}
+
 		p.updateJSON()
 		p.mu.Unlock()
 	case "enable", "disable":
 		var filename string
+
 		if err := json.Unmarshal(data, &filename); err != nil {
 			return nil, err
 		}
+
 		p.mu.Lock()
+
 		plugin, ok := p.plugins[filename]
 		if !ok {
 			p.mu.Unlock()
+
 			return nil, ErrUnknownPlugin
 		}
+
 		plugin.Enabled = method == "enable"
+
 		if err := p.FileStore.Set(filename+pluginConfigExt, plugin); err != nil {
 			return nil, err
 		}
+
 		cd.CurrentMap = 0
+
 		p.socket.broadcastMapChange(cd, broadcastPluginChange, json.RawMessage{'0'}, userAny)
 		p.updateJSON()
 		p.mu.Unlock()
 	default:
 		return nil, ErrUnknownMethod
 	}
+
 	return nil, nil
 }
