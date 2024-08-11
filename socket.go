b62df2d527a0ea33ebee052438ee73324c583779
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-11 20:32:02 +0100

    reformatted

--- socket.go
+++ socket.go
@@ -24,6 +24,7 @@ type socket struct {
 func (s *socket) Init(b *Battlemap, _ links) error {
 	s.Battlemap = b
 	s.conns = make(map[*conn]struct{})
+
 	return nil
 }
 
@@ -32,12 +33,17 @@ func (s *socket) ServeConn(wconn *websocket.Conn) {
 		cu keystore.Uint64
 		c  conn
 	)
+
 	s.config.Get("currentUserMap", &cu)
 	s.mu.Lock()
+
 	s.nextID++
+
 	id := s.nextID
 	s.conns[&c] = struct{}{}
+
 	s.mu.Unlock()
+
 	c = conn{
 		Battlemap: s.Battlemap,
 		rpc:       jsonrpc.New(wconn, &c),
@@ -47,6 +53,7 @@ func (s *socket) ServeConn(wconn *websocket.Conn) {
 			userState:  s.authConn(wconn),
 		},
 	}
+
 	c.rpc.Handle()
 	s.mu.Lock()
 	delete(s.conns, &c)
@@ -66,6 +73,7 @@ type ID uint64
 // headers. It returns zero if no ID is found.
 func SocketIDFromRequest(r *http.Request) ID {
 	id, _ := strconv.ParseUint(r.Header.Get("X-ID"), 10, 0)
+
 	return ID(id)
 }
 
@@ -83,6 +91,7 @@ func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, erro
 		ID:         ID(atomic.LoadUint64((*uint64)(&c.ID))),
 		userState:  c.userState,
 	}
+
 	switch method {
 	case "conn.ready":
 		if c.IsAdmin() {
@@ -94,6 +103,7 @@ func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, erro
 			c.maps.mu.RLock()
 			mapData := c.maps.maps[uint64(cd.CurrentMap)]
 			c.maps.mu.RUnlock()
+
 			c.rpc.Send(jsonrpc.Response{
 				ID:     broadcastCurrentUserMapData,
 				Result: json.RawMessage(mapData.UserJSON),
@@ -107,38 +117,49 @@ func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, erro
 			if err := json.Unmarshal(data, &cd.CurrentMap); err != nil {
 				return nil, err
 			}
+
 			atomic.StoreUint64(&c.CurrentMap, cd.CurrentMap)
+
 			return nil, nil
 		}
 	case "maps.signalPosition":
 		who := userAdmin
+
 		if cd.IsAdmin() {
 			who = userAny
 		} else if !cd.IsUser() {
 			break
 		}
+
 		c.socket.broadcastMapChange(cd, broadcastSignalPosition, data, who)
+
 		return nil, nil
 	case "maps.signalMovePosition":
 		if cd.IsAdmin() {
 			c.socket.broadcastMapChange(cd, broadcastSignalMovePosition, data, userNotAdmin)
+
 			return nil, nil
 		}
 	case "maps.signalMeasure":
 		if cd.IsAdmin() {
 			c.socket.broadcastMapChange(cd, broadcastSignalMeasure, data, userNotAdmin)
+
 			return nil, nil
 		}
 	case "broadcast":
 		if cd.IsAdmin() || cd.IsUser() {
 			cd.CurrentMap = 0
+
 			c.socket.broadcastMapChange(cd, broadcastAny, data, userAny)
+
 			return nil, nil
 		}
 	case "broadcastWindow":
 		if cd.IsAdmin() {
 			cd.CurrentMap = 0
+
 			c.socket.broadcastMapChange(cd, broadcastWindow, data, userAny)
+
 			return nil, nil
 		}
 	default:
@@ -146,8 +167,10 @@ func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, erro
 		if pos <= 0 {
 			return nil, ErrUnknownMethod
 		}
+
 		submethod := method[pos+1:]
 		method = method[:pos]
+
 		switch method {
 		case "imageAssets":
 			if cd.IsAdmin() {
@@ -168,7 +191,9 @@ func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, erro
 		case "maps":
 			if submethod == "getUserMap" {
 				var currentUserMap keystore.Uint64
+
 				c.config.Get("currentUserMap", &currentUserMap)
+
 				return currentUserMap, nil
 			} else if cd.IsAdmin() {
 				return c.maps.RPCData(cd, submethod, data)
@@ -179,5 +204,6 @@ func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, erro
 			}
 		}
 	}
+
 	return nil, ErrUnknownMethod
 }
